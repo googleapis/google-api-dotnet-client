2640055c0a288de7942a7dde9aa64d5abb2cb8e1
David Waters <davidwaters@google.com>
2010-11-02 19:02:24 +0000

    Refactor ResourceClasses to new class

--- Src/GoogleApis.Tools.CodeGen/CodeGen.cs
+++ Src/GoogleApis.Tools.CodeGen/CodeGen.cs
@@ -23,75 +23,19 @@ namespace Google.Apis.Tools.CodeGen
 			CreateClient();
 			AddUsings();
 			
-			client.Types.Add(new ServiceClassGenerator().CreateServiceClass(service));
+			var serviceClass = new ServiceClassGenerator().CreateServiceClass(service);
 			
+			client.Types.Add(serviceClass);
+			var resourceGenerator = new ResourceClassGenerator();
 			foreach(var res in service.Resources) {
 				// Create a class for the resource
-				client.Types.Add(CreateClass(res.Value));
+				client.Types.Add(resourceGenerator.CreateClass(res.Value));
 			}
 			
 			return compileUnit;
 		}
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		private static String GetMethodName(Method method){
-			return UpperFirstLetter(method.Name);
-		}
-		
-		/// <summary>
-		/// Create the class for a given resource and add all the methods.
-		/// </summary>
-		/// <param name="res">
-		/// A <see cref="Resource"/>
-		/// </param>
-		/// <returns>
-		/// A <see cref="CodeTypeDeclaration"/>
-		/// </returns>
-		private CodeTypeDeclaration CreateClass(Resource res) {
-			var className = GetClassName(res);
-			
-			var resourceClass = new CodeTypeDeclaration(className);
-			
-			var methods = res.Methods.Values;
-			
-			foreach(var method in methods) {
-				resourceClass.Members.Add(CreateClassMethod(method));
-			}
-		
-			return resourceClass;
-		}
-		
-		/// <summary>
-		/// For a given resource, there are number of methods, this function creates a single method
-		/// </summary>
-		/// <param name="method">
-		/// A <see cref="Method"/>
-		/// </param>
-		/// <returns>
-		/// A <see cref="CodeMemberMethod"/>
-		/// </returns>
-		private CodeMemberMethod CreateClassMethod(Method method) {
-			var member = new CodeMemberMethod();
-			
-			member.Name = GetMethodName(method);
-			member.ReturnType = new CodeTypeReference("System.IO.Stream");
-			
-			// Add Required parameters to the method.
-			var paramList = method.Parameters.Values;
-			foreach(var param in paramList) {
-				member.Parameters.Add( 
-				  new CodeParameterDeclarationExpression(typeof(string), param.Name));
-			}
-			
-			return member;
-		}
 		
 		private void CreateClient() {
 			client = new CodeNamespace(clientNamespace + "." + UpperFirstLetter(service.Name));	
