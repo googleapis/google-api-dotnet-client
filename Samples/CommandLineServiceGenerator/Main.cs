a9af552d118c89d2f241ac547e2b256440f2f703
David Waters <davidwaters@google.com>
2010-11-15 07:56:06 +0000

    =Add Samples project ignore bin folders

---
+++ Samples/CommandLineServiceGenerator/Main.cs
@@ -0,0 +1,21 @@
+using System;
+using Google.Apis;
+using Google.Apis.Tools.CodeGen;
+
+namespace Google.Apis.Samples.ComandLineServiceGenerator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var serviceName = "buzz";
+			var version = "v1";
+			var clientNameSpace ="Google.Apis.Samples.ComandLineGeneratedService.Buzz";
+			var lang = "CSharp";
+			var output = "../../../CommandLineGeneratedService/Buzz/BuzzService.cs";
+			
+			Google.Apis.Tools.CodeGen.MainClass.GenerateClass(serviceName, version, clientNameSpace, lang, output); 
+		}
+	}
+}
+
