83635cda8e2110f459c9a270d62fbfb4e5414bf5
David Waters <davidwaters@google.com>
2010-12-04 12:53:39 +0000

    Make some of the servicerDecorators easier to test, add empty test for VersionInfoSD

---
+++ Src/GoogleApis.Tools.CodeGen.Tests/Decorator/ServiceDecorator/VersionInformationServiceDecoratorTest.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+namespace Google.Apis.Tools.CodeGen.Tests
+{
+	[TestFixture()]
+	public class VersionInformationServiceDecoratorTest
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
