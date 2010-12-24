4107b4840356cd732fce5f394aa10fb6f3181cf4
David Waters <davidwaters@google.com>
2010-12-24 12:33:34 +0000

    =More unit testing

--- Src/GoogleApis.Tests/Apis/Discovery/ServiceFactoryImplTest.cs
+++ Src/GoogleApis.Tests/Apis/Discovery/ServiceFactoryImplTest.cs
@@ -10,8 +10,36 @@ namespace Google.Apis.Tests.Apis.Discovery
     [TestFixture()]
     public class ServiceFactoryImplTest
     {
+        private const string BadDiscoveryv0_2_No_Name =  @"{
+ 'NO_name': 'adsense-mgmt',
+ 'version': 'v1beta1',
+ 'description': 'AdSense Management API',
+ 'restBasePath': '/adsense-mgmt/v1beta1/',
+ 'rpcPath': '/rpc',
+ 'resources': {
+  'mgmt': {
+   'resources': {
+    'adunits': {'foo':'bar' }
+    }
+    }
+    }
+    }";
+
         [Test()]
-        public void ServiceFactoryDiscoveryV0_2ConstructorTest ()
+        public void ServiceFactoryDiscoveryV0_2ConstructorSucsessFail ()
+        {
+            var param = new FactoryParameterV0_2("server", "base");
+            var json = (JsonDictionary)JsonReader.Parse(ServiceFactoryTest.V0_2SAMPLE_DISCOVERY);
+            
+            Assert.Throws(typeof(ArgumentNullException), () => new ServiceFactoryDiscoveryV0_2(null, param));
+            Assert.Throws(typeof(ArgumentNullException), () => new ServiceFactoryDiscoveryV0_2(json, null));
+            
+            json = (JsonDictionary)JsonReader.Parse(BadDiscoveryv0_2_No_Name);
+            Assert.Throws(typeof(ArgumentException), () => new ServiceFactoryDiscoveryV0_2(json, param));
+        }
+        
+        [Test()]
+        public void ServiceFactoryDiscoveryV0_2ConstructorSucsessTest ()
         {
             var param = new FactoryParameterV0_2("server", "base");
             var json = (JsonDictionary)JsonReader.Parse(ServiceFactoryTest.V0_2SAMPLE_DISCOVERY);
@@ -21,8 +49,7 @@ namespace Google.Apis.Tests.Apis.Discovery
             Assert.AreEqual(param, fact.Param);
             Assert.AreEqual(json, fact.Information);
             
-            Assert.Throws(typeof(ArgumentNullException), () => new ServiceFactoryDiscoveryV0_2(null, param));
-            Assert.Throws(typeof(ArgumentNullException), () => new ServiceFactoryDiscoveryV0_2(json, null));
+            
         }
     }
 }
