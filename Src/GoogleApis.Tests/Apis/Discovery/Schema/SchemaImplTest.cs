03e5e5096a8310778b083f7bf82cb11e4058cd99
David Waters <davidwaters@google.com>
2011-01-08 14:08:36 +0000

    =Add infrastructure for schemaPropertys

---
+++ Src/GoogleApis.Tests/Apis/Discovery/Schema/SchemaImplTest.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+namespace Google.Apis.Tests.Apis.Discovery.Schema
+{
+    [TestFixture()]
+    public class SchemaImplTest
+    {
+        [Test()]
+        public void TestCase ()
+        {
+        }
+    }
+}
+
