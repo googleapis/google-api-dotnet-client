1a02f71ab5bc0b16f50272c5260f36d3632d5aa2
David Waters <davidwaters@google.com>
2011-01-15 16:22:57 +0000

    White space and formating changes only, no functional change.

--- Src/GoogleApis.Tests/JSON/TokenStreamTest.cs
+++ Src/GoogleApis.Tests/JSON/TokenStreamTest.cs
@@ -19,191 +19,196 @@ using System.IO;
 
 namespace Google.Apis.Tests.Json
 {
-    
-    
+
+
     /// <summary>
     ///This is a test class for TokenStreamTest and is intended
     ///to contain all TokenStreamTest Unit Tests
     ///</summary>
-  [TestFixture()]
-  public class TokenStreamTest {
-        
-    [Test()]
-    public void GetNextTokenTestEscapeUniCode()
+    [TestFixture()]
+    public class TokenStreamTest
     {
-      TokenStream target = new TokenStream("'\\uABCD'");
-      var actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("\uABCD", actual.value);
-      
-      target = new TokenStream("'\\u0000'");
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("\u0000", actual.value);
+
+        [Test()]
+        public void GetNextTokenTestEscapeUniCode ()
+        {
+            TokenStream target = new TokenStream ("'\\uABCD'");
+            var actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("\uABCD", actual.value);
             
-      target = new TokenStream("'\\uFFFF'");
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("\uFFFF", actual.value);
-    }
-  
-    [Test()]
-    public void GetNextTokenTestEscapeTab()
-    {
-      TokenStream target = new TokenStream("'\\t'");
-      var actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("\t", actual.value);
-    }
-    
-    [Test()]
-    public void GetNextTokenTestEscapeSingleQuoteSimple()
-    {
-      TokenStream target = new TokenStream("'{\\'id\\':\\'Activitiylist\\'}'");
-      var actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("{'id':'Activitiylist'}", actual.value);            
-    }
-        
-    [Test()]
-    public void GetNextTokenTestEscapeSingleQuoteFull()
-    {
-      TokenStream target = new TokenStream("'{\\'id\\':\\'Activitiylist\\',\\'properties\\':{\\'id\\':{\\'type\\':\\'any\\'},\\'title\\':{\\'type\\':\\'any\\'},\\'items\\':{\\'items\\':{\\'$ref\\':\\'ChiliActivitiesResourceJson\\'},\\'type\\':\\'array\\'},\\'updated\\':{\\'type\\':\\'string\\'},\\'links\\':{\\'additionalProperties\\':{\\'items\\':{\\'properties\\':{\\'title\\':{\\'type\\':\\'any\\'},\\'height\\':{\\'type\\':\\'any\\'},\\'count\\':{\\'type\\':\\'any\\'},\\'updated\\':{\\'type\\':\\'string\\'},\\'width\\':{\\'type\\':\\'any\\'},\\'type\\':{\\'type\\':\\'any\\'},\\'href\\':{\\'type\\':\\'any\\'}},\\'type\\':\\'object\\'},\\'type\\':\\'array\\'},\\'type\\':\\'object\\'},\\'kind\\':{\\'default\\':\\'buzz#activityFeed\\',\\'type\\':\\'string\\'}},\\'type\\':\\'object\\'}'");
-      var actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("{\'id\':\'Activitiylist\',\'properties\':{\'id\':{\'type\':\'any\'},\'title\':{\'type\':\'any\'},\'items\':{\'items\':{\'$ref\':\'ChiliActivitiesResourceJson\'},\'type\':\'array\'},\'updated\':{\'type\':\'string\'},\'links\':{\'additionalProperties\':{\'items\':{\'properties\':{\'title\':{\'type\':\'any\'},\'height\':{\'type\':\'any\'},\'count\':{\'type\':\'any\'},\'updated\':{\'type\':\'string\'},\'width\':{\'type\':\'any\'},\'type\':{\'type\':\'any\'},\'href\':{\'type\':\'any\'}},\'type\':\'object\'},\'type\':\'array\'},\'type\':\'object\'},\'kind\':{\'default\':\'buzz#activityFeed\',\'type\':\'string\'}},\'type\':\'object\'}", actual.value);
-    }
+            target = new TokenStream ("'\\u0000'");
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("\u0000", actual.value);
+            
+            target = new TokenStream ("'\\uFFFF'");
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("\uFFFF", actual.value);
+        }
 
-    /// <summary>
-    ///A test for GetNextToken
-    ///</summary>
-    [Test()]
-    public void GetNextTokenTestSimpleObject() {
-      TokenStream target = new TokenStream("{ \"Member\": \"Value\" }"); 
-      JsonToken actual;
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ObjectStart, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("Member", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.NameSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("Value", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ObjectEnd, actual.type);
-     }
+        [Test()]
+        public void GetNextTokenTestEscapeTab ()
+        {
+            TokenStream target = new TokenStream ("'\\t'");
+            var actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("\t", actual.value);
+        }
 
-    /// <summary>
-    ///A test for GetNextToken
-    ///</summary>
-    [Test()]
-    public void GetNextTokenTestSimpleArray() {
-      TokenStream target = new TokenStream("[ \"Value1\", \"Value2\" ]");
-      JsonToken actual;
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ArrayStart, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("Value1", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("Value2", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ArrayEnd, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(null, actual);
-    }
+        [Test()]
+        public void GetNextTokenTestEscapeSingleQuoteSimple ()
+        {
+            TokenStream target = new TokenStream ("'{\\'id\\':\\'Activitiylist\\'}'");
+            var actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("{'id':'Activitiylist'}", actual.value);
+        }
 
-    /// <summary>
-    ///A test for GetNextToken
-    ///</summary>
-    [Test()]
-    public void GetNextTokenTestSpecialValues() {
-      TokenStream target = new TokenStream("[ true, \"Value2\", false, null ]");
-      JsonToken actual;
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ArrayStart, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.True, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("Value2", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.False, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.Null, actual.type);
-      actual = target.GetNextToken(); 
-      Assert.AreEqual(JsonToken.Type.ArrayEnd, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(null, actual);
-    }
+        [Test()]
+        public void GetNextTokenTestEscapeSingleQuoteFull ()
+        {
+            TokenStream target = new TokenStream ("'{\\'id\\':\\'Activitiylist\\',\\'properties\\':{\\'id\\':{\\'type\\':\\'any\\'},\\'title\\':{\\'type\\':\\'any\\'},\\'items\\':{\\'items\\':{\\'$ref\\':\\'ChiliActivitiesResourceJson\\'},\\'type\\':\\'array\\'},\\'updated\\':{\\'type\\':\\'string\\'},\\'links\\':{\\'additionalProperties\\':{\\'items\\':{\\'properties\\':{\\'title\\':{\\'type\\':\\'any\\'},\\'height\\':{\\'type\\':\\'any\\'},\\'count\\':{\\'type\\':\\'any\\'},\\'updated\\':{\\'type\\':\\'string\\'},\\'width\\':{\\'type\\':\\'any\\'},\\'type\\':{\\'type\\':\\'any\\'},\\'href\\':{\\'type\\':\\'any\\'}},\\'type\\':\\'object\\'},\\'type\\':\\'array\\'},\\'type\\':\\'object\\'},\\'kind\\':{\\'default\\':\\'buzz#activityFeed\\',\\'type\\':\\'string\\'}},\\'type\\':\\'object\\'}'");
+            var actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("{\'id\':\'Activitiylist\',\'properties\':{\'id\':{\'type\':\'any\'},\'title\':{\'type\':\'any\'},\'items\':{\'items\':{\'$ref\':\'ChiliActivitiesResourceJson\'},\'type\':\'array\'},\'updated\':{\'type\':\'string\'},\'links\':{\'additionalProperties\':{\'items\':{\'properties\':{\'title\':{\'type\':\'any\'},\'height\':{\'type\':\'any\'},\'count\':{\'type\':\'any\'},\'updated\':{\'type\':\'string\'},\'width\':{\'type\':\'any\'},\'type\':{\'type\':\'any\'},\'href\':{\'type\':\'any\'}},\'type\':\'object\'},\'type\':\'array\'},\'type\':\'object\'},\'kind\':{\'default\':\'buzz#activityFeed\',\'type\':\'string\'}},\'type\':\'object\'}", actual.value);
+        }
 
-    /// <summary>
-    ///A test for GetNextToken
-    ///</summary>
-    [Test()]
-    public void GetNextTokenTestNestedObjects() {
-      TokenStream target = new TokenStream("{ \"name\" : [ true, \"Value2\", false ], \"sub\" : { \"subname\" : 1234 } }");
-      JsonToken actual;
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ObjectStart, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("name", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.NameSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ArrayStart, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.True, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("Value2", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();   
-      Assert.AreEqual(JsonToken.Type.False, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ArrayEnd, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.MemberSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("sub", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.NameSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ObjectStart, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.String, actual.type);
-      Assert.AreEqual("subname", actual.value);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.NameSeperator, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.Number, actual.type);
-      Assert.AreEqual("1234", actual.value);
-      Assert.AreEqual(1234, actual.number);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ObjectEnd, actual.type);
-      actual = target.GetNextToken();
-      Assert.AreEqual(JsonToken.Type.ObjectEnd, actual.type);
-      
-      
-      
-      actual = target.GetNextToken();
-      Assert.AreEqual(null, actual);
+        /// <summary>
+        ///A test for GetNextToken
+        ///</summary>
+        [Test()]
+        public void GetNextTokenTestSimpleObject ()
+        {
+            TokenStream target = new TokenStream ("{ \"Member\": \"Value\" }");
+            JsonToken actual;
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ObjectStart, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("Member", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.NameSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("Value", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ObjectEnd, actual.type);
+        }
+
+        /// <summary>
+        ///A test for GetNextToken
+        ///</summary>
+        [Test()]
+        public void GetNextTokenTestSimpleArray ()
+        {
+            TokenStream target = new TokenStream ("[ \"Value1\", \"Value2\" ]");
+            JsonToken actual;
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ArrayStart, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("Value1", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("Value2", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ArrayEnd, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (null, actual);
+        }
+
+        /// <summary>
+        ///A test for GetNextToken
+        ///</summary>
+        [Test()]
+        public void GetNextTokenTestSpecialValues ()
+        {
+            TokenStream target = new TokenStream ("[ true, \"Value2\", false, null ]");
+            JsonToken actual;
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ArrayStart, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.True, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("Value2", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.False, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.Null, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ArrayEnd, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (null, actual);
+        }
+
+        /// <summary>
+        ///A test for GetNextToken
+        ///</summary>
+        [Test()]
+        public void GetNextTokenTestNestedObjects ()
+        {
+            TokenStream target = new TokenStream ("{ \"name\" : [ true, \"Value2\", false ], \"sub\" : { \"subname\" : 1234 } }");
+            JsonToken actual;
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ObjectStart, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("name", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.NameSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ArrayStart, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.True, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("Value2", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.False, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ArrayEnd, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.MemberSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("sub", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.NameSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ObjectStart, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.String, actual.type);
+            Assert.AreEqual ("subname", actual.value);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.NameSeperator, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.Number, actual.type);
+            Assert.AreEqual ("1234", actual.value);
+            Assert.AreEqual (1234, actual.number);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ObjectEnd, actual.type);
+            actual = target.GetNextToken ();
+            Assert.AreEqual (JsonToken.Type.ObjectEnd, actual.type);
+            
+            
+            
+            actual = target.GetNextToken ();
+            Assert.AreEqual (null, actual);
+        }
     }
-  }
 }
