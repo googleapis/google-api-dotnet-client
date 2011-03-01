5538f06733173fb05dc0aa7754627bee07dbd9a3
David Waters <davidwaters@google.com>
2011-03-01 11:40:32 +0000

    =Further work on JsonSerilization

--- Src/GoogleApis.Tools.CodeGen/Decorator/ServiceDecorator/NewtonsoftObjectToJson.cs
+++ Src/GoogleApis.Tools.CodeGen/Decorator/ServiceDecorator/NewtonsoftObjectToJson.cs
@@ -15,6 +15,7 @@ namespace Google.Apis.Tools.CodeGen.Decorator.ServiceDecorator
         private const string FieldName = "newtonJsonSerilizer";
         private const string PropertyName = "NewtonJsonSerilizer";
         private const string MethodName = "ObjectToJson";
+        private const string settingsVarName = "settings";
         
         /// <summary>
         /// Creates a JsonSerializer field.
@@ -54,8 +55,79 @@ namespace Google.Apis.Tools.CodeGen.Decorator.ServiceDecorator
         internal CodeMemberProperty CreateJsonSerializerGetter()
         {
             var property = new CodeMemberProperty();
-            //TODO
+            
+            //private JsonSerializer NewtonJsonSerilizer
+            property.Name = PropertyName;
+            property.Attributes = MemberAttributes.Private;
+            property.HasSet = false;
+            property.HasGet = true;
+            
+            var creationBlock = CreateSerilizerCreationBlock ();
+            
+            // if( this.newtonwJsonSerilizer == null )
+            //      ... // creationBlock
+            var condition = new CodeBinaryOperatorExpression();
+            condition.Operator = CodeBinaryOperatorType.IdentityEquality;
+            condition.Left = new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), FieldName);
+            condition.Right = new CodePrimitiveExpression(null);
+            
+            var ifStatement = new CodeConditionStatement();
+            ifStatement.Condition = condition;
+            ifStatement.TrueStatements.AddRange(creationBlock);
+            
+            // return this.newtonwJsonSerilizer;
+            var returnStatment = new CodeMethodReturnStatement();
+            returnStatment.Expression = new CodeFieldReferenceExpression(
+                                            new CodeThisReferenceExpression(),
+                                            FieldName);
+            
+            property.GetStatements.Add(ifStatement);
+            property.GetStatements.Add(returnStatment);
             return property;
+        }
+        
+        /// <summary>
+        /// Creates the following block of code
+        ///  <code>
+        ///     JsonSerializerSettings settings = new JsonSerializerSettings();
+        ///     settings.NullValueHandling = NullValueHandling.Ignore;
+        ///     this.newtonwJsonSerilizer = JsonSerializer.Create(settings);
+        ///  </code>
+        /// </summary>
+        /// <returns>
+        /// A <see cref="CodeStatementCollection"/>
+        /// </returns>
+        private CodeStatementCollection CreateSerilizerCreationBlock ()
+        {
+            // JsonSerializerSettings settings = new JsonSerializerSettings()
+            var settingsDeclarAndConstruct = new CodeVariableDeclarationStatement(
+                    typeof(JsonSerializerSettings),
+                    settingsVarName,
+                    new CodeObjectCreateExpression(typeof(JsonSerializerSettings)));
+            
+            // settings.NullValueHandling = NullValueHandling.Ignore;
+            var assignNullValueHandling = 
+                new CodeAssignStatement(
+                    new CodePropertyReferenceExpression(
+                        new CodeVariableReferenceExpression(settingsVarName), "NullValueHandling"),
+                    new CodeFieldReferenceExpression(
+                        new CodeTypeReferenceExpression(typeof(NullValueHandling)),
+                        "Ignore"));
+            
+            // this.newtonwJsonSerilizer = JsonSerializer.Create(settings);
+            var createSerilizerFromSettings = 
+                new CodeAssignStatement(
+                    new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), FieldName),
+                    new CodeMethodInvokeExpression(
+                        new CodeTypeReferenceExpression(typeof(JsonSerializer)),
+                        "Create",
+                        new CodeVariableReferenceExpression(settingsVarName)));
+            
+            var creationBlock = new CodeStatementCollection();            
+            creationBlock.Add(settingsDeclarAndConstruct);
+            creationBlock.Add(assignNullValueHandling);
+            creationBlock.Add(createSerilizerFromSettings);
+            return creationBlock;
         }
         
         /// <summary>
