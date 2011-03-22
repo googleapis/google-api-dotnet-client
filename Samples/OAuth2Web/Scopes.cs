d12bcf373190cc4296b3d80b573df5933ae51952
Hongliang Qiang <hqiang@google.com>
2011-03-21 23:56:55 -0400

    Completed ApiExplorerWinForm sample to work with 2-legged OAuth

--- Samples/OAuth2Web/Scopes.cs
+++ Samples/OAuth2Web/Scopes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Google.Apis.Samples.OAuth2Web
+namespace Google.Apis.Samples.ApiExplorerWeb
 {
     public class Scopes
     {
