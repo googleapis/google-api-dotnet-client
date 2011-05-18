7de1cb0caec4a9c4094df1533dcde785f901d875
mlinder <mlinder@google.com>
2011-05-18 16:37:16 -0400

    Updated the ListServices example to Discovery V1 -- WIP

--- Samples/ListServices/Main.cs
+++ Samples/ListServices/Main.cs
@@ -21,51 +21,93 @@ using Google.Apis.Discovery;
 
 namespace Google.Apis.Samples.ListServices
 {
-	class MainClass
-	{
-		/// <summary>
-		/// Lists all the services for a given API.
-		/// 
-		/// If no service is provided
-		/// </summary>
-		/// <param name="args">
-		/// A <see cref="System.String[]"/>
-		/// </param>
-		/// <example>
-		/// Windows:
-		/// 
-		/// ListServices.exe buzz
-		/// ListServices.exe latitude
-		/// ListServices.exe moderator
-		/// 
-		/// Mac (Mono)
-		/// mono ListServices.exe buzz
-		/// mono ListServices.exe latitude
-		/// mono ListServices.exe moderator
-		/// </example>
-		public static void Main (string[] args)
-		{
-			string serviceName = "moderator";
-			string uri = "https://www.googleapis.com/discovery/0.1/describe?api={0}&apiVersion=v1";
-			
-			if(args.Length == 1) {
-				serviceName = args[0];
-			}
-			
-			var webfetcher = new WebDiscoveryDevice { DiscoveryUri = new Uri(string.Format(uri, serviceName))};
-			var discovery = new DiscoveryService(webfetcher);
-			
-			// Build the service based on discovery information.
-			var service = discovery.GetService("v1", DiscoveryVersion.Version_0_1, null);
-			
-			// Output sevice information
-			foreach(var resource in service.Resources) {
-				var methods = resource.Value.Methods;
-				Console.Out.WriteLine("{0}", resource.Key);
-				foreach(var methodEntry in methods) {
-					Console.Out.WriteLine("\t- {0}", methodEntry.Value.RpcName);
-				}
-			}
-		}
-	}
+    class MainClass
+    {
+        /// <summary>
+        /// Lists all the services for a given API.
+        /// 
+        /// If no service is provided
+        /// </summary>
+        /// <param name="args">
+        /// A <see cref="System.String[]"/>
+        /// </param>
+        /// <example>
+        /// Windows:
+        /// 
+        /// ListServices.exe buzz
+        /// ListServices.exe latitude
+        /// ListServices.exe moderator
+        /// 
+        /// Mac / Unix (Mono)
+        /// mono ListServices.exe buzz
+        /// mono ListServices.exe latitude
+        /// mono ListServices.exe moderator
+        /// </example>
+        public static void Main (string[] args)
+        {
+            // Define our discovery source
+            string serviceName = "moderator";
+            string uri = "https://www.googleapis.com/discovery/v1/apis/{0}/v1/rest";
+            
+            if (args.Length == 1) {
+                serviceName = args[0];
+            }
+
+            var webfetcher = new WebDiscoveryDevice { DiscoveryUri = new Uri (string.Format (uri, serviceName)) };
+            var discovery = new DiscoveryService (webfetcher);
+            
+            // Build the service based on discovery information.
+            var service = discovery.GetService ("v1", DiscoveryVersion.Version_1_0, new FactoryParameterV1_0 ());
+            
+            #region Output: Service listing
+            
+            // Highlight output
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            // Output general API information
+            Console.WriteLine ("API Information:");
+            Console.WriteLine ("  Name:        {0}", service.Name);
+            // Console.WriteLine("  Title:       {0}", service.Name);
+            Console.WriteLine ("  ID:          {0}", service.Id);
+            Console.WriteLine ("  Description: {0}", service.Description);
+            Console.WriteLine ("  BaseURI:     {0}", service.BaseUri);
+            // Console.WriteLine("  RPC URI:     {0}", service.RpcUri); // no longer supported?
+            Console.WriteLine ("");
+            
+            // Output a small count-summary of this service
+            Console.WriteLine ("Summary:");
+            Console.WriteLine ("  Schemes:     {0}", service.Schemas.Count);
+            Console.WriteLine ("  Features:    {0}", service.Features.Count);
+            Console.WriteLine ("  Resources:   {0}", service.Resources.Count);
+            Console.WriteLine ("  Labels:      {0}", service.Labels.Count);
+            
+            // Output all resources & methods
+            Console.WriteLine ("");
+            Console.WriteLine ("Resources / Services:");
+            
+            OutputResourcesRecursivly (service, "  ");
+            
+            #endregion
+        }
+
+        private static void OutputResourcesRecursivly (IResourceContainer container, string prefix)
+        {
+            // Display the name of the container
+            Console.WriteLine (prefix + container.Name);
+            prefix += "    ";
+            
+            // If applicable: Show methods
+            var resource = container as IResource;
+            if (resource != null) {
+                foreach (var method in resource.Methods.Values) {
+                    Console.WriteLine ((prefix + method.Name + "()").PadRight (25) + method.Description);
+                }
+            }
+            
+            // Recursivly output all subcontainers
+            foreach (var subcontainer in container.Resources.Values) {
+                OutputResourcesRecursivly (subcontainer, prefix);
+            }
+        }
+    }
 }
