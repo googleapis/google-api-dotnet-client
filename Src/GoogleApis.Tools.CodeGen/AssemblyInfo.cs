39afb594abe861c1f0db5fac3db39e9d6a53ea1f
David Waters <davidwaters@google.com>
2011-05-23 18:10:55 +0100

    Set the version as 0.8.1 as a place to start

--- Src/GoogleApis.Tools.CodeGen/AssemblyInfo.cs
+++ Src/GoogleApis.Tools.CodeGen/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Google Inc")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Google 2010")]
+[assembly: AssemblyCopyright("Google 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,7 +35,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
