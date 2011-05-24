c920848aa5284a62707204645a88c468e28a5c76
David Waters <davidwaters@google.com>
2011-05-24 09:03:37 +0100

    Still simplifing this cl

--- GoogleApis.Tools.NAntTasks/AssemblyInfo.cs
+++ GoogleApis.Tools.NAntTasks/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GoogleApis.Tools.NAntTasks")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("Google Inc")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Google 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
