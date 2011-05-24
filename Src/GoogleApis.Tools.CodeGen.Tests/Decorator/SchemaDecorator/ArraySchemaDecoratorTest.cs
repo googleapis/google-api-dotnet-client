2e8be5f7eced35dd7f58607c15a097d7d740e9f8
mlinder <mlinder@google.com>
2011-05-24 11:11:33 -0400

    Added support for array schemas Also updated GoogleServiceGenerator for discovery v1

---
+++ Src/GoogleApis.Tools.CodeGen.Tests/Decorator/SchemaDecorator/ArraySchemaDecoratorTest.cs
@@ -0,0 +1,111 @@
+﻿/*
+Copyright 2011 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the ""License"");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an ""AS IS"" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+
+using Newtonsoft.Json.Schema;
+using NUnit.Framework;
+
+using Google.Apis.Tools.CodeGen.Decorator.SchemaDecorator;
+using Google.Apis.Tools.CodeGen.Tests.Generator;
+
+namespace Google.Apis.Tools.CodeGen.Tests.Decorator.SchemaDecorator
+{
+    /// <summary>
+    /// Tests the ArraySchemaDecorator class, which provides support
+    /// for top-level json arrays
+    /// </summary>
+    [TestFixture]
+    public class ArraySchemaDecoratorTest
+    {
+        /// <summary>
+        /// Tests if the constructor can be called without an exception
+        /// </summary>
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.DoesNotThrow(() => new ArraySchemaDecoratorTest());
+        }
+
+        /// <summary>
+        /// Tests the parameters of the DecorateClass method using null-inputs
+        /// </summary>
+        [Test]
+        public void DecorateClassTest()
+        {
+            var decorator = new ArraySchemaDecorator();
+            var declaration = new CodeTypeDeclaration();
+            var schema = new MockSchema();
+            var internalClassProvider = new ObjectInternalClassProvider();
+            Assert.Throws(typeof(ArgumentNullException), () => decorator.DecorateClass(null, schema, internalClassProvider));
+            Assert.Throws(typeof(ArgumentNullException), () => decorator.DecorateClass(declaration, null, internalClassProvider));
+            Assert.Throws(typeof(ArgumentNullException), () => decorator.DecorateClass(declaration, schema, null));
+        }
+
+        /// <summary>
+        /// Tests the edge cases of the different classes handed to the DecorateClass method
+        /// </summary>
+        [Test]
+        public void DecorateClassTestEdgeCases()
+        {
+            var decorator = new ArraySchemaDecorator();
+            var internalClassProvider = new ObjectInternalClassProvider();
+
+            var schema = new MockSchema();
+            schema.Name = "test";
+            schema.SchemaDetails = null;
+            Assert.Throws(typeof(ArgumentNullException), () => decorator.DecorateClass(new CodeTypeDeclaration(), schema, internalClassProvider));
+
+            schema.SchemaDetails = new JsonSchema();
+            schema.SchemaDetails.Type = JsonSchemaType.Float;
+            Assert.DoesNotThrow(() => decorator.DecorateClass(new CodeTypeDeclaration(), schema, internalClassProvider));
+
+            schema.SchemaDetails.Type = JsonSchemaType.Array;
+            schema.SchemaDetails.Items = new List<JsonSchema>();
+            schema.SchemaDetails.Items.Add(new JsonSchema());
+            schema.SchemaDetails.Items.Add(new JsonSchema());
+            Assert.DoesNotThrow(() => decorator.DecorateClass(new CodeTypeDeclaration(), schema, internalClassProvider));
+        }
+
+        /// <summary>
+        /// Confirms the result of the DecorateClass-method
+        /// </summary>
+        [Test]
+        public void DecorateClassGenerationTest()
+        {
+            var decorator = new ArraySchemaDecorator();
+            var internalClassProvider = new ObjectInternalClassProvider();
+            var decl = new CodeTypeDeclaration();
+
+            var schema = new MockSchema();
+            schema.Name = "test";
+            schema.SchemaDetails = new JsonSchema();
+            schema.SchemaDetails.Type = JsonSchemaType.Array;
+            schema.SchemaDetails.Items = new List<JsonSchema>();
+            schema.SchemaDetails.Items.Clear();
+            schema.SchemaDetails.Items.Add(new JsonSchema() { Description = "Test", Id = "TestSchema", Type = JsonSchemaType.Object});
+            Assert.DoesNotThrow(() => decorator.DecorateClass(decl, schema, internalClassProvider));
+
+            foreach (CodeTypeReference reference in decl.BaseTypes)
+                Assert.That(reference.BaseType, Is.StringStarting("List<"));
+
+            // Subtype will only be created later on by the NestedClassGenerator,
+            // and therefore cannot be tested here
+        }
+    }
+}
---
+++ Src/GoogleApis.Tools.CodeGen.Tests/Generator/MethodGeneratorUtilsTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Google.Apis.Tools.CodeGen.Generator;
+using Google.Apis.Discovery;
+
+namespace Google.Apis.Tools.CodeGen.Tests.Generator
+{
+    [TestFixture()]
+    public class MethodGeneratorUtilsTest
+    {
+        [Test()]
+        public void TestGetRequiredParameters()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            List<IParameter> parameters = MethodGeneratorUtils.GetRequiredParameters(method).ToList();
+            Assert.AreEqual(2, parameters.Count);
+            Assert.AreEqual("req_a", parameters[0].Name);
+            Assert.AreEqual("req_b", parameters[1].Name);
+        }
+
+        [Test()]
+        public void TestGetOptionalParameters()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            List<IParameter> parameters = MethodGeneratorUtils.GetOptionalParameters(method).ToList();
+            Assert.AreEqual(2, parameters.Count);
+            Assert.AreEqual("opt_a", parameters[0].Name);
+            Assert.AreEqual("opt_b", parameters[1].Name);
+
+            method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.noParameterTest);
+            parameters = MethodGeneratorUtils.GetOptionalParameters(method).ToList();
+            Assert.AreEqual(0, parameters.Count);
+        }
+
+        [Test()]
+        public void TestHasOptionalParameters()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            Assert.That(MethodGeneratorUtils.HasOptionalParameters(method));
+            Assert.That(MethodGeneratorUtils.HasOptionalParameters(GetMethod(BaseCodeGeneratorTest.TestMethodNames.oneOptionalParameterTest)));
+            Assert.IsFalse(MethodGeneratorUtils.HasOptionalParameters(GetMethod(BaseCodeGeneratorTest.TestMethodNames.oneRequiredParameterTest)));
+            Assert.IsFalse(MethodGeneratorUtils.HasOptionalParameters(GetMethod(BaseCodeGeneratorTest.TestMethodNames.noParameterTest)));
+        }
+
+        [Test()]
+        public void TestHasRequiredParameters()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            Assert.That(method.HasRequiredParameters());
+            Assert.IsFalse(MethodGeneratorUtils.HasRequiredParameters(GetMethod(BaseCodeGeneratorTest.TestMethodNames.oneOptionalParameterTest)));
+            Assert.That(MethodGeneratorUtils.HasRequiredParameters(GetMethod(BaseCodeGeneratorTest.TestMethodNames.oneRequiredParameterTest)));
+            Assert.IsFalse(MethodGeneratorUtils.HasRequiredParameters(GetMethod(BaseCodeGeneratorTest.TestMethodNames.noParameterTest)));
+        }
+
+        [Test()]
+        public void TestRequiredParameterSorting()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            IParameter[] parameters = method.GetRequiredParameters().ToArray();
+            Assert.AreEqual(2, parameters.Length);
+            Assert.AreEqual("req_a", parameters[0].Name);
+            Assert.AreEqual("req_b", parameters[1].Name);
+
+            method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.postTest);
+            parameters = method.GetRequiredParameters().ToArray();
+            Assert.AreEqual(3, parameters.Length);
+            Assert.AreEqual("req_a", parameters[0].Name);
+            Assert.AreEqual("req_b", parameters[1].Name);
+            Assert.AreEqual("req_c", parameters[2].Name);
+        }
+
+        [Test()]
+        public void TestOptionalParameterSorting()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            IParameter[] parameters = method.GetOptionalParameters().ToArray();
+            Assert.AreEqual(2, parameters.Length);
+            Assert.AreEqual("opt_a", parameters[0].Name);
+            Assert.AreEqual("opt_b", parameters[1].Name);
+
+            method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.postTest);
+            parameters = method.GetOptionalParameters().ToArray();
+            Assert.AreEqual(2, parameters.Length);
+            Assert.AreEqual("opt_a", parameters[0].Name);
+            Assert.AreEqual("opt_b", parameters[1].Name);
+        }
+
+        [Test()]
+        public void TestAllParameterSorting()
+        {
+            var method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.getTest);
+            IParameter[] parameters = method.GetAllParametersSorted().ToArray();
+            Assert.AreEqual(4, parameters.Length);
+            Assert.AreEqual("req_a", parameters[0].Name);
+            Assert.AreEqual("req_b", parameters[1].Name);
+            Assert.AreEqual("opt_a", parameters[2].Name);
+            Assert.AreEqual("opt_b", parameters[3].Name);
+
+            method = GetMethod(BaseCodeGeneratorTest.TestMethodNames.postTest);
+            parameters = method.GetAllParametersSorted().ToArray();
+            Assert.AreEqual(5, parameters.Length);
+            Assert.AreEqual("req_a", parameters[0].Name);
+            Assert.AreEqual("req_b", parameters[1].Name);
+            Assert.AreEqual("req_c", parameters[2].Name);
+            Assert.AreEqual("opt_a", parameters[3].Name);
+            Assert.AreEqual("opt_b", parameters[4].Name);
+        }
+
+        private IMethod GetMethod(BaseCodeGeneratorTest.TestMethodNames testMethod)
+        {
+            var resource = BaseCodeGeneratorTest.CreateResourceDivcoveryV_1_0(BaseCodeGeneratorTest.ResourceName, BaseCodeGeneratorTest.ResourceAsJson);
+            return resource.Methods[testMethod.ToString()];
+        }
+    }
+}
