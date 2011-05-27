f2e50f93d48d1454ba9d9be8ae81c1fcaa3fde04
mlinder <mlinder@google.com>
2011-05-27 13:07:22 -0400

    Code refactored

---
+++ Src/GoogleApis.Tools.CodeGen/Decorator/ResourceDecorator/StandardMethodResourceDecorator.cs
@@ -0,0 +1,314 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+
+using Google.Apis.Discovery;
+using Google.Apis.Testing;
+using Google.Apis.Tools.CodeGen.Generator;
+using Google.Apis.Util;
+
+namespace Google.Apis.Tools.CodeGen.Decorator.ResourceDecorator
+{
+    /// <summary>
+    /// Adds the standard methods to the given Resource class, 
+    /// one method for each one described in the discovery document.
+    /// There will be parameters added for each of the parameters described in the discovery document
+    /// </summary>
+    public class StandardMethodResourceDecorator : IResourceDecorator
+    {
+        private static log4net.ILog logger = log4net.LogManager.GetLogger (typeof(StandardMethodResourceDecorator));
+        private readonly bool returnObjects;
+        private readonly bool acceptObjectsAsBody;
+        private readonly IObjectTypeProvider objectTypeProvider;
+        private readonly string methodNameSufix;
+        private readonly IMethodCommentCreator commentCreator;
+        
+        /// <summary>
+        /// Constructs a StandardMethodResourceDecorator which creates methods with body parameter as string 
+        /// and return type as System.io.Stream.
+        /// </summary>
+        public StandardMethodResourceDecorator()
+        {
+            returnObjects = false;
+            acceptObjectsAsBody = false;
+            objectTypeProvider = null;
+            commentCreator = new DefaultEnglishCommentCreator();
+            
+            methodNameSufix = "";
+        }
+        
+        public StandardMethodResourceDecorator(
+                bool returnObjects, bool acceptObjectsAsBody, IObjectTypeProvider objectTypeProvider, IMethodCommentCreator commentCreator)
+        {
+            if (returnObjects || acceptObjectsAsBody ) 
+            {
+                objectTypeProvider.ThrowIfNull("objectTypeProvider");
+            }
+            this.returnObjects = returnObjects;
+            this.acceptObjectsAsBody = acceptObjectsAsBody;
+            this.objectTypeProvider = objectTypeProvider;
+            this.commentCreator = commentCreator;
+            this.methodNameSufix = "AsObject";
+        }
+
+        public void DecorateClass (IResource resource, string className, CodeTypeDeclaration resourceClass, 
+                                   ResourceClassGenerator generator, string serviceClassName, 
+                                   IEnumerable<IResourceDecorator> allDecorators)
+        {
+            var gen = new ResourceGenerator (
+                className, returnObjects, acceptObjectsAsBody, objectTypeProvider, methodNameSufix, commentCreator);
+            int methodNumber = 1;
+            foreach (var method in resource.Methods.Values) {
+                logger.DebugFormat ("Adding Standard Method {0}.{1}", resource.Name, method.Name);
+                CodeTypeMember convenienceMethod = gen.CreateMethod (resource, method, methodNumber, allDecorators);
+                if (convenienceMethod != null) {
+                    resourceClass.Members.Add (convenienceMethod);
+                }
+                methodNumber++;
+            }
+            
+        }
+
+        public void DecorateMethodBeforeExecute (IResource resource, IMethod method, CodeMemberMethod codeMember)
+        {
+            ;
+        }
+
+
+        public void DecorateMethodAfterExecute (IResource resource, IMethod method, CodeMemberMethod codeMember)
+        {
+            ;
+        }
+
+        [VisibleForTestOnly]
+        internal class ResourceGenerator : ResourceBaseGenerator
+        {
+            private readonly string className;
+            private readonly bool returnObjects;
+            private readonly bool acceptObjectsAsBody;
+            private readonly IObjectTypeProvider objectTypeProvider;
+            private readonly string methodNameSuffix;
+            private readonly IMethodCommentCreator commentCreator;
+
+            public ResourceGenerator (
+                    string className,bool returnObjects, bool acceptObjectsAsBody, 
+                    IObjectTypeProvider objectTypeProvider, string methodNameSuffix,
+                    IMethodCommentCreator commentCreator)
+            {
+                if (returnObjects || acceptObjectsAsBody ) 
+                {
+                    objectTypeProvider.ThrowIfNull("objectTypeProvider");
+                }
+                this.returnObjects = returnObjects;
+                this.acceptObjectsAsBody = acceptObjectsAsBody;
+                this.objectTypeProvider = objectTypeProvider;
+                this.methodNameSuffix = methodNameSuffix;
+                this.className = className;
+                this.commentCreator = commentCreator;
+            }
+   
+            [VisibleForTestOnly]
+            internal CodeTypeReference GetReturnType(IMethod method) 
+            {
+                if (returnObjects == false || method.ResponseType.IsNullOrEmpty())
+                {
+                    return new CodeTypeReference ("System.IO.Stream");
+                } 
+                else 
+                {
+                    return objectTypeProvider.GetReturnType(method);
+                }
+            }
+            
+            public CodeTypeReference GetBodyType(IMethod method) 
+            {
+                if ( acceptObjectsAsBody == false || method.RequestType.IsNullOrEmpty() )
+                {
+                    return new CodeTypeReference(typeof(string));
+                } else {
+                    return objectTypeProvider.GetBodyType(method);
+                }
+            }
+            
+            public CodeMemberMethod CreateMethod (IResource resource, IMethod method, int methodNumber, 
+                                                  IEnumerable<IResourceDecorator> allDecorators)
+            {
+                var member = new CodeMemberMethod ();
+                
+                member.Name = GeneratorUtils.GetMethodName (method, methodNumber, resource.Methods.Keys) + this.methodNameSuffix;
+                member.ReturnType = GetReturnType(method);
+                member.Attributes = MemberAttributes.Public;
+                if( commentCreator != null )
+                {
+                    member.Comments.AddRange(commentCreator.CreateMethodComment(method));
+                }
+                
+                
+                CodeStatementCollection assignmentStatments = new CodeStatementCollection ();
+                
+                ResourceCallAddBodyDeclaration (method, member, GetBodyType(method));
+                
+                AddAllDeclaredParameters (method, member, assignmentStatments);
+                
+                //System.Collections.Generic.Dictionary<string, string> parameters = 
+                //      new System.Collections.Generic.Dictionary<string, string>();
+                member.Statements.Add (DeclareParamaterDictionary ());
+                
+                //parameters["<%=parameterName%>"] = <%=parameterName%>;
+                member.Statements.AddRange (assignmentStatments);
+                
+                foreach (IResourceDecorator decorator in allDecorators) {
+                    decorator.DecorateMethodBeforeExecute (resource, method, member);
+                }
+                
+                // System.IO.Stream ret = this.service.ExecuteRequest(this.RESOURCE, "<%=methodName%>", parameters);
+                member.Statements.Add (CreateExecuteRequest (method));
+                
+                foreach (IResourceDecorator decorator in allDecorators) {
+                    decorator.DecorateMethodAfterExecute (resource, method, member);
+                }
+                
+                // return ret;
+                var returnStatment = new CodeMethodReturnStatement (
+                                        new CodeVariableReferenceExpression (ReturnVariableName));
+                
+                member.Statements.Add (returnStatment);
+                
+                return member;
+            }
+            
+            protected override CodeExpression GetBodyAsString (IMethod method)
+            {
+                if (this.acceptObjectsAsBody == false || method.RequestType.IsNullOrEmpty())
+                {
+                    return base.GetBodyAsString(method);
+                }
+                
+                // this.service.ObjectToJson(body)
+                var service = new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), ServiceFieldName);
+                var body = new CodeVariableReferenceExpression("body");
+                return new CodeMethodInvokeExpression(service, "ObjectToJson", body);
+            }
+
+            
+            /// <summary>
+            /// [ReturnType] ret = this.service.JsonToObject&lt;ReturnType&gt;(this.service.Execute(...));
+            /// </summary>
+            protected override CodeStatement CreateExecuteRequest (IMethod method)
+            {
+                method.ThrowIfNull("method");
+                if ( this.returnObjects == false || method.ResponseType.IsNullOrEmpty()) 
+                {
+                    return base.CreateExecuteRequest (method);
+                }
+                this.objectTypeProvider.ThrowIfNull("objectTypeProvider");
+                
+                var returnType = objectTypeProvider.GetReturnType(method);
+                
+                // this.service.Execute(...)
+                var getStream = CreateExecuteCall(method);
+                
+                // this.service.JsonToObject<ReturnType>([stream])
+                var call = new CodeMethodInvokeExpression();
+                call.Method = new CodeMethodReferenceExpression();
+                call.Method.TargetObject = new CodeFieldReferenceExpression(
+                        new CodeThisReferenceExpression(), ServiceFieldName);
+                call.Method.MethodName = "JsonToObject";
+                call.Method.TypeArguments.Add(returnType);
+                call.Parameters.Add(getStream);
+                
+                // [ReturnType] ret = [call]
+                return new CodeVariableDeclarationStatement (returnType, ReturnVariableName, call);
+            }
+
+            [VisibleForTestOnly()]
+            protected void AddAllDeclaredParameters (IMethod method, CodeMemberMethod member, 
+                                                     CodeStatementCollection assignmentStatments)
+            {
+                // Add All parameters to the method.
+                if (method.Parameters != null && method.Parameters != null) {
+                    int parameterCount = 1;
+                    foreach (var param in method.GetAllParametersSorted()) {
+                        string parameterName = GeneratorUtils.GetParameterName (param, parameterCount, method.Parameters.Keys);
+                        member.Parameters.Add (DeclareInputParameter (param, parameterCount, method));
+                        assignmentStatments.Add (AssignParameterToDictionary (param, parameterCount, method));
+                        AddParameterComment(commentCreator, member, param, parameterName);
+                        parameterCount++;
+                    }
+                }
+            }
+
+            protected override string GetClassName ()
+            {
+                return className;
+            }
+
+            /// <summary>
+            /// produces
+            /// Dictionary<string, object> parameters = new Dictionary<string, object>();
+            /// </summary>
+            private CodeStatement DeclareParamaterDictionary ()
+            {
+                // produces
+                //Dictionary<string, object> parameters = new Dictionary<string, object>();
+                return new CodeVariableDeclarationStatement (typeof(Dictionary<string, object>),
+                    ParameterDictionaryName, 
+                    new CodeObjectCreateExpression (typeof(Dictionary<string, object>)));
+            }
+        }
+        
+        /// <summary>
+        /// Implementors of this interface will know what the return type of a given method should be
+        /// and what the type of the body parameter should be.
+        /// For example the return type may be System.IO.String or a 
+        /// generated type based on the schema section of the discovery document,
+        /// </summary>
+        public interface IObjectTypeProvider
+        {
+            CodeTypeReference GetReturnType(IMethod method);
+            CodeTypeReference GetBodyType(IMethod method);
+        }
+        
+        public class DefaultObjectTypeProvider:IObjectTypeProvider
+        {
+            private readonly string schemaNamespace;
+            
+            public DefaultObjectTypeProvider (string schemaNamespace)
+            {
+                this.schemaNamespace = schemaNamespace;
+            }
+
+            public CodeTypeReference GetReturnType (IMethod method)
+            {
+                method.ThrowIfNull("method");
+                method.ResponseType.ThrowIfNullOrEmpty("method.ResponseType");
+                return new CodeTypeReference(schemaNamespace + '.' + method.ResponseType);
+            }
+
+            public CodeTypeReference GetBodyType (IMethod method)
+            {
+                method.ThrowIfNull("method");
+                method.RequestType.ThrowIfNullOrEmpty("method.RequestType");
+                return new CodeTypeReference(schemaNamespace + '.' + method.RequestType);                
+            }
+        }
+        
+    }
+}
+
---
+++ Src/GoogleApis/Apis/Requests/Request.cs
@@ -0,0 +1,360 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Google.Apis.Authentication;
+using Google.Apis.Discovery;
+using Google.Apis.Testing;
+using Google.Apis.Util;
+
+namespace Google.Apis.Requests
+{
+    /// <summary>
+    /// Request to a service
+    /// </summary>
+    public class Request : IRequest
+    {
+        private const string UserAgent = "{0} google-api-dotnet-client/{1}";
+        private const string GZipUserAgentSuffix = " (gzip)";
+        private const string GZipEncoding = "gzip";
+
+        private static readonly String ApiVersion = typeof (Request).Assembly.GetName().Version.ToString();
+
+        /// <summary>
+        /// The authenticator used for this request
+        /// </summary>
+        internal IAuthenticator Authenticator { get; private set; }
+
+        /// <summary>
+        /// The name of the application making the request. Affects the User Agent of this client. 
+        /// </summary>
+        internal String AppName { get; private set; }
+        
+        /// <summary>
+        /// The developer API Key sent along with the request
+        /// </summary>
+        internal String DeveloperKey { get; private set; }
+
+        /// <summary>
+        /// Set of method parameters
+        /// </summary>
+        [VisibleForTestOnly]
+        internal IDictionary<string, string> Parameters { get; set; }
+
+        private IService Service { get; set; }
+        private IMethod Method { get; set; }
+        private Uri BaseURI { get; set; }
+        private string RPCName { get; set; } // note: this property is apparently never used
+        private string Body { get; set; }
+        private ReturnType ReturnType { get; set; }
+        
+        private Uri requestUrl;
+
+        public Request()
+        {
+            AppName = "Unknown Application";
+            Authenticator = new NullAuthenticator();
+        }
+
+        /// <summary>
+        /// Given an API method, create the appropriate Request for it.
+        /// </summary>
+        public static Request CreateRequest(IService service, IMethod method)
+        {
+            switch (method.HttpMethod)
+            {
+                case "GET":
+                    return new GETRequest { Service = service, Method = method, BaseURI = service.BaseUri };
+                case "PUT":
+                    return new PUTRequest { Service = service, Method = method, BaseURI = service.BaseUri };
+                case "POST":
+                    return new POSTRequest { Service = service, Method = method, BaseURI = service.BaseUri };
+                case "DELETE":
+                    return new DELETERequest { Service = service, Method = method, BaseURI = service.BaseUri };
+            }
+
+            throw new ArgumentOutOfRangeException("method",
+                                                  string.Format("Unknown HTTP method \"{0}\"", method.HttpMethod));
+        }
+
+        /// <summary>
+        /// The method to call
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Request"/>
+        /// </returns>
+        public IRequest On(string rpcName)
+        {
+            RPCName = rpcName;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the type of data that is expected to be returned from the request.
+        /// 
+        /// Defaults to Json
+        /// </summary>
+        /// <param name="returnType">
+        /// A <see cref="ReturnType"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="Request"/>
+        /// </returns>
+        public IRequest Returning(ReturnType returnType)
+        {
+            ReturnType = returnType;
+            return this;
+        }
+
+
+        /// <summary>
+        /// Adds the parameters to the request.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Request"/>
+        /// </returns>
+        public IRequest WithParameters(IDictionary<string, object> parameters)
+        {
+            return WithParameters(parameters.ToDictionary(
+                k => k.Key, v => v.Value != null ? v.Value.ToString() : null));
+        }
+
+
+        /// <summary>
+        /// Adds the parameters to the request.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Request"/>
+        /// </returns>
+        public IRequest WithParameters(IDictionary<string, string> parameters)
+        {
+            Parameters = parameters;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a set of URL encoded parameters to the request
+        /// </summary>
+        public IRequest WithParameters(string parameters)
+        {
+            Parameters = Utilities.QueryStringToDictionary(parameters);
+            return this;
+        }
+
+        /// <summary>
+        /// Uses the string provied as the body of the request.
+        /// </summary>
+        public IRequest WithBody(string body)
+        {
+            Body = body;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the Application name on the UserAgent String
+        /// </summary>
+        /// <param name="name">
+        /// A <see cref="System.String"/>
+        /// </param>
+        public IRequest WithAppName(string name)
+        {
+            AppName = name;
+            return this;
+        }
+
+        /// <summary>
+        /// Uses the provided authenticator to add authentication information to this request.
+        /// </summary>
+        public IRequest WithAuthentication(IAuthenticator authenticator)
+        {
+            authenticator.ThrowIfNull("Authenticator");
+            Authenticator = authenticator;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the developer key to this request
+        /// </summary>
+        public IRequest WithDeveloperKey(string key)
+        {
+            DeveloperKey = key;
+            return this;
+        }
+
+        /// <summary>
+        /// Builds the resulting Url for the whole request
+        /// </summary>
+        [VisibleForTestOnly]
+        internal Uri BuildRequestUrl()
+        {
+            string restPath = Method.RestPath;
+            var queryParams = new List<string>();
+
+            queryParams.Add(ReturnType == ReturnType.Json ? "alt=json" : "alt=atom");
+
+            if (DeveloperKey.IsNotNullOrEmpty())
+            {
+                queryParams.Add("key=" + Uri.EscapeUriString(DeveloperKey). // Escapses most of what we need
+                                             Replace("&", "%26"). // Also escaped & and ?
+                                             Replace("?", "%3F"));
+            }
+
+            // Replace the substitution parameters
+            foreach (var parameter in Parameters)
+            {
+                IParameter parameterDefinition = Method.Parameters[parameter.Key];
+                string value = parameter.Value;
+                if (value.IsNullOrEmpty()) // If the parameter is present and has no value, use the default
+                {
+                    value = parameterDefinition.DefaultValue;
+                }
+                switch (parameterDefinition.ParameterType)
+                {
+                    case "path":
+                        restPath = restPath.Replace(String.Format("{{{0}}}", parameter.Key), value);
+                        break;
+                    case "query":
+                        // If the parameter is optional and no value is given, don't add to url.
+                        if (parameterDefinition.Required == false && value.IsNullOrEmpty())
+                        {
+                            continue;
+                        }
+                        queryParams.Add(parameterDefinition.Name + "=" + value);
+                        break;
+                    default:
+                        throw new NotSupportedException("Found an unsupported Parametertype [" +
+                                                        parameterDefinition.ParameterType + "]");
+                }
+            }
+
+            // URL encode the parameters and append them to the URI
+            string path = restPath;
+            if (queryParams.Count > 0)
+            {
+                path += "?" + String.Join("&", queryParams.ToArray());
+            }
+
+
+            return new Uri(BaseURI, path);
+        }
+
+        private static string GetReturnMimeType(ReturnType returnType)
+        {
+            switch (returnType)
+            {
+                case ReturnType.Atom:
+                    return "application/atom+xml";
+                case ReturnType.Json:
+                    return "application/json";
+                default:
+                    throw new ArgumentOutOfRangeException("returnType", returnType, "Unknown return type"); 
+            }
+        }
+
+        private HttpWebRequest CreateRequest()
+        {
+            // Formulate the RequestUrl
+            requestUrl = BuildRequestUrl();
+
+            // Create the request
+            HttpWebRequest request = Authenticator.CreateHttpWebRequest(Method.HttpMethod, requestUrl);
+
+            // Insert the content type and user agent
+            request.ContentType = GetReturnMimeType(ReturnType);
+            request.UserAgent = String.Format(UserAgent, AppName, ApiVersion);
+
+            // Check if compression is supported
+            if (Service.GZipEnabled)
+            {
+                request.UserAgent += GZipUserAgentSuffix;
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            }
+
+            return request;
+        }
+
+        private void AttachBody(HttpWebRequest request)
+        {
+            Stream bodyStream = request.GetRequestStream();
+
+            // If enabled: Encapsulate in GZipStream
+            if (Service.GZipEnabled) 
+            {
+                // Change the content encoding and apply a gzip filter
+                request.Headers.Add(HttpRequestHeader.ContentEncoding, GZipEncoding);
+                bodyStream = new GZipStream(bodyStream, CompressionMode.Compress);
+            }
+
+            // Write data into the stream
+            using (bodyStream)
+            {
+                byte[] postBody = Encoding.ASCII.GetBytes(Body);
+                bodyStream.Write(postBody, 0, postBody.Length);
+            }
+        }
+
+
+        /// <summary>
+        /// Executes a request given the configuration options supplied.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Stream"/>
+        /// </returns>
+        public Stream ExecuteRequest()
+        {
+            // Validate the input
+            var validator = new MethodValidator(Method, Parameters);
+            if (validator.ValidateAllParameters() == false)
+            {
+                return Stream.Null;
+            }
+
+            // Create the request
+            HttpWebRequest request = CreateRequest();
+
+            // Attach a body if a POST and there is something to attach.
+            if (String.IsNullOrEmpty(Body) == false && (Method.HttpMethod == "POST" || Method.HttpMethod == "PUT"))
+            {
+                AttachBody(request);
+            }
+
+            // Execute the request
+            try
+            {
+                var response = (HttpWebResponse) request.GetResponse();
+                return response.GetResponseStream();
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response != null)
+                {
+                    return ex.Response.GetResponseStream();
+                }
+
+                // The exception is not something the client can handle via a stream.
+                throw;
+            }
+        }
+    }
+}
