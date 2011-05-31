38a654967637b081022f426655bf654af0d17ebb
mlinder <mlinder@google.com>
2011-05-31 17:11:20 -0400

    Added more comments to the ServiceGenerator example

--- SamplesV1/ServiceGenerator/Program.cs
+++ SamplesV1/ServiceGenerator/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+Copyright 2011 Google Inc
+
+Licensed under the Apache License, Version 2.0(the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using Google.Apis.Samples.Helper;
 using Google.Apis.Tools.CodeGen;
 
-namespace ServiceGenerator
+namespace Google.Apis.Samples.ServiceGenerator
 {
-    class Program
+    /// <summary>
+    /// This example is a command line service generator, which uses discovery and the CodeGen library
+    /// to create a strongly typed binding for the specified service. You can either use this example,
+    /// or use the pregenerated service files found on the Google APIs page.
+    /// 
+    /// Command-line:
+    ///     ServiceGenerator.exe [serviceName serviceVersion]
+    /// </summary>
+    internal class Program
     {
+        /// <summary>
+        /// User input for this example
+        /// </summary>
         [Description("service")]
         public class ServiceDescription
         {
@@ -19,6 +42,10 @@ namespace ServiceGenerator
             public string ServiceVersion = "v1";
         }
 
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             CommandLine.DisplayGoogleSampleHeader("Service Generator");
@@ -29,6 +56,7 @@ namespace ServiceGenerator
                                   : CommandLine.CreateClassFromUserinput<ServiceDescription>();
 
 
+            //todo: work on the naming schema
             string fileName = string.Format("{0}.{1}.cs", description.ServiceName,
                                             description.ServiceVersion);
 
@@ -40,10 +68,10 @@ namespace ServiceGenerator
                                                    fileName);
 
 
-            CommandLine.WriteLine("^9 Service generated in " + fileName + "!");
+            CommandLine.WriteLine("^9 Service generated in ^4" + fileName + "^9!");
 
+            // ..and we are done!
             CommandLine.PressAnyKeyToExit();
-
         }
     }
 }
