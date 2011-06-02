96124a9ab301e317ec7c927145d08974ec360294
mlinder <mlinder@google.com>
2011-06-02 18:52:49 -0400

    Enabled the renaming routine in GeneratorUtils for colliding property names

---
+++ Src/GoogleApis.Tools.CodeGen/Decorator/SchemaDecorator/INestedClassSchemaDecorator.cs
@@ -0,0 +1,35 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.CodeDom;
+using Newtonsoft.Json.Schema;
+
+namespace Google.Apis.Tools.CodeGen.Decorator.SchemaDecorator
+{
+    /// <summary>
+    /// A given schema may contain nested classes, classes that implement this method may 
+    /// decorate those internal classes, often classes that implement this 
+    /// interface will also implement ISchemaDecorator
+    /// </summary>
+    /// <seealso cref="ISchemaDecorator"/> 
+    public interface INestedClassSchemaDecorator
+    {
+        void DecorateInternalClass(CodeTypeDeclaration typeDeclaration,
+                                   string name,
+                                   JsonSchema schema,
+                                   INestedClassProvider internalClassProvider);
+    }
+}
