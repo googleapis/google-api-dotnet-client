cf8f69e695e659365391de343a49a869d42c91c3
mlinder <mlinder@google.com>
2011-06-03 14:58:43 -0400

    Added a system test which tests discovery, the base api and the code gen

---
+++ Src/GoogleApis.Tools.CodeGen/Decorator/SchemaDecorator/StandardPropertyDecorator.cs
@@ -0,0 +1,119 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System.Collections.Generic;
+using System.CodeDom;
+using System.Linq;
+using log4net;
+using Newtonsoft.Json.Schema;
+using Google.Apis.Discovery.Schema;
+using Google.Apis.Testing;
+using Google.Apis.Util;
+
+namespace Google.Apis.Tools.CodeGen.Decorator.SchemaDecorator
+{
+    /// <summary>
+    /// Adds all the properties (getters and setters) for schema classes.
+    /// This depends on fields being added by StandardFieldDecorator, or similar.
+    /// </summary>
+    public class StandardPropertyDecorator : ISchemaDecorator, INestedClassSchemaDecorator
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(StandardPropertyDecorator));
+
+        #region INestedClassSchemaDecorator Members
+
+        public void DecorateInternalClass(CodeTypeDeclaration typeDeclaration,
+                                          string name,
+                                          JsonSchema schema,
+                                          INestedClassProvider internalClassProvider)
+        {
+            typeDeclaration.ThrowIfNull("typeDeclatation");
+            schema.ThrowIfNull("schema");
+            internalClassProvider.ThrowIfNull("internalClassProvider");
+            typeDeclaration.Members.AddRange(GenerateAllProperties(name, schema, internalClassProvider).ToArray());
+        }
+
+        #endregion
+
+        #region ISchemaDecorator Members
+
+        public void DecorateClass(CodeTypeDeclaration typeDeclaration,
+                                  ISchema schema,
+                                  INestedClassProvider internalClassProvider)
+        {
+            typeDeclaration.ThrowIfNull("typeDeclatation");
+            schema.ThrowIfNull("schema");
+            typeDeclaration.Members.AddRange(
+                GenerateAllProperties(schema.Name, schema.SchemaDetails, internalClassProvider).ToArray());
+        }
+
+        #endregion
+
+        [VisibleForTestOnly]
+        internal IList<CodeMemberProperty> GenerateAllProperties(string name,
+                                                                 JsonSchema schema,
+                                                                 INestedClassProvider internalClassProvider)
+        {
+            schema.ThrowIfNull("schema");
+            name.ThrowIfNullOrEmpty("name");
+            logger.DebugFormat("Adding properties for {0}", name);
+
+
+            var fields = new List<CodeMemberProperty>();
+
+            if (schema.Properties.IsNullOrEmpty())
+            {
+                logger.Debug("No proeprties found for schema " + name);
+                return fields;
+            }
+
+            int index = 0;
+            foreach (var propertyPair in schema.Properties)
+            {
+                fields.Add(
+                    GenerateProperty(
+                        propertyPair.Key, propertyPair.Value, index++, internalClassProvider, schema.Properties.Keys));
+            }
+            return fields;
+        }
+
+        [VisibleForTestOnly]
+        internal CodeMemberProperty GenerateProperty(string name,
+                                                     JsonSchema propertySchema,
+                                                     int index,
+                                                     INestedClassProvider internalClassProvider,
+                                                     IEnumerable<string> otherPropertyNames)
+        {
+            name.ThrowIfNullOrEmpty("name");
+            propertySchema.ThrowIfNull("propertySchema");
+
+            var ret = new CodeMemberProperty();
+            ret.Name = SchemaDecoratorUtil.GetPropertyName(name, index, otherPropertyNames);
+            ret.Type = SchemaDecoratorUtil.GetCodeType(propertySchema, internalClassProvider);
+            ret.Attributes = MemberAttributes.Public;
+
+            ret.HasGet = true;
+            var fieldReference = new CodeFieldReferenceExpression(
+                new CodeThisReferenceExpression(), SchemaDecoratorUtil.GetFieldName(name, index, otherPropertyNames));
+            ret.GetStatements.Add(new CodeMethodReturnStatement(fieldReference));
+
+            ret.HasSet = true;
+            var parameterReference = new CodeVariableReferenceExpression("value");
+            ret.SetStatements.Add(new CodeAssignStatement(fieldReference, parameterReference));
+
+            return ret;
+        }
+    }
+}
---
+++ Tools/TestJSonReader/TokenStreamTest.cs
@@ -0,0 +1,195 @@
+﻿using Google.Apis.JSON;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TestJSonReader
+{
+    
+    
+    /// <summary>
+    ///This is a test class for TokenStreamTest and is intended
+    ///to contain all TokenStreamTest Unit Tests
+    ///</summary>
+  [TestClass()]
+  public class TokenStreamTest {
+
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext {
+      get {
+        return testContextInstance;
+      }
+      set {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+
+    /// <summary>
+    ///A test for GetNextToken
+    ///</summary>
+    [TestMethod()]
+    public void GetNextTokenTestSimpleObject() {
+      TokenStream target = new TokenStream("{ \"Member\": \"Value\" }"); 
+      JSONToken actual;
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ObjectStart, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("Member", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.NameSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("Value", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ObjectEnd, actual.type);
+     }
+
+    /// <summary>
+    ///A test for GetNextToken
+    ///</summary>
+    [TestMethod()]
+    public void GetNextTokenTestSimpleArray() {
+      TokenStream target = new TokenStream("[ \"Value1\", \"Value2\" ]");
+      JSONToken actual;
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ArrayStart, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("Value1", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("Value2", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ArrayEnd, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(null, actual);
+    }
+
+    /// <summary>
+    ///A test for GetNextToken
+    ///</summary>
+    [TestMethod()]
+    public void GetNextTokenTestSpecialValues() {
+      TokenStream target = new TokenStream("[ true, \"Value2\", false, null ]");
+      JSONToken actual;
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ArrayStart, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.True, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("Value2", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.False, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.Null, actual.type);
+      actual = target.GetNextToken(); 
+      Assert.AreEqual(JSONToken.Type.ArrayEnd, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(null, actual);
+    }
+
+    /// <summary>
+    ///A test for GetNextToken
+    ///</summary>
+    [TestMethod()]
+    public void GetNextTokenTestNestedObjects() {
+      TokenStream target = new TokenStream("{ \"name\" : [ true, \"Value2\", false ], \"sub\" : { \"subname\" : 1234 } }");
+      JSONToken actual;
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ObjectStart, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("name", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.NameSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ArrayStart, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.True, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("Value2", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();   
+      Assert.AreEqual(JSONToken.Type.False, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ArrayEnd, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.MemberSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("sub", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.NameSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ObjectStart, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.String, actual.type);
+      Assert.AreEqual("subname", actual.value);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.NameSeperator, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.Number, actual.type);
+      Assert.AreEqual("1234", actual.value);
+      Assert.AreEqual(1234, actual.number);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ObjectEnd, actual.type);
+      actual = target.GetNextToken();
+      Assert.AreEqual(JSONToken.Type.ObjectEnd, actual.type);
+      
+      
+      
+      actual = target.GetNextToken();
+      Assert.AreEqual(null, actual);
+    }
+  }
+}
