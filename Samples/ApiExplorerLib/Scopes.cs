2ff44b3c72d728d5f3863ec7c31adc14a776b6c7
David Waters <davidwaters@google.com>
2011-06-13 08:21:24 +0100

    Changes for AutoBuild - configure projects to generate .xml docs, move properties into common NAnt.Config.xml used by both build files, Ignore new Generated Folder, Change folder structure of generated code.

---
+++ Samples/ApiExplorerLib/Scopes.cs
@@ -0,0 +1,53 @@
+﻿/*
+Copyright 2011 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Google.Apis.Samples.ApiExplorer
+{
+    /// <summary>
+    /// Provides map from service name to the (first) scope to use for multiple Google APIs
+    /// </summary>
+    /// <remarks>
+    /// Hard-coded here because Discovery service does not provide scope information.
+    /// </remarks>
+    public class Scopes
+    {
+        private static IDictionary<string, string> _scopes = 
+            new Dictionary<string, string>
+            {
+    			{ "buzz", "https://www.googleapis.com/auth/buzz" },
+    			{ "chromewebstore", "https://www.googleapis.com/auth/chromewebstore" },
+    			{ "translate", "https://translate.google.com/toolkit/feeds" },
+                { "moderator", "https://www.googleapis.com/auth/moderator" },
+            };
+
+        public static string GetScope(string serviceName)
+        {
+            if (_scopes.ContainsKey(serviceName))
+            {
+                return _scopes[serviceName];
+            }
+            else
+            {
+                return "https://www.googleapis.com/auth/" + serviceName;
+            }
+        }
+    }
+}
---
+++ Src/GoogleApis.Tools.CodeGen/Generator/MethodGeneratorUtils.cs
@@ -0,0 +1,81 @@
+﻿/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+using Google.Apis.Discovery;
+using Google.Apis.Util;
+
+namespace Google.Apis.Tools.CodeGen.Generator
+{
+    /// <summary>
+    /// Utility class for Method-generators
+    /// </summary>
+    public static class MethodGeneratorUtils
+    {
+        /// <summary>
+        /// Return all required IParemters from the given method.
+        /// Sorted alphabeticly by name 
+        /// </summary>
+        public static IEnumerable<IParameter> GetRequiredParameters(this IMethod method)
+        {
+            if (method == null || method.Parameters.IsNullOrEmpty())
+            {
+                return Enumerable.Empty<IParameter>();
+            }
+            return from p in method.Parameters where p.Value.Required orderby p.Value.Name select p.Value;
+        }
+
+
+        /// <summary>
+        /// Return all optional IParemters from the given method.
+        /// Sorted alphabeticly by name 
+        /// </summary>
+        public static IEnumerable<IParameter> GetOptionalParameters(this IMethod method)
+        {
+            if (method == null || method.Parameters.IsNullOrEmpty())
+            {
+                return Enumerable.Empty<IParameter>();
+            }
+            return from p in method.Parameters where p.Value.Required == false orderby p.Value.Name select p.Value;
+        }
+
+        /// <summary>
+        /// Return all IParemters from the given method.
+        /// sorted by required then optional, alphabeticly within those groupings 
+        /// </summary>
+        public static IEnumerable<IParameter> GetAllParametersSorted(this IMethod method)
+        {
+            return method.GetRequiredParameters().Concat(method.GetOptionalParameters());
+        }
+
+        /// <summary>
+        /// True if any of the parameters are required
+        /// </summary>
+        public static bool HasRequiredParameters(this IMethod method)
+        {
+            return GetRequiredParameters(method).Any();
+        }
+
+        /// <summary>
+        /// True if any of the parameters are optional
+        /// </summary>
+        public static bool HasOptionalParameters(this IMethod method)
+        {
+            return GetOptionalParameters(method).Any();
+        }
+    }
+}
