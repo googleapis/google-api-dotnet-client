b89382d9af826a168865f341e31ae16d79be07c0
David Waters <davidwaters@google.com>
2011-06-18 15:18:03 +0100

    Produce .xml documentation file for each genreated service dll.

---
+++ Src/GoogleApis.Tools.CodeGen.Tests/GeneratorUtilsTest.cs
@@ -0,0 +1,334 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Google.Apis.Discovery;
+using Google.Apis.Json;
+
+namespace Google.Apis.Tools.CodeGen.Tests
+{
+    /// <summary>
+    /// Tests for the GeneratorUtils class
+    /// </summary>
+    [TestFixture]
+    public class GeneratorUtilsTest
+    {
+        /// <summary>
+        /// Tests the generation of safe parameter names
+        /// </summary>
+        [Test]
+        public void GetParameterNameTest()
+        {
+            var paramArgs = new KeyValuePair<string, object>("safeName", new JsonDictionary());
+            IParameter param = new ParameterFactory.BaseParameter(paramArgs);
+            Assert.AreEqual("safeName", GeneratorUtils.GetParameterName(param, Enumerable.Empty<string>()));
+
+            paramArgs = new KeyValuePair<string, object>("string", new JsonDictionary());
+            param = new ParameterFactory.BaseParameter(paramArgs);
+            Assert.AreEqual("stringValue", GeneratorUtils.GetParameterName(param, Enumerable.Empty<string>()));
+
+            paramArgs = new KeyValuePair<string, object>("String", new JsonDictionary());
+            param = new ParameterFactory.BaseParameter(paramArgs);
+            Assert.AreEqual("stringValue", GeneratorUtils.GetParameterName(param, Enumerable.Empty<string>()));
+
+            paramArgs = new KeyValuePair<string, object>("SafeName", new JsonDictionary());
+            param = new ParameterFactory.BaseParameter(paramArgs);
+            Assert.AreEqual("safeName", GeneratorUtils.GetParameterName(param, Enumerable.Empty<string>()));
+        }
+
+        /// <summary>
+        /// Tests the generation of safe member names
+        /// </summary>
+        [Test]
+        public void GetSafeMemberNameTest()
+        {
+            IList<string> simpleSafeWords = new List<string> { "unsafe", "words", "abound" };
+
+            Assert.AreEqual(
+                "fishBurger",
+                GeneratorUtils.GetSafeMemberName(simpleSafeWords, 0, "fishBurger"));
+            Assert.AreEqual(
+                "member",
+                GeneratorUtils.GetSafeMemberName(
+                    simpleSafeWords, GeneratorUtils.TargetCase.ToLower, "!@#$$%^&^&**((())+}{|\":\\\t\r"));
+            
+            foreach (string word in GeneratorUtils.UnsafeWords)
+            {
+                Assert.AreEqual(word + "Member", GeneratorUtils.GetSafeMemberName(GeneratorUtils.UnsafeWords, 0, word));
+            }
+
+            // Test the "basenameMember"-pattern
+            string[] unsafeWords = new string[GeneratorUtils.SafeMemberMaximumIndex];
+            for (int i = 0; i < unsafeWords.Length; i++)
+                unsafeWords[i] = "test" + (i == 0 ? "" : (i + 1).ToString());
+
+            Assert.AreEqual("testMember", GeneratorUtils.GetSafeMemberName(unsafeWords, 0, "test"));
+        }
+
+        /// <summary>
+        /// Tests if the LowerFirstLetter method works
+        /// </summary>
+        [Test]
+        public void LowwerFirstTest()
+        {
+            string upper = "ABC";
+            string lower = "aBC";
+
+            Assert.AreEqual(lower, GeneratorUtils.LowerFirstLetter(lower));
+            Assert.AreEqual(lower, GeneratorUtils.LowerFirstLetter(upper));
+
+            Assert.AreEqual("", GeneratorUtils.LowerFirstLetter(""));
+            Assert.AreEqual(null, GeneratorUtils.LowerFirstLetter(null));
+        }
+
+        /// <summary>
+        /// Tests if the UpperfirstLetter method works
+        /// </summary>
+        [Test]
+        public void UpperFirstTest()
+        {
+            string upper = "ABC";
+            string lower = "aBC";
+
+            Assert.AreEqual(upper, GeneratorUtils.UpperFirstLetter(lower));
+            Assert.AreEqual(upper, GeneratorUtils.UpperFirstLetter(upper));
+
+            Assert.AreEqual("", GeneratorUtils.UpperFirstLetter(""));
+            Assert.AreEqual(null, GeneratorUtils.UpperFirstLetter(null));
+        }
+
+        /// <summary>
+        /// Tests if the generator can generate valid body names
+        /// </summary>
+        [Test]
+        public void ValidBodyCharTest()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Assert.IsTrue(GeneratorUtils.IsValidBodyChar(c), "Char " + c + " should be valid");
+            }
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Assert.IsTrue(GeneratorUtils.IsValidBodyChar(c), "Char " + c + " should be valid");
+            }
+
+            for (char c = '0'; c <= '9'; c++)
+            {
+                Assert.IsTrue(GeneratorUtils.IsValidBodyChar(c), "Char " + c + " should be valid");
+            }
+
+            Assert.IsTrue(GeneratorUtils.IsValidBodyChar('_'));
+            
+            for (char c = Char.MinValue; c < '0'; c++)
+            {
+                Assert.IsFalse(GeneratorUtils.IsValidBodyChar(c), "Char " + (int) c + " should be invalid");
+            }
+
+            for (char c = (char) ('9' + 1); c < 'A'; c++)
+            {
+                Assert.IsFalse(GeneratorUtils.IsValidBodyChar(c), "Char " + (int) c + " should be invalid");
+            }
+
+            for (char c = (char) ('Z' + 1); c < 'a'; c++)
+            {
+                if (c == '_')
+                {
+                    continue;
+                }
+
+                Assert.IsFalse(GeneratorUtils.IsValidBodyChar(c), "Char " + (int) c + " should be invalid");
+            }
+
+            for (char c = (char) ('z' + 1); c < Char.MaxValue; c++)
+            {
+                Assert.IsFalse(GeneratorUtils.IsValidBodyChar(c), "Char " + (int) c + " should be invalid");
+            }
+        }
+
+        /// <summary>
+        /// Tests if the generator will only generate valid first chars
+        /// </summary>
+        [Test]
+        public void ValidFirstCharTest()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Assert.IsTrue(GeneratorUtils.IsValidFirstChar(c), "Char " + c + " should be valid");
+            }
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Assert.IsTrue(GeneratorUtils.IsValidFirstChar(c), "Char " + c + " should be valid");
+            }
+
+            Assert.IsTrue(GeneratorUtils.IsValidBodyChar('_'));
+
+            for (char c = Char.MinValue; c < 'A'; c++)
+            {
+                Assert.IsFalse(GeneratorUtils.IsValidFirstChar(c), "Char " + (int) c + " should be invalid");
+            }
+
+            for (char c = (char) ('Z' + 1); c < 'a'; c++)
+            {
+                if (c == '_')
+                {
+                    continue;
+                }
+
+                Assert.IsFalse(GeneratorUtils.IsValidFirstChar(c), "Char " + (int) c + " should be invalid");
+            }
+
+            for (char c = (char) ('z' + 1); c < Char.MaxValue; c++)
+            {
+                Assert.IsFalse(GeneratorUtils.IsValidFirstChar(c), "Char " + (int) c + " should be invalid");
+            }
+        }
+
+        /// <summary>
+        /// Tests the GetWordContextListFromClass method
+        /// </summary>
+        [Test]
+        public void GetWordContextListFromClassTest()
+        {
+            // Test validation.
+            Assert.Throws<ArgumentNullException>(() => GeneratorUtils.GetWordContextListFromClass(null).First());
+
+            // Test normal operation
+            var decl = new CodeTypeDeclaration();
+            decl.Name = "MockClass";
+            decl.Members.Add(new CodeMemberField(typeof(int), "FieldA"));
+            decl.Members.Add(new CodeMemberProperty() { Name = "FieldB" });
+            decl.Members.Add(new CodeTypeDeclaration() { Name = "NestedClassC" });
+
+            CollectionAssert.AreEqual(
+                new[] { "MockClass", "FieldA", "FieldB", "NestedClassC" },
+                GeneratorUtils.GetWordContextListFromClass(decl));
+        }
+        
+        /// <summary>
+        /// Checks that the MakeValidMemberName method returns valid member names.
+        /// </summary>
+        [Test]
+        public void MakeValidMemberNameTest()
+        {
+            // Test empty strings.
+            Assert.AreEqual(null, GeneratorUtils.MakeValidMemberName(null));
+            Assert.AreEqual(null, GeneratorUtils.MakeValidMemberName(""));
+
+            // Tests strings consisting only out of invalid characters
+            Assert.AreEqual(null, GeneratorUtils.MakeValidMemberName("!@#"));
+            Assert.AreEqual(null, GeneratorUtils.MakeValidMemberName("   "));
+            Assert.AreEqual(null, GeneratorUtils.MakeValidMemberName("123456789"));
+
+            // Test valid names
+            Assert.AreEqual("SomeClassName2", GeneratorUtils.MakeValidMemberName("SomeClassName2"));
+            Assert.AreEqual("_memberName", GeneratorUtils.MakeValidMemberName("_memberName"));
+
+            // Test that invalid characters are removed
+            Assert.AreEqual("ref", GeneratorUtils.MakeValidMemberName("$ref"));
+            Assert.AreEqual("unknown", GeneratorUtils.MakeValidMemberName("(unknown)"));
+            Assert.AreEqual("FooBar", GeneratorUtils.MakeValidMemberName("Foo@bar"));
+            Assert.AreEqual("fooBar", GeneratorUtils.MakeValidMemberName("foo!@#$bar"));
+        }
+
+        /// <summary>
+        /// Confirms that the IsNameValidInContext method will recognize invalid names.
+        /// </summary>
+        [Test]
+        public void IsNameValidInContextTest()
+        {
+            string[] emptyList = new string[0];
+            string[] forbiddenContext = new[] { "evil", "foo", "bar", "INVALID" };
+
+            // Test with an empty name.
+            Assert.IsFalse(GeneratorUtils.IsNameValidInContext(null, emptyList));
+            Assert.IsFalse(GeneratorUtils.IsNameValidInContext("", emptyList));
+
+            // Test with a valid name.
+            Assert.IsTrue(GeneratorUtils.IsNameValidInContext("foobar", emptyList));
+            Assert.IsTrue(GeneratorUtils.IsNameValidInContext("foobar", forbiddenContext));
+            
+            // Test with some invalid names.
+            Assert.IsFalse(GeneratorUtils.IsNameValidInContext("foo", forbiddenContext));
+            Assert.IsFalse(GeneratorUtils.IsNameValidInContext("evil", forbiddenContext));
+
+            // Confirm that character casing is valued.
+            Assert.IsTrue(GeneratorUtils.IsNameValidInContext("Foo", forbiddenContext));
+            Assert.IsTrue(GeneratorUtils.IsNameValidInContext("Evil", forbiddenContext));
+            Assert.IsTrue(GeneratorUtils.IsNameValidInContext("invalid", forbiddenContext));
+            Assert.IsFalse(GeneratorUtils.IsNameValidInContext("INVALID", forbiddenContext));
+        }
+
+        /// <summary>
+        /// Tests the AlterCase method.
+        /// </summary>
+        [Test]
+        public void AlterFirstCharCaseTest()
+        {
+            // Test empty strings.
+            Assert.IsNull(GeneratorUtils.AlterFirstCharCase(null, GeneratorUtils.TargetCase.ToLower));
+            Assert.AreEqual("", GeneratorUtils.AlterFirstCharCase("", GeneratorUtils.TargetCase.ToLower));
+
+            // Test the ToLower variant.
+            Assert.AreEqual("a", GeneratorUtils.AlterFirstCharCase("A", GeneratorUtils.TargetCase.ToLower));
+            Assert.AreEqual("a", GeneratorUtils.AlterFirstCharCase("a", GeneratorUtils.TargetCase.ToLower));
+
+            // Test the ToUpper variant.
+            Assert.AreEqual("A", GeneratorUtils.AlterFirstCharCase("a", GeneratorUtils.TargetCase.ToUpper));
+            Assert.AreEqual("A", GeneratorUtils.AlterFirstCharCase("A", GeneratorUtils.TargetCase.ToUpper));
+
+            // Test the DontChange variant.
+            Assert.AreEqual("a", GeneratorUtils.AlterFirstCharCase("a", GeneratorUtils.TargetCase.DontChange));
+            Assert.AreEqual("A", GeneratorUtils.AlterFirstCharCase("A", GeneratorUtils.TargetCase.DontChange));
+
+            // Test a more complex string.
+            Assert.AreEqual("FooBar", GeneratorUtils.AlterFirstCharCase("fooBar", GeneratorUtils.TargetCase.ToUpper));
+            Assert.AreEqual("fooBar", GeneratorUtils.AlterFirstCharCase("FooBar", GeneratorUtils.TargetCase.ToLower));
+        }
+
+        /// <summary>
+        /// Confirm the returned enumeration of the AppendIndices method.
+        /// </summary>
+        [Test]
+        public void AppendIndicesTest()
+        {
+            Assert.Throws<ArgumentException>(() => GeneratorUtils.AppendIndices("test", 10, 1).First());
+            
+            CollectionAssert.AreEqual(new string[0], GeneratorUtils.AppendIndices(null, 1, 5));
+            CollectionAssert.AreEqual(new string[0], GeneratorUtils.AppendIndices("", 1, 5));
+            CollectionAssert.AreEqual(new[] { "abc1", "abc2", "abc3" }, GeneratorUtils.AppendIndices("abc", 1, 3));
+        }
+
+        /// <summary>
+        /// Test for GenerateAlternativeNamesFor method.
+        /// </summary>
+        [Test]
+        public void GenerateAlternativeNamesForTest()
+        {
+            // Generate the list of expected values.
+            IEnumerable<string> expect = new[] { "to", "toMember" };
+            expect = expect.Concat(GeneratorUtils.AppendIndices("to", 2, GeneratorUtils.SafeMemberMaximumIndex));
+            expect = expect.Concat(GeneratorUtils.AppendIndices("toMember", 2, GeneratorUtils.SafeMemberMaximumIndex));
+
+            // Compare with the implementation.
+            IEnumerable<string> alternatives = GeneratorUtils.GenerateAlternativeNamesFor("to");
+            CollectionAssert.AreEquivalent(expect, alternatives);
+        }
+    }
+}
---
+++ Src/GoogleApis/Apis/Discovery/Service.cs
@@ -0,0 +1,387 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Google.Apis.Json;
+using Google.Apis.JSON;
+using Google.Apis.Requests;
+using Google.Apis.Testing;
+using Google.Apis.Util;
+using Google.Apis.Discovery.Schema;
+using log4net;
+using Newtonsoft.Json;
+
+namespace Google.Apis.Discovery
+{
+
+    #region BaseService
+
+    /// <summary>
+    /// Represents the basic implementation of a service
+    /// </summary>
+    public abstract class BaseService : IService
+    {
+        /// <summary>
+        /// Name of the method used for serialization (object -> json)
+        /// </summary>
+        public const string SerializationMethodName = "SerializeRequest";
+
+        /// <summary>
+        /// Name of the method used for deserialization (json -> object)
+        /// </summary>
+        public const string DeserializationMethodName = "DeserializeResponse";
+
+        /// <summary>
+        /// Name of the property defining the ISerializer
+        /// </summary>
+        public const string SerializerPropertyName = "Serializer";
+
+        private static readonly ILog logger = LogManager.GetLogger(typeof(BaseService));
+
+        protected internal readonly JsonDictionary information;
+        private Dictionary<string, IResource> resources;
+        private IDictionary<String, ISchema> schemas;
+        private ISerializer serializer;
+
+        internal BaseService(string version, string name, JsonDictionary values, BaseFactoryParameters param) : this()
+        {
+            version.ThrowIfNull("version");
+            name.ThrowIfNull("name");
+            values.ThrowIfNull("values");
+            param.ThrowIfNull("param");
+
+            // Set required properties
+            Version = version;
+            Name = name;
+            information = values;
+
+            // Set optional properties
+            Id = values.GetValueAsNull("id") as string;
+            Labels = values.GetValueAsStringListOrEmpty("labels").ToList().AsReadOnly();
+            Features = values.GetValueAsStringListOrEmpty("features").ToList().AsReadOnly();
+            DocumentationLink = values.GetValueAsNull("documentationLink") as string;
+            Protocol = values.GetValueAsNull("protocol") as string;
+            Description = values.GetValueAsNull("description") as string;
+            Title = values.GetValueAsNull("title") as string;
+
+            // Determine the Server URL and (optional) Base Path
+            param.ServerUrl.ThrowIfNull("param.ServerUrl");
+            ServerUrl = param.ServerUrl;
+            BasePath = param.BasePath;
+        }
+
+        private BaseService()
+        {
+            GZipEnabled = true;
+            Serializer = new NewtonsoftJsonSerializer();
+        }
+
+        protected string ServerUrl { get; set; }
+        protected string BasePath { get; set; }
+
+        #region IService Members
+
+        public string Name { get; private set; }
+        public string Version { get; private set; }
+        public string Description { get; private set; }
+        public string Title { get; private set; }
+
+        public string Id { get; private set; }
+        public IList<string> Labels { get; private set; }
+        public IList<string> Features { get; private set; }
+        public string DocumentationLink { get; private set; }
+        public string Protocol { get; private set; }
+
+        public abstract DiscoveryVersion DiscoveryVersion { get; }
+
+        public Uri BaseUri
+        {
+            get { return new Uri(ServerUrl + BasePath); }
+        }
+
+        public Uri RpcUri
+        {
+            get { return new Uri(information[ServiceFactory.RpcUrl] as string); }
+        }
+
+        public IDictionary<string, IResource> Resources
+        {
+            get
+            {
+                if (resources == null)
+                {
+                    JsonDictionary js = information.GetValueAsNull(ServiceFactory.Resources) as JsonDictionary;
+                    if (js != null)
+                    {
+                        resources = new Dictionary<string, IResource>();
+                        foreach (KeyValuePair<string, object> kvp in js)
+                        {
+                            IResource r = CreateResource(kvp);
+                            resources.Add(kvp.Key, r);
+                        }
+                    }
+                    else
+                    {
+                        resources = new Dictionary<string, IResource>();
+                    }
+                }
+                return resources;
+            }
+        }
+
+        public virtual IDictionary<string, ISchema> Schemas
+        {
+            get
+            {
+                if (schemas != null)
+                {
+                    return schemas;
+                }
+
+                logger.DebugFormat("Fetching Schemas for service {0}", Name);
+                var js = information[ServiceFactory.Schemas] as JsonDictionary;
+                if (js != null)
+                {
+                    schemas = ParseSchemas(js);
+                }
+                else
+                {
+                    schemas = new Dictionary<string, ISchema>(0).AsReadOnly();
+                }
+
+                return schemas;
+            }
+        }
+
+        public ISerializer Serializer
+        {
+            get { return serializer; }
+            set
+            {
+                value.ThrowIfNull("value");
+                serializer = value;
+            }
+        }
+
+        /// <summary>
+        /// Creates a Request Object based on the HTTP Method Type.
+        /// </summary>
+        public IRequest CreateRequest(string resource, string methodName)
+        {
+            var method = GetResource(this, resource).Methods[methodName];
+            var request = Request.CreateRequest(this, method);
+
+            return request;
+        }
+
+        /// <summary>
+        /// Retrieves a resource using the full resource name.
+        /// Example:
+        ///     TopResource.SubResource will retrieve the SubResource which can be found under the TopResource.
+        /// </summary>
+        [VisibleForTestOnly]
+        internal static IResource GetResource(IResourceContainer container, string fullResourceName)
+        {
+            fullResourceName.ThrowIfNull("fullResourceName");
+
+            string[] split = fullResourceName.Split(new[] { '.' }, 2);
+            string topResourceName = split[0];
+            IResource topResource = container.Resources[topResourceName];
+
+            if (split.Length <= 1)
+            {
+                // This is the resource we are looking for.
+                return topResource;
+            }
+            
+            // Retrieve the top resource, and re-run this method on it.
+            string fullSubresourceName = split[1];
+            return GetResource(topResource, fullSubresourceName);
+        }
+
+        public string SerializeRequest(object obj)
+        {
+            if (HasFeature(Discovery.Features.LegacyDataResponse))
+            {
+                // Legacy path
+                var request = new StandardResponse<object> { Data = obj };
+                return Serializer.Serialize(request);
+            }
+
+            // New v1.0 path
+            return Serializer.Serialize(obj);
+        }
+
+        public T DeserializeResponse<T>(Stream input)
+        {
+            // Read in the entire content
+            string text;
+
+            using (var reader = new StreamReader(input))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            // Check if there was an error returned. The error node is returned in both paths
+            // Deserialize the stream based upon the format of the stream
+            if (HasFeature(Discovery.Features.LegacyDataResponse))
+            {
+                // Legacy path (deprecated!)
+                StandardResponse<T> response = Serializer.Deserialize<StandardResponse<T>>(text);
+
+                if (response.Error != null)
+                {
+                    throw new GoogleApiException(this, "Server error - " + response.Error);
+                }
+            
+                if (response.Data == null)
+                {
+                    throw new GoogleApiException(this, "The response could not be deserialized.");
+                }
+
+                return response.Data;
+            }
+
+            // New path: Deserialize the object directly
+            T result = Serializer.Deserialize<T>(text);
+
+            // If this schema/object provides an error container, check it
+            if (result is IResponse)
+            {
+                var response = (IResponse) result;
+                if (response.Error != null)
+                {
+                    throw new GoogleApiException(this, "Server error - " + response.Error);
+                }
+            }
+
+            return result;
+        }
+
+        public bool HasFeature(Features feature)
+        {
+            return Features.Contains(feature.GetStringValue());
+        }
+
+        public bool GZipEnabled { get; set; }
+
+        #endregion
+
+        internal static IDictionary<string, ISchema> ParseSchemas(JsonDictionary js)
+        {
+            js.ThrowIfNull("values");
+
+            var working = new Dictionary<string, ISchema>();
+
+            var resolver = new FutureJsonSchemaResolver();
+            foreach (KeyValuePair<string, object> kvp in js)
+            {
+                logger.DebugFormat("Found schema {0}", kvp.Key);
+                var serilizer = new JsonSerializer();
+                var textWriter = new StringWriter();
+                serilizer.Serialize(textWriter, kvp.Value);
+                string result = textWriter.ToString();
+                ISchema schema = new SchemaImpl(kvp.Key, result, resolver);
+                working.Add(schema.Name, schema);
+            }
+
+            resolver.ResolveAndVerify();
+
+            return working.AsReadOnly();
+        }
+
+        /// <summary>
+        /// Creates a version specific resource containing 
+        /// out of the specified KeyValuePair
+        /// </summary>
+        /// <param name="kvp"></param>
+        /// <returns></returns>
+        public virtual IResource CreateResource(KeyValuePair<string, object> kvp)
+        {
+            return new ResourceV1_0(kvp);
+        }
+    }
+
+    #endregion
+
+    #region Service V1.0
+
+    /// <summary>
+    /// Represents a Service as defined in Discovery V1.0
+    /// </summary>
+    public class ServiceV1_0 : BaseService
+    {
+        private const string BasePathField = "basePath";
+
+        /// <summary>
+        /// Creates a v1.0 service
+        /// </summary>
+        public ServiceV1_0(string version, string name, FactoryParameterV1_0 param, JsonDictionary values)
+            : base(version, name, values, param)
+        {
+            // If no BasePath has been set, then retrieve it from the json document
+            if (BasePath.IsNullOrEmpty())
+            {
+                BasePath = information.GetMandatoryValue<string>(BasePathField);
+            }
+        }
+
+        public override DiscoveryVersion DiscoveryVersion
+        {
+            get { return DiscoveryVersion.Version_1_0; }
+        }
+    }
+
+    #endregion
+
+    #region Service V0.3
+
+    /// <summary>
+    /// Represents a Service as defined in Discovery V0.3
+    /// </summary>
+    public class ServiceV0_3 : BaseService
+    {
+        private const string RestBasePathField = "restBasePath";
+
+        /// <summary>
+        /// Creates a v0.3 service
+        /// </summary>
+        public ServiceV0_3(string version, string name, FactoryParameterV0_3 param, JsonDictionary values)
+            : base(version, name, values, param)
+        {
+            // If no BasePath has been set, then retrieve it from the json document
+            if (BasePath.IsNullOrEmpty())
+            {
+                BasePath = information.GetMandatoryValue<string>(RestBasePathField);
+            }
+        }
+
+        public override DiscoveryVersion DiscoveryVersion
+        {
+            get { return DiscoveryVersion.Version_0_3; }
+        }
+
+        public override IResource CreateResource(KeyValuePair<string, object> kvp)
+        {
+            return new ResourceV0_3(kvp);
+        }
+    }
+
+    #endregion
+}
