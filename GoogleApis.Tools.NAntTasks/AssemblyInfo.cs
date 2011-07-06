403fee40f2ffcb5ab85fcb1284737a35cc665d26
mlinder <mlinder@google.com>
2011-07-06 10:03:16 -0400

    Weekly Release 0.9.0

--- GoogleApis.Tools.NAntTasks/AssemblyInfo.cs
+++ GoogleApis.Tools.NAntTasks/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.8.3.*")]
+[assembly: AssemblyVersion("0.9.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
