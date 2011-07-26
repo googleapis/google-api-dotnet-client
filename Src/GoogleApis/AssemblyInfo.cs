de8ac6fce4e36e022279c7d8c7953722d36e99c7
mlinder <mlinder@google.com>
2011-07-26 18:06:34 -0400

    Merged changes from branch issue_76_etag.

--- Src/GoogleApis/AssemblyInfo.cs
+++ Src/GoogleApis/AssemblyInfo.cs
@@ -30,13 +30,12 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Google.Apis.Tests")]
 [assembly: InternalsVisibleTo("Google.Apis.Tools.CodeGen.Tests")]
-[assembly: InternalsVisibleTo("Google.Apis.Tests.Apis.Discovery.V0_3")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.9.0.*")]
+[assembly: AssemblyVersion("0.9.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
