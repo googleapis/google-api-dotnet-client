3d8a62a79530c466cf79db730278595afee56c9a
mlinder <mlinder@google.com>
2011-08-05 15:26:54 -0400

    Added the missing "Fields" and "UserIP" fields on requests.

---
+++ Samples/AdSenseApi/Auth/ClientAuth.cs
@@ -0,0 +1,127 @@
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Google.Apis.Authentication;
+
+namespace Google.Apis.Samples.AdSenseApi.Auth
+{
+	public class ClientAuth:Authenticator, IAuthenticator
+	{
+		private log4net.ILog logger = log4net.LogManager.GetLogger(typeof(ClientAuth));
+		private readonly static Regex authTokenRegex = new Regex(@"Auth=([0-9a-zA-Z-_]+)", 
+		                                                         //RegexOptions.Compiled 
+		                                                          RegexOptions.Singleline 
+		                                                         //| RegexOptions.ECMAScript
+		                                                         );
+		
+		private readonly IUserNamePasswordSupplier supplier;
+		private readonly string scope;
+		private string authToken = null;
+		public ClientAuth (IUserNamePasswordSupplier supplier, string scope, string applicationName):base(applicationName)
+		{
+			this.supplier = supplier;
+			this.scope = scope;
+		}
+		
+		public override void ApplyAuthenticationToRequest (System.Net.HttpWebRequest request)
+		{
+			CheckAuthentication();
+			if(authToken == null)
+			{
+				throw new ApplicationException("Found no Authentication Token");
+			}
+			
+			logger.DebugFormat("Adding Authorization header to request");
+			request.Headers.Add("Authorization", string.Format("GoogleLogin auth={0}", authToken));
+			logger.DebugFormat("Request was [{0}]", request.RequestUri);
+		}
+
+		private void CheckAuthentication()
+		{
+			if(authToken != null)
+			{
+				logger.Debug("Auth Token Already Present");
+				return;
+			}
+			logger.Debug("No AuthenticationToken present.");
+			authToken = FetchAuthentication();
+		}
+		
+		private string FetchAuthentication()
+		{
+			
+			Uri uri = new Uri(string.Format(
+				"https://www.google.com/accounts/ClientLogin?Email={0}&Passwd={1}&service={2}&accountType={3}",
+			    HttpUtility.UrlEncode(supplier.GetUserName()),
+			    HttpUtility.UrlEncode(supplier.GetPassword()),
+			    HttpUtility.UrlEncode(scope),
+			    HttpUtility.UrlEncode("HOSTED_OR_GOOGLE")));
+			logger.DebugFormat("Fetching Auth Token from ["+uri.ToString()+"]");
+			HttpWebRequest request = RequestFactory.Create (uri);
+			
+			
+			using(var response = request.GetResponse ())
+			{
+				StreamReader sr = new StreamReader(response.GetResponseStream());
+				string result = sr.ReadToEnd();
+				Match match = authTokenRegex.Match (result);
+				if(match.Success == false)
+				{
+					throw new ApplicationException("Failed to find auth token in ["+result+"]" );
+				}
+				Group grp = match.Groups[1];
+				string auth = grp.Value;
+				logger.DebugFormat("Found auth token [{0}] from result [{1}]", auth, result);
+				return auth;
+			}			
+		}
+	}
+	
+	public interface IUserNamePasswordSupplier
+	{
+		string GetUserName();
+		string GetPassword();
+	}
+	
+	public class PrefetchedUserNamePasswordSupplier: IUserNamePasswordSupplier
+	{
+		private string UserName{get;set;}
+		private string Password{get;set;}
+		
+		public PrefetchedUserNamePasswordSupplier(string userName, string password)
+		{
+			this.UserName = userName;
+			this.Password = password;
+		}
+		
+		public string GetUserName ()
+		{
+			return UserName;
+		}
+		
+		public string GetPassword ()
+		{
+			return Password;
+		}
+		
+	}
+}
---
+++ Src/GoogleApis.Tests/Apis/Requests/ServiceRequestTest.cs
@@ -0,0 +1,207 @@
+﻿/*
+Copyright 2011 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Google.Apis.Discovery;
+using Google.Apis.Requests;
+using Google.Apis.Testing;
+using Google.Apis.Util;
+using Moq;
+using NUnit.Framework;
+
+namespace Google.Apis.Tests.Apis.Requests
+{
+    /// <summary>
+    /// Tests for the ServiceRequest class.
+    /// </summary>
+    [TestFixture]
+    public class ServiceRequestTest
+    {
+        /// <summary>
+        /// Mock implementation of a IRequestProvider.
+        /// </summary>
+        private class MockRequestProvider : IRequestProvider
+        {
+            public IRequest CreateRequest(string resource, string method)
+            {
+                Assert.AreEqual("Resource", resource);
+                Assert.AreEqual("Method", method);
+                return new MockRequest() { StreamToReturn = new MemoryStream() };
+            }
+
+            public string SerializeObject(object data)
+            {
+                // Return a random string. The result won't matter/won't be checked, but we don't want the
+                // code to throw an exception here.
+                return "Not implemented";
+            }
+
+            public T DeserializeResponse<T>(IResponse response)
+            {
+                return (T)Convert.ChangeType("FooBar", typeof(T));
+            }
+        }
+
+        private class MockServiceRequest<T> : ServiceRequest<T>
+        {
+            public MockServiceRequest(IRequestProvider service) : base(service) {}
+
+            protected override string ResourceName
+            {
+                get { return "Resource"; }
+            }
+
+            protected override string MethodName
+            {
+                get { return "Method"; }
+            }
+
+            [RequestParameter]
+            public int TestParameterA { get; set; }
+
+            [RequestParameter("B")]
+            public int TestParameterB { get; set; }
+
+            public int TestParameterC { get; set; }
+
+            /// <summary>
+            /// The body of this Mock request.
+            /// </summary>
+            public object Body { get; set; }
+
+            protected override object GetBody()
+            {
+                return Body;
+            }
+        }
+
+        /// <summary>
+        /// Tests the constructor of a ServiceRequest.
+        /// </summary>
+        [Test]
+        public void ConstructTest()
+        {
+            Assert.DoesNotThrow(() => new MockServiceRequest<string>(new MockRequestProvider()));
+        }
+
+        /// <summary>
+        /// Tests the CreateParameterDictionary method.
+        /// </summary>
+        [Test]
+        public void CreateParameterDictionaryTest()
+        {
+            var request = new MockServiceRequest<string>(new MockRequestProvider());
+            request.TestParameterA = 42;
+            request.TestParameterB = 43;
+            request.TestParameterC = 44;
+
+            IDictionary<string, object> parameters = request.CreateParameterDictionary();
+            Assert.AreEqual(2, parameters.Count);
+            Assert.AreEqual(42, parameters["testparametera"]);
+            Assert.AreEqual(43, parameters["B"]);
+        }
+
+        /// <summary>
+        /// Tests the Execute method of a ServiceRequest.
+        /// </summary>
+        [Test]
+        public void ExecuteToStreamTest()
+        {
+            var request = new MockServiceRequest<string>(new MockRequestProvider());
+
+            // Request validation is done in the MockSchemaAwareRequestExecutor.
+            Assert.IsNotNull(request.FetchAsStream());
+        }
+
+        /// <summary>
+        /// Tests the Execute method of a ServiceRequest.
+        /// </summary>
+        [Test]
+        public void ExecuteTest()
+        {
+            var request = new MockServiceRequest<string>(new MockRequestProvider());
+
+            // Request validation is done in the MockSchemaAwareRequestExecutor.
+            Assert.AreEqual("FooBar", request.Fetch());
+        }
+
+        /// <summary>
+        /// Tests the ETag behavior of the BuildRequest method of a ServiceRequest.
+        /// </summary>
+        [Test]
+        public void ETagTest()
+        {
+            var request = new MockServiceRequest<string>(new MockRequestProvider());
+            request.ETag = "FooBar";
+            request.ETagAction = ETagAction.IfMatch;
+            
+            // Confirm that the E-tag and the behavior is passed along.
+            MockRequest result = (MockRequest)request.BuildRequest();
+            Assert.AreEqual("FooBar", result.ETag);
+            Assert.AreEqual(ETagAction.IfMatch, result.ETagAction);
+        }
+
+        /// <summary>
+        /// Tests the ETag behavior of the BuildRequest method of a ServiceRequest when using a typed body.
+        /// </summary>
+        [Test]
+        public void ETagTypedBodyTest()
+        {
+            var mockBody = new Mock<IDirectResponseSchema>();
+            mockBody.Setup(b => b.ETag).Returns("FooBar");
+
+            var request = new MockServiceRequest<IDirectResponseSchema>(new MockRequestProvider());
+            request.ETagAction = ETagAction.IfMatch;
+            request.Body = mockBody.Object;
+
+            // Confirm that the E-tag and the behavior is passed along.
+            MockRequest result = (MockRequest)request.BuildRequest();
+            Assert.AreEqual("FooBar", result.ETag);
+            Assert.AreEqual(ETagAction.IfMatch, result.ETagAction);
+            mockBody.Verify();
+        }
+
+        /// <summary>
+        /// Tests the that the FieldsMask will be set on the resulting request.
+        /// </summary>
+        [Test]
+        public void FieldsMaskTest()
+        {
+            var request = new MockServiceRequest<string>(new MockRequestProvider());
+            request.FieldsMask = "FooBar";
+
+            // Confirm that the E-tag and the behavior is passed along.
+            MockRequest result = (MockRequest)request.BuildRequest();
+            Assert.AreEqual("FooBar", result.Fields);
+        }
+
+        /// <summary>
+        /// Tests the that the UserIp will be set on the resulting request.
+        /// </summary>
+        [Test]
+        public void UserIpTest()
+        {
+            var request = new MockServiceRequest<string>(new MockRequestProvider());
+            request.UserIp = "FooBar";
+
+            // Confirm that the E-tag and the behavior is passed along.
+            MockRequest result = (MockRequest)request.BuildRequest();
+            Assert.AreEqual("FooBar", result.UserIp);
+        }
+    }
+}
