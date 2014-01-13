bad01f96e895242eb1122878a0b8a578f21ed336
Eyal Peled <peleyal@google.com>
2014-01-13 14:51:08 -0500

    Issue 436: Add Utility methods to parse DateTime to string and stirng to DateTime https://codereview.appspot.com/46920043/

--- Src/GoogleApis.Core/Apis/Util/Utilities.cs
+++ Src/GoogleApis.Core/Apis/Util/Utilities.cs
@@ -125,5 +125,29 @@ namespace Google.Apis.Util
             }
             return date.ToString("yyyy-MM-dd'T'HH:mm:ss.fffK", DateTimeFormatInfo.InvariantInfo);
         }
+
+        /// <summary>
+        /// Parses the input string and returns <seealso cref="System.DateTime"/> if the input is a valid 
+        /// representation of a date. Otherwise it returns <c>null</c>.
+        /// </summary>
+        public static DateTime? GetDateTimeFromString(string raw)
+        {
+            DateTime result;
+            if (!DateTime.TryParse(raw, out result))
+            {
+                return null;
+            }
+            return result;
+        }
+
+        /// <summary>Returns a string (by RFC3339) form the input <seealso cref="DateTime"/> instance.</summary>
+        public static string GetStringFromDateTime(DateTime? date)
+        {
+            if (!date.HasValue)
+            {
+                return null;
+            }
+            return ConvertToRFC3339(date.Value);
+        }
     }
 }
