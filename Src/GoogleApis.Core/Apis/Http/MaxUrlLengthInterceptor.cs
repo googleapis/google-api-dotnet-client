1a09e5c35e15e9080200a83503091b7cbfc55b41
Eyal Peled <peleyal@google.com>
2014-04-15 13:11:50 -0400

    Issue 455: Translate api doesn't work in POST mode (bug in MaxUrlLengthInterceptor) https://codereview.appspot.com/87170043/

--- Src/GoogleApis.Core/Apis/Http/MaxUrlLengthInterceptor.cs
+++ Src/GoogleApis.Core/Apis/Http/MaxUrlLengthInterceptor.cs
@@ -62,7 +62,7 @@ namespace Google.Apis.Http
                 var requestString = request.RequestUri.ToString();
                 // The new request URI is the old one minus the "?" and everything that follows, since we moved the
                 // query params to the body. For example: "www.example.com/?q=foo" => "www.example.com/".
-                request.RequestUri = new Uri(requestString.Remove(requestString.IndexOf("?") - 1));
+                request.RequestUri = new Uri(requestString.Remove(requestString.IndexOf("?")));
             }
             request.Headers.Add("X-HTTP-Method-Override", "GET");
             return TaskEx.Delay(0);
