29a882d9b950ce53ddf12e691fbc140ce12f048d
Eyal Peled <peleyal@google.com>
2014-05-26 17:59:01 -0400

    Release 1.8.2

--- Src/GoogleApis.Auth/Properties/AssemblyInfo.cs
+++ Src/GoogleApis.Auth/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.8.1.*")]
+[assembly: AssemblyVersion("1.8.2.*")]
