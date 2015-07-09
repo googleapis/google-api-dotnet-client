0743139d67c30b69e05ff65bbda6ce0215dd287c
vsubramani@google.com <vsubramani@google.com>
2015-07-09 14:42:07 -0700

    Moving constants into a region

--- Src/GoogleApis.Auth.DotNet4/OAuth2/ServiceAccountCredential.cs
+++ Src/GoogleApis.Auth.DotNet4/OAuth2/ServiceAccountCredential.cs
@@ -98,6 +98,13 @@ namespace Google.Apis.Auth.OAuth2
             }
         }
 
+        #region Constants
+
+        private const string privateKeyPrefix = "-----BEGIN PRIVATE KEY-----";
+        private const string privateKeySuffix = "-----END PRIVATE KEY-----";
+
+        #endregion
+
         #region Readonly fields
 
         private readonly string id;
@@ -106,9 +113,6 @@ namespace Google.Apis.Auth.OAuth2
         private readonly RSACryptoServiceProvider key;
 
         #endregion
-
-        private const string privateKeyPrefix = "-----BEGIN PRIVATE KEY-----";
-        private const string privateKeySuffix = "-----END PRIVATE KEY-----";
         
         /// <summary>Gets the service account ID (typically an e-mail address).</summary>
         public string Id { get { return id; } }
