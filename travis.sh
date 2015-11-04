5cd8c08851676ecb4b3538e7bd35c2eb523db8ce
Jan Tattermusch <jtattermusch@google.com>
2015-11-04 11:39:19 -0800

    add travis config file and script

---
+++ travis.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Script run by Travis CI
+
+set -e
+
+# grab nunit runners
+nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
+NUNIT="mono ./testrunner/NUnit.Runners.2.6.4/tools/nunit-console.exe"
+
+OUTDIR=bin/Release
+
+# Build ReleaseTravis configuration that skips all windows projects
+# as they are tricky to build under Mono.
+xbuild /p:Configuration=ReleaseTravis GoogleApisClient.sln
+
+$NUNIT Src/GoogleApis.Tests/$OUTDIR/Google.Apis.Tests.dll
+$NUNIT Src/GoogleApis.Auth.Tests/$OUTDIR/Google.Apis.Auth.Tests.dll
+$NUNIT Src/GoogleApis.Apis.DotNet4.Tests/$OUTDIR/Google.Apis.Auth.DotNet4.Tests.dll
