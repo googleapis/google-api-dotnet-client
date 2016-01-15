f79fbf5399129912d8e3aa452d87f645b9915664
Don Benson <dbenson85+git@gmail.com>
2016-01-15 09:28:08 -0500

    Implement enhancement for issue #547
    Added a code receiver that uses a WinForms dialog with an embedded
    browser control. Sample web broker functions have an optional parameter
    to supply the receiver, while maintaining compatibility with current
    behavior, which issues a local server code receiver.

---
+++ Src/GoogleApis.Auth.DotNet4/OAuth2/AuthorizationDialog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2.Responses;
+
+namespace Google.Apis.Auth.OAuth2
+{
+    public partial class AuthorizationDialog : Form
+    {
+        TaskCompletionSource<AuthorizationCodeResponseUrl> tcsAuthorizationCodeResponse;
+
+        public AuthorizationDialog()
+        {
+            InitializeComponent();
+            authWebBrowser.Navigating += authWebBrowser_Navigating;
+            this.FormClosing += AuthorizationDialog_FormClosing;
+        }
+
+        void AuthorizationDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // If the task was not completed, it should be marked as canceled.
+            if (tcsAuthorizationCodeResponse != null && tcsAuthorizationCodeResponse.Task.Result == null)
+            {
+                tcsAuthorizationCodeResponse.SetCanceled();
+            }
+                   
+        }
+
+
+        void authWebBrowser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            // The code or the error should be received by localhost.
+            if (e.Url.Host == "localhost")
+            {
+                var query = e.Url.Query.Substring(1);
+                tcsAuthorizationCodeResponse.SetResult(new AuthorizationCodeResponseUrl(query));
+                // Dialog is no longer neeed.
+                this.Close();
+            }
+        }
+
+        /// <summary>The window launcher that starts browse the browser controller to the given URI.</summary>
+        /// <param name="uri">The authorization code request URI</param>
+        /// <returns>The authorization code response</returns>
+        public Task<AuthorizationCodeResponseUrl> Launch(Uri uri)
+        {
+            tcsAuthorizationCodeResponse = new TaskCompletionSource<AuthorizationCodeResponseUrl>();
+            authWebBrowser.Navigate(uri);
+            this.Show();
+            return tcsAuthorizationCodeResponse.Task;
+        }
+    }
+}
---
+++ Src/GoogleApis.Auth.DotNet4/OAuth2/EmbeddedBrowserCodeReceiver.cs
@@ -0,0 +1,58 @@
+﻿/*
+Copyright 2016 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+using Google.Apis.Auth.OAuth2.Requests;
+using Google.Apis.Auth.OAuth2.Responses;
+
+namespace Google.Apis.Auth.OAuth2
+{
+    /// <summary>
+    /// OAuth 2.0 verification code receiver for Windows Phone that opens an embedded Google account form to enter the
+    /// user's credentials and accepts the application access to its token.
+    /// </summary>
+    public class EmbeddedBrowserCodeReceiver : ICodeReceiver
+    {
+        #region ICodeReceiver Members
+
+        public string RedirectUri
+        {
+            get { return GoogleAuthConsts.LocalhostRedirectUri; }
+        }
+
+        public async Task<AuthorizationCodeResponseUrl> ReceiveCodeAsync(AuthorizationCodeRequestUrl url,
+            CancellationToken taskCancellationToken)
+        {
+            // Create the web authentication user control and add it to the current window.
+            var webAuthDialog = new AuthorizationDialog();
+            try
+            {
+                return await webAuthDialog.Launch(url.Build());
+            }
+            finally
+            {
+                // TODO: Is any cleanup needed? Dialog should be able to close itself.
+            }
+        }
+
+        #endregion
+    }
+}
