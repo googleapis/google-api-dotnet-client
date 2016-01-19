b584f769838e4d97da38475543b523787886ec32
Tony Aiuto <aiuto@google.com>
2016-01-19 14:19:20 -0500

    update setup to specify django 1.8 or less

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
              'googleapis.codegen.script_stubs:RunExpandTemplates')
             ]},
     include_package_data=True,
-    install_requires=['django',
+    install_requires=['django<1.9',
                       'httplib2',
                       'google-apputils',
                       'python-gflags',
