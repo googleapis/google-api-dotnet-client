bc17c063d8173d72d8b32f6bef4494e9ab149c99
Chris Smith <chrsmith@google.com>
2016-01-25 15:51:47 -0800

    Initial stab at a MyGet build file.

---
+++ MyGet.ps1
@@ -0,0 +1,22 @@
+# Performs the build for MyGet and uploads the generated NuGet packages to
+# the "google-dotnet" feed.
+#
+# TODO(chrsmith): Confirm MyGet brings in the necessary submodules. Otherwise:
+# git submodule update --init ClientGenerator
+$msbuild14 = "${Env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
+
+function PrintHeader($message) {
+    Write-Host "****************************************"
+    Write-Host $message
+    Write-Host "****************************************"
+}
+# TODO(chrsmith): Explicitly set VisualStudioVersion here.
+
+# Build the Support libraries.
+# TODO(chrsmith): Run unit tests as applicable.
+PrintHeader("Building SupportLibraries.proj")
+& $msbuild14 /m SupportLibraries.proj
+
+# Build the Generated code (i.e. the specific API clients).
+PrintHeader("Building GeneratedLibraries.proj")
+& $msbuild14 /m GeneratedLibraries.proj
