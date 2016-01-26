e5201adbc7b1785562e958bd44ddb73c442470b1
Chris Smith <chrsmith@google.com>
2016-01-26 09:48:11 -0800

    Fix MSBuild residency problem

--- MyGet.ps1
+++ MyGet.ps1
@@ -16,20 +16,10 @@ function PrintHeader($message) {
 # Build the Support libraries.
 # TODO(chrsmith): Run unit tests as applicable.
 PrintHeader("Building SupportLibraries.proj")
-& $msbuild14 /m SupportLibraries.proj
+& $msbuild14 /m /nr:false SupportLibraries.proj
 
 # Build the Generated code (i.e. the specific API clients).
 PrintHeader("Building GeneratedLibraries.proj")
-& $msbuild14 /m GeneratedLibraries.proj
-
-# Build the Generated code (i.e. the specific API clients).
-PrintHeader("Building GeneratedLibraries.proj")
-# Note: When running multiple times on the same machine this may fail with:
-#   error MSB3231: Unable to remove directory "Src/Generated". Access to the
-#   path 'Microsoft.Bcl.Build.Tasks.dll' is denied.
-# This is because the Clean target tries to delete a file locked by and
-# msbuild process still in memory (but waiting for work). To fix:
-#   taskkill /im msbuild.exe /f
 
 # Install dependent Python libraries.
 pip install django==1.7
@@ -37,4 +27,4 @@ pip install httplib2
 pip install google-apputils
 pip install google-api-python-client
 
-& $msbuild14 /m GeneratedLibraries.proj
+& $msbuild14 /m /nr:false GeneratedLibraries.proj
