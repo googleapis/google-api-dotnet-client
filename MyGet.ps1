de662a637bd57b892692334b02e48d5439bcd3f1
Chris Smith <chrsmith@google.com>
2016-01-27 09:10:29 -0800

    MyGet bug fixed

--- MyGet.ps1
+++ MyGet.ps1
@@ -22,24 +22,6 @@ PrintHeader("Building SupportLibraries.proj")
 PrintHeader("Building GeneratedLibraries.proj")
 
 # Install dependent Python libraries.
-Write-Host "Installing via py"
-py -V
-py -m ensurepip --upgrade
-py -m pip list
-py -m pip install django==1.7
-py -m pip install httplib2
-py -m pip install google-apputils
-py -m pip install google-api-python-client
-py -m pip list
-
-Write-Host "Trying to call pip3 directly."
-C:\Python34\Scripts\pip3.exe install django==1.7
-C:\Python34\Scripts\pip3.exe install httplib2
-C:\Python34\Scripts\pip3.exe install google-apputils
-C:\Python34\Scripts\pip3.exe install google-api-python-client
-C:\Python34\Scripts\pip3.exe list
-
-Write-Host "Installing via python"
 python -V
 python -m ensurepip --upgrade
 python -m pip list
