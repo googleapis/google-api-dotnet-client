b851069d2c1c71b74d88e21db373caeb2fdb28d8
Don Benson <dbenson@trulinx.com>
2016-02-02 13:26:34 -0500

    Minor updates to Authorization Dialog
    Changed ReponseURl to an auto-implemented property and added clarifying
    comments where requested.

--- Src/Support/GoogleApis.Auth.DotNet4/OAuth2/AuthorizationDialog.cs
+++ Src/Support/GoogleApis.Auth.DotNet4/OAuth2/AuthorizationDialog.cs
@@ -23,33 +23,35 @@ namespace Google.Apis.Auth.OAuth2
 {
     public partial class AuthorizationDialog : Form
     {
-        private AuthorizationCodeResponseUrl responseUrl;
+        /// <summary>
+        /// The response from the authorization service when the user clicks Accept or Deny.
+        /// </summary>
+        /// <remarks>Will be null if the user has not clicked Accept or Deny.</remarks>
+        public AuthorizationCodeResponseUrl ResponseUrl { get; protected set; }
 
-        public AuthorizationCodeResponseUrl ResponseUrl
-        {
-            get
-            {
-                return responseUrl;
-            }
-        }
-
-        /// <summary>Constructs a new authentication broker dialog.</summary>
+        /// <summary>Constructs a new authentication broker dialog and navigates to
+        /// the authorization page. From there, the user has control to choose Accept or Deny
+        /// via the dialog's embedded web browser.</summary>
         public AuthorizationDialog(Uri authUri)
         {
             InitializeComponent();
             // Set the browser to the initial uri.
             authWebBrowser.Navigate(authUri);
             // Need to track when the results has been received.
+            // The event is fired whenever the browser finishes a web request.
+            // When the user clicks Accept or Deny, the redirected response will
+            // cause this event to fire and the URL will contain the authorization resposne.
             authWebBrowser.DocumentCompleted += authWebBrowser_DocumentCompleted;
         }
 
         private void authWebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
+            // It is assumed this dialog is being used by an installed application.
             // The code or the error should be received by localhost.
             if (e.Url.Host == "localhost")
             {
                 var query = e.Url.Query.Substring(1);
-                responseUrl = new AuthorizationCodeResponseUrl(query);
+                ResponseUrl = new AuthorizationCodeResponseUrl(query);
                 // Dialog is no longer neeed.
                 this.Close();
             }
