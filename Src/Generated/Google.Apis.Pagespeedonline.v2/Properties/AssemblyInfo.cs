7c23a686c26bcfd3c5b4d2225ad635ba1887f2d9
Matthew Riley <mattdr@google.com>
2016-03-22 15:19:58 -0700

    Update discovery documents and generated code
    automatically_generated_update

--- Src/Generated/Google.Apis.Pagespeedonline.v2/Properties/AssemblyInfo.cs
+++ Src/Generated/Google.Apis.Pagespeedonline.v2/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Google Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.11.0.430")]
+[assembly: AssemblyVersion("1.11.1.430")]
