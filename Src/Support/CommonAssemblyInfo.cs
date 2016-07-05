37bcf9b47f4b8f8d887ca21f6247d3f6d6c0449a
Google C# Builder <nobody+buildbot@google.com>
2016-07-05 09:18:45 +0000

    Updated version to 1.14.0 for support and generated libraries

--- Src/Support/CommonAssemblyInfo.cs
+++ Src/Support/CommonAssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("Google Inc")]
 [assembly: AssemblyCopyright("Copyright 2016 Google Inc")]
 
-[assembly: AssemblyVersion("1.13.1.0")]
+[assembly: AssemblyVersion("1.14.0.0")]
 
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
