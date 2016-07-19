8735173cbdb1d265fe14cb44699de0a2a4d65ef2
Ivan Naranjo <ivann@google.com>
2016-07-19 15:37:37 +0000

    Updating the test runner.

--- run_tests_dotnetcore.bat
+++ run_tests_dotnetcore.bat
@@ -7,18 +7,20 @@ REM Build the support libraries.
 msbuild SupportLibraries.proj
 
 REM Clean slate after the build.
-del /s/q %USERPROFILE%\.nuget
-set workspace=%~p0
+set workspace=%~dp0
 set fallback=%workspace%\NuPkgs
+set packages=%workspace%\packages
 
 REM Restore the test projects.
 dotnet restore ^
-  "Src\Support\GoogleApis.Tests" ^
-  "Src\Support\GoogleApis.Auth.Tests" ^
-  --no-cache -f "%fallback%"
+  "Src\Support\GoogleApis.Tests_dotnetcore" ^
+  "Src\Support\GoogleApis.Auth.Tests_dotnetcore" ^
+  --packages "%packages%" ^
+  -f "%fallback%" ^
+  --no-cache
 
 REM Run the tests.
-dotnet test "Src\Support\GoogleApis.Tests"
-dotnet test "Src\Support\GoogleApis.Auth.Tests"
+dotnet test "Src\Support\GoogleApis.Tests_dotnetcore"
+dotnet test "Src\Support\GoogleApis.Auth.Tests_dotnetcore"
 
 echo Done.
