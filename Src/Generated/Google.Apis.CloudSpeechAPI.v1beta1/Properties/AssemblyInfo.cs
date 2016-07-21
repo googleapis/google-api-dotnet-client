5b849273fd088fde99f13620b9176970957d36f4
cloudsharp <cloudsharp>
2016-07-21 08:11:51 +0000

    Update discovery documents and generated code
    automatically_generated_update

--- Src/Generated/Google.Apis.CloudSpeechAPI.v1beta1/Properties/AssemblyInfo.cs
+++ Src/Generated/Google.Apis.CloudSpeechAPI.v1beta1/Properties/AssemblyInfo.cs
@@ -22,12 +22,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Google.Apis.CloudMonitoring.v2beta2")]
+[assembly: AssemblyTitle("Google.Apis.CloudSpeechAPI.v1beta1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Google.Apis.CloudMonitoring.v2beta2")]
+[assembly: AssemblyProduct("Google.Apis.CloudSpeechAPI.v1beta1")]
 [assembly: AssemblyCompany("Google Inc.")]
 [assembly: AssemblyCopyright("Copyright © Google Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.14.0.564")]
+[assembly: AssemblyVersion("1.14.0.566")]
