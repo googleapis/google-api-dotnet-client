f8c00b0679ff7814d9acc4e9a8e029835a510a15
Ivan Naranjo <ivann@google.com>
2016-08-03 14:15:40 +0100

    Running the .NET Core tests after the build.

--- travis.sh
+++ travis.sh
@@ -3,10 +3,6 @@
 
 set -e
 
-# Run the .NET Core tests.
-dotnet test "Src/Support/GoogleApis.Tests_dotnetcore"
-dotnet test "Src/Support/GoogleApis.Auth.Tests_dotnetcore"
-
 # grab nunit runners
 nuget install NUnit.ConsoleRunner -Version 3.2.1 -OutputDirectory testrunner
 NUNIT="${PWD}/testrunner/NUnit.ConsoleRunner.3.2.1/tools/nunit3-console.exe"
@@ -23,3 +19,7 @@ mono "${NUNIT}" "--where" "cat != IgnoreOnTravis" \
     "GoogleApis.Tests/${OUTDIR}/Google.Apis.Tests.exe" \
     "GoogleApis.Auth.Tests/${OUTDIR}/Google.Apis.Auth.Tests.exe" \
     "GoogleApis.Auth.DotNet4.Tests/${OUTDIR}/Google.Apis.Auth.DotNet4.Tests.exe"
+
+# Run the .NET Core tests.
+dotnet test "GoogleApis.Tests_dotnetcore"
+dotnet test "GoogleApis.Auth.Tests_dotnetcore"
