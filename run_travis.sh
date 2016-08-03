4e8bf43ce406ffd94d6295905bd49cb3dc3fadfd
Ivan Naranjo <ivann@google.com>
2016-08-03 14:31:44 +0100

    Don't get the nuget packages.

--- run_travis.sh
+++ run_travis.sh
@@ -1,14 +1,9 @@
 #! /usr/bin/env bash
 # Runs the travis tests on the test container.
-#   $1, the path to the directory with the packages.
 
-set -eu
-
-readonly test_runner=test_runner:latest
-readonly workspace=$(dirname $0)
+set -e
 
 docker run \
     -it \
     -v "${PWD}/":/var/repo:ro \
-    -v "$1":/var/nuget:ro \
-    ${test_runner}
+    test_runner:latest
