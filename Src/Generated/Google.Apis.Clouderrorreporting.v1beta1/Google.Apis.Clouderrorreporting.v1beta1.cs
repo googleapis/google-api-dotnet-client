04abc3246d7f33ff7512da062ad7e323f10bb9c0
cloudsharp <cloudsharp>
2016-09-15 08:27:56 +0000

    Update discovery documents and generated code
    automatically_generated_update

--- Src/Generated/Google.Apis.Clouderrorreporting.v1beta1/Google.Apis.Clouderrorreporting.v1beta1.cs
+++ Src/Generated/Google.Apis.Clouderrorreporting.v1beta1/Google.Apis.Clouderrorreporting.v1beta1.cs
@@ -26,7 +26,7 @@
  *      <tr><th>API
  *          <td><a href='https://cloud.google.com/error-reporting/'>Stackdriver Error Reporting API</a>
  *      <tr><th>API Version<td>v1beta1
- *      <tr><th>API Rev<td>20160720 (566)
+ *      <tr><th>API Rev<td>20160822 (599)
  *      <tr><th>API Docs
  *          <td><a href='https://cloud.google.com/error-reporting/'>
  *              https://cloud.google.com/error-reporting/</a>
