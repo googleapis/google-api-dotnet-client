0dddffc17498e625d7e13c0fc23dcaffb03df9ad
Chris Bacon <chrisbacon@google.com>
2016-10-14 11:39:57 +0100

    Patch directory and games discovery-docs
    Updates #757 and #682.

---
+++ Src/Tools/DiscoveryDocPatcher/Patcher.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoveryDocPatcher
+{
+    class Patcher
+    {
+        public static Patcher Load(string path)
+        {
+            return new Patcher(path, JObject.Parse(File.ReadAllText(path)));
+        }
+
+        private Patcher(string path, JObject json)
+        {
+            _path = path;
+            _json = json;
+        }
+
+        private readonly string _path;
+        private readonly JObject _json;
+        private bool changed = false;
+
+        public void SaveWithBackup()
+        {
+            if (changed)
+            {
+                File.Move(_path, _path + ".original");
+                string json = _json.ToString();
+                File.WriteAllText(_path, json);
+            }
+        }
+
+        public void Remove(string jPath, JToken oldToken, JToken replacedToken)
+        {
+            var token = _json.SelectToken(jPath);
+            if (!JToken.DeepEquals(token, oldToken))
+            {
+                if (JToken.DeepEquals(token, replacedToken))
+                {
+                    // Already replaced, so do nothing.
+                    return;
+                }
+                throw new InvalidOperationException("Content wrong");
+            }
+            changed = true;
+            token.Remove();
+        }
+
+        public void Remove(string jPath, string oldContent, string replacedContent)
+        {
+            JToken oldToken = JToken.Parse(oldContent.Replace('\'', '"'));
+            JToken replacedToken = JToken.Parse(replacedContent.Replace('\'', '"'));
+            Remove(jPath, oldToken, replacedToken);
+        }
+
+        public void Replace(string jPath, JToken oldToken, JToken newToken)
+        {
+            var token = _json.SelectToken(jPath);
+            if (!JToken.DeepEquals(token, oldToken))
+            {
+                if (JToken.DeepEquals(token, newToken))
+                {
+                    // Already replaced, so do nothing.
+                    return;
+                }
+                throw new InvalidOperationException("Content wrong");
+            }
+            changed = true;
+            token.Replace(newToken);
+        }
+
+        public void Replace(string jPath, string oldContent, string newContent)
+        {
+            JToken oldToken = JToken.Parse(oldContent.Replace('\'', '"'));
+            JToken newToken = JToken.Parse(newContent.Replace('\'', '"'));
+            Replace(jPath, oldToken, newToken);
+        }
+    }
+}
---
+++ Src/Tools/DiscoveryDocPatcher/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoveryDocPatcher
+{
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                string discoveryDocPath = args[0];
+                PatchGames(discoveryDocPath);
+                PatchDirectory(discoveryDocPath);
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return 1;
+            }
+        }
+
+        static void PatchGames(string rootPath)
+        {
+            var patcher = Patcher.Load(Path.Combine(rootPath, "games_v1.json"));
+            // Remove deprecated enum value that causes duplicate C# enum value.
+            patcher.Remove("resources.players.methods.list.parameters.collection.enum[1]", "'playedWith'", "'played_with'");
+            patcher.Remove("resources.players.methods.list.parameters.collection.enumDescriptions[1]",
+                "'(DEPRECATED: please use played_with!) Retrieve a list of players you have played a multiplayer game (realtime or turn-based) with recently.'",
+                "'Retrieve a list of players you have played a multiplayer game (realtime or turn-based) with recently.'");
+            patcher.SaveWithBackup();
+        }
+
+        static void PatchDirectory(string rootPath)
+        {
+            var patcher = Patcher.Load(Path.Combine(rootPath, "admin_directory_v1.json"));
+            // Strongly-type properties that are defined without typing.
+            patcher.Replace("schemas.User.properties.addresses", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserAddress' } }");
+            patcher.Replace("schemas.User.properties.emails", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserEmail' } }");
+            patcher.Replace("schemas.User.properties.externalIds", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserExternalId' } }");
+            patcher.Replace("schemas.User.properties.ims", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserIm' } }");
+            patcher.Replace("schemas.User.properties.organizations", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserOrganization' } }");
+            patcher.Replace("schemas.User.properties.phones", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserPhone' } }");
+            patcher.Replace("schemas.User.properties.relations", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserRelation' } }");
+            patcher.Replace("schemas.User.properties.websites", "{ 'type': 'any' }", "{ 'type': 'array', 'items': { '$ref': 'UserWebsite' } }");
+            patcher.Replace("schemas.Aliases.properties.aliases.items", "{ 'type': 'any' }", "{ '$ref': 'Alias' }");
+            patcher.SaveWithBackup();
+
+        }
+    }
+}
