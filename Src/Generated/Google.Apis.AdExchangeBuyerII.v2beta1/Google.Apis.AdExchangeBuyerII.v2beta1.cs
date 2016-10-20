1fb9c361f3230edc13339e887ec8a5b6c2ec10e0
cloudsharp <cloudsharp>
2016-10-20 18:29:47 +0000

    Update discovery documents and generated code
    automatically_generated_update

--- Src/Generated/Google.Apis.AdExchangeBuyerII.v2beta1/Google.Apis.AdExchangeBuyerII.v2beta1.cs
+++ Src/Generated/Google.Apis.AdExchangeBuyerII.v2beta1/Google.Apis.AdExchangeBuyerII.v2beta1.cs
@@ -26,7 +26,7 @@
  *      <tr><th>API
  *          <td><a href='https://developers.google.com/ad-exchange/buyer-rest/guides/client-access/'>Ad Exchange Buyer API II</a>
  *      <tr><th>API Version<td>v2beta1
- *      <tr><th>API Rev<td>20161018 (656)
+ *      <tr><th>API Rev<td>20161019 (657)
  *      <tr><th>API Docs
  *          <td><a href='https://developers.google.com/ad-exchange/buyer-rest/guides/client-access/'>
  *              https://developers.google.com/ad-exchange/buyer-rest/guides/client-access/</a>
--- Src/Generated/Google.Apis.ServiceControl.v1/Google.Apis.ServiceControl.v1.cs
+++ Src/Generated/Google.Apis.ServiceControl.v1/Google.Apis.ServiceControl.v1.cs
@@ -26,7 +26,7 @@
  *      <tr><th>API
  *          <td><a href='https://cloud.google.com/service-control/'>Google Service Control API</a>
  *      <tr><th>API Version<td>v1
- *      <tr><th>API Rev<td>20161010 (648)
+ *      <tr><th>API Rev<td>20161017 (655)
  *      <tr><th>API Docs
  *          <td><a href='https://cloud.google.com/service-control/'>
  *              https://cloud.google.com/service-control/</a>
