aa7cb664af072b7944540f1e4d2b2d6ce4b9f60c
cloudsharp <cloudsharp>
2016-11-22 08:27:07 +0000

    Update discovery documents and generated code
    automatically_generated_update

--- Src/Generated/Google.Apis.Calendar.v3/Google.Apis.Calendar.v3.cs
+++ Src/Generated/Google.Apis.Calendar.v3/Google.Apis.Calendar.v3.cs
@@ -26,7 +26,7 @@
  *      <tr><th>API
  *          <td><a href='https://developers.google.com/google-apps/calendar/firstapp'>Calendar API</a>
  *      <tr><th>API Version<td>v3
- *      <tr><th>API Rev<td>20161108 (677)
+ *      <tr><th>API Rev<td>20161115 (684)
  *      <tr><th>API Docs
  *          <td><a href='https://developers.google.com/google-apps/calendar/firstapp'>
  *              https://developers.google.com/google-apps/calendar/firstapp</a>
