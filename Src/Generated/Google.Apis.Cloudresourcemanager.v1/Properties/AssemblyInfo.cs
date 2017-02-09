9e00f4cae5664c05dadbaa96e19973dc7dcea7ce
cloudsharp <cloudsharp>
2017-02-09 12:46:21 +0000

    Update discovery documents and generated code
    automatically_generated_update

--- Src/Generated/Google.Apis.Cloudresourcemanager.v1/Properties/AssemblyInfo.cs
+++ Src/Generated/Google.Apis.Cloudresourcemanager.v1/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Google Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.20.0.762")]
+[assembly: AssemblyVersion("1.21.0.767")]
