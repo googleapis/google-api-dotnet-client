22b389b2926b6b4bd5ea35953b7cf63cebb20918
Chris Bacon <chrisbacon@google.com>
2017-05-16 19:03:35 +0000

    Address review comments, and fix CI

--- BuildGenerated.sh
+++ BuildGenerated.sh
@@ -36,41 +36,39 @@ while [[ $# -gt 0 ]]; do
       SKIPDOWNLOAD=TRUE
       SKIPGENERATE=TRUE
       SKIPBUILD=TRUE
-	  SKIPPACK=TRUE
+      SKIPPACK=TRUE
       ;;
     --onlydownload)
       SKIPGENERATE=TRUE
       SKIPBUILD=TRUE
-	  SKIPPACK=TRUE
+      SKIPPACK=TRUE
       ;;
     --onlygenerate)
       SKIPDOWNLOAD=TRUE
       SKIPBUILD=TRUE
-	  SKIPPACK=TRUE
+      SKIPPACK=TRUE
       ;;
     --onlybuild)
       SKIPDOWNLOAD=TRUE
       SKIPGENERATE=TRUE
-	  SKIPPACK=TRUE
+      SKIPPACK=TRUE
       ;;
     --onlypack)
       SKIPDOWNLOAD=TRUE
       SKIPGENERATE=TRUE
-	  SKIPBUILD=TRUE
+      SKIPBUILD=TRUE
       ;;
     *)
       echo ERROR: Invalid argument to BuildGenerated.sh: \'$key\'
-	  exit 1
+      exit 1
   esac
   shift
 done
 
 if [ -z ${SKIPDOWNLOAD+x} ]; then
-  # Delete all discovery docs. Ignore failure if this dir doesn't exist.
-  if [ -d $DISCOVERY_DOC_DIR ]; then
-    echo Deleting existing \'$DISCOVERY_DOC_DIR\' directory...
-    rm -r $DISCOVERY_DOC_DIR
-  fi
+  # Delete all discovery docs
+  echo Deleting existing \'$DISCOVERY_DOC_DIR\' directory...
+  rm -rf $DISCOVERY_DOC_DIR
   # Download all discovery docs
   python -u get_discovery_documents.py --destination_dir $DISCOVERY_DOC_DIR
   # Patch discovery docs
@@ -79,16 +77,14 @@ if [ -z ${SKIPDOWNLOAD+x} ]; then
 fi
 
 if [ -z ${SKIPGENERATE+x} ]; then
-  # Delete all generated code. Ignore failure if this dir doesn't exist.
-  if [ -d $CODE_GENERATION_DIR ]; then
-    echo Deleting existing \'$CODE_GENERATION_DIR\' directory...
-    rm -r $CODE_GENERATION_DIR
-  fi
+  # Delete all generated code
+  echo Deleting existing \'$CODE_GENERATION_DIR\' directory...
+  rm -rf $CODE_GENERATION_DIR
   # Generate API projects from discovery docs
   export PYTHONPATH=$(pwd)/ClientGenerator/src
   for jsonfile in $DISCOVERY_DOC_DIR/*.json; do
     IFS='/'; names=($jsonfile); unset IFS
-	name=${names[-1]}
+    name=${names[-1]}
     case $name in
       identitytoolkit_v3.json|oauth2_v1.json)
         echo Ignoring: \'$name\'
@@ -102,35 +98,16 @@ if [ -z ${SKIPGENERATE+x} ]; then
 fi
 
 if [ -z ${SKIPBUILD+x} ]; then
-  # dotnet restore then build over all generated projects
-  # Don't restore in parallel, 'dotnet restore' doesn't support concurrent restores.
-  for csproj in $CODE_GENERATION_DIR/*/*.csproj; do
-    dotnet restore $csproj
-  done
-  # Build in batches. This isn't the most efficient way of building concurrently, but it's fairly simple.
-  # Each pid must be waited for individually to ensure errors stop this script.
-  declare -r -i CONCURRENCY=4
-  declare -a pids
-  for csproj in $CODE_GENERATION_DIR/*/*.csproj; do
-    if (( ${#pids[@]} >= $CONCURRENCY )); then
-      wait ${pids[0]}
-      unset pids[0]
-      pids=( "${pids[@]}" )
-    fi
-    echo Building: $csproj
-    dotnet build $csproj --configuration $BUILD_CONFIGURATION &
-    pid=$!
-    pids+=($pid)
-  done
-  for pid in "${pids[@]}"; do
-    echo Waiting for pid: $pid
-    wait $pid
-  done
+  rm -f Generated.sln
+  dotnet new sln --name Generated
+  dotnet sln Generated.sln add $CODE_GENERATION_DIR/*/*.csproj
+  dotnet restore Generated.sln
+  dotnet build Generated.sln --configuration $BUILD_CONFIGURATION
 fi
 
 if [ -z ${SKIPPACK+x} ]; then
-  # dotnet restore then build over all generated projects
-  for csproj in $CODE_GENERATION_DIR/*/*.csproj; do
-    dotnet pack $csproj --configuration $BUILD_CONFIGURATION --no-build --output $NUPKG_DIR --include-symbols --include-source
-  done
+  # Delete all generated nupkgs
+  echo Deleting existing \'$NUPKG_DIR\' directory...
+  rm -rf $NUPKG_DIR
+  dotnet pack Generated.sln --configuration $BUILD_CONFIGURATION --no-build --output $NUPKG_DIR --include-symbols --include-source
 fi
