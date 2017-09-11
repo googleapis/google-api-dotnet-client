dde137c741b7bc47a7bc0d9485d176e02e6a6736
Chris Bacon <chris@dunelm.org.uk>
2017-09-12 00:04:01 +0100

    Tidy up

--- Src/Support/Google.Apis/Util/Store/MemoryDataStore.cs
+++ Src/Support/Google.Apis/Util/Store/MemoryDataStore.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+Copyright 2017 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +27,7 @@ namespace Google.Apis.Util.Store
     /// </summary>
     public class MemoryDataStore : IDataStore
     {
-        private static readonly Task s_completedTask = CompletedTask<int>();
-
-        private static Task<T> CompletedTask<T>()
-        {
-            var tcs = new TaskCompletionSource<T>();
-            tcs.SetResult(default(T));
-            return tcs.Task;
-        }
+        private static readonly Task s_completedTask = Task.FromResult(0);
 
         internal static string GenerateStoredKey(string key, Type t) => string.Format("{0}-{1}", t.FullName, key);
 
