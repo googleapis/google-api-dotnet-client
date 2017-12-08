2a8b6233f99e647dcc16b3081d931153f7b21954
Jon Skeet <jonskeet@google.com>
2017-12-08 11:11:42 +0000

    Allow stream interception before decompression We now have a number of handlers to build a pipeline from:
    - Decompression
    - Interception
    - Choice between two other handlers to delegate to
    
    These are constructed appropriately in HttpClientFactory.
    
    Currently we don't support interception other than in MediaDownloader. We could potentially
    add an AddResponseStreamInterceptorProvider method in ConfigurableMessageHandler, but
    that has two difficulties:
    
    - If the MediaDownloader has already specified an interceptor, we'd need to make sure they
      work well together
    - We'd either need to limit it to a single interceptor, or add code to handle multiple interceptors.
    
    It's all feasible, but it's more work for a feature we don't know we need yet.

--- Src/Support/Google.Apis.Core/Http/HttpClientFactory.cs
+++ Src/Support/Google.Apis.Core/Http/HttpClientFactory.cs
@@ -14,13 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-
 using Google.Apis.Logging;
+using System.Net;
+using System.Net.Http;
 
 namespace Google.Apis.Http
 {
@@ -53,25 +49,55 @@ namespace Google.Apis.Http
         /// <summary>Creates a HTTP message handler. Override this method to mock a message handler.</summary>
         protected virtual HttpMessageHandler CreateHandler(CreateHttpClientArgs args)
         {
-            var handler = new HttpClientHandler();
+            // We need to handle three situations in order to intercept uncompressed data where necessary
+            // while using the built-in decompression where possible.
+            // - No compression requested
+            // - Compression requested but not supported by HttpClientHandler (easy; just GzipDeflateHandler on top of an interceptor on top of HttpClientHandler)
+            // - Compression requested and HttpClientHandler (complex: create two different handlers and decide which to use on a per-request basis)
 
-            // If the framework supports redirect configuration, set it to false, because ConfigurableMessageHandler 
-            // handles redirect.
+            var clientHandler = CreateSimpleClientHandler();
+
+            if (!args.GZipEnabled)
+            {
+                // Simple: nothing will be decompressing content, so we can just intercept the original handler.
+                return new StreamInterceptionHandler(clientHandler);
+            }
+            else if (!clientHandler.SupportsAutomaticDecompression)
+            {
+                // Simple: we have to create our own decompression handler anyway, so there's still just a single chain.
+                var interceptionHandler = new StreamInterceptionHandler(clientHandler);
+                return new GzipDeflateHandler(interceptionHandler);
+            }
+            else
+            {
+                // Complex: we want to use a simple handler with no interception but with built-in decompression
+                // for requests that wouldn't perform interception anyway, and a longer chain for interception cases.
+                clientHandler.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+                return new TwoWayDelegatingHandler(
+                    // Normal handler (with built-in decompression) when there's no interception.
+                    clientHandler,
+                    // Alternative handler for requests that might be intercepted, and need that interception to happen
+                    // before decompression. We need to delegate to a new client handler that *doesn't* 
+                    new GzipDeflateHandler(new StreamInterceptionHandler(CreateSimpleClientHandler())),
+                    request => StreamInterceptionHandler.GetInterceptorProvider(request) != null);
+            }
+        }
+
+        /// <summary>
+        /// Creates a simple client handler with redirection and compression disabled.
+        /// </summary>
+        private HttpClientHandler CreateSimpleClientHandler()
+        {
+            var handler = new HttpClientHandler();
             if (handler.SupportsRedirectConfiguration)
             {
                 handler.AllowAutoRedirect = false;
             }
-
-            // If the framework supports automatic decompression and GZip is enabled, set automatic decompression.
-            if (handler.SupportsAutomaticDecompression && args.GZipEnabled)
+            if (handler.SupportsAutomaticDecompression)
             {
-                handler.AutomaticDecompression = System.Net.DecompressionMethods.GZip |
-                    System.Net.DecompressionMethods.Deflate;
+                handler.AutomaticDecompression = DecompressionMethods.None;
             }
-
-            Logger.Debug("Handler was created. SupportsRedirectConfiguration={0}, SupportsAutomaticDecompression={1}",
-                handler.SupportsRedirectConfiguration, handler.SupportsAutomaticDecompression);
-
             return handler;
         }
     }
---
+++ Src/Support/Google.Apis.Core/Http/TwoWayDelegatingHandler.cs
@@ -0,0 +1,73 @@
+﻿/*
+Copyright 2017 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Apis.Http
+{
+    /// <summary>
+    /// An HttpMessageHandler that delegates to one of two inner handlers based on a condition
+    /// checked on each request.
+    /// </summary>
+    internal sealed class TwoWayDelegatingHandler : DelegatingHandler
+    {
+        private readonly AccessibleDelegatingHandler _alternativeHandler;
+        private readonly Func<HttpRequestMessage, bool> _useAlternative;
+        private bool disposed = false;
+
+        internal TwoWayDelegatingHandler(HttpMessageHandler normalHandler, HttpMessageHandler alternativeHandler, Func<HttpRequestMessage, bool> useAlternative)
+            : base(normalHandler)
+        {
+            _alternativeHandler = new AccessibleDelegatingHandler(alternativeHandler);
+            _useAlternative = useAlternative;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) =>
+            _useAlternative(request) ? _alternativeHandler.InternalSendAsync(request, cancellationToken) : base.SendAsync(request, cancellationToken);
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !disposed)
+            {
+                disposed = true;
+                try
+                {
+                    base.Dispose();
+                }
+                finally
+                {
+                    _alternativeHandler.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler to wrap another, just so that we can effectively expose its SendAsync method.
+        /// </summary>
+        private sealed class AccessibleDelegatingHandler : DelegatingHandler
+        {
+            internal AccessibleDelegatingHandler(HttpMessageHandler innerHandler) : base(innerHandler)
+            {
+            }
+
+            internal Task<HttpResponseMessage> InternalSendAsync(HttpRequestMessage request, CancellationToken cancellationToken) =>
+                SendAsync(request, cancellationToken);
+        }
+    }
+}
