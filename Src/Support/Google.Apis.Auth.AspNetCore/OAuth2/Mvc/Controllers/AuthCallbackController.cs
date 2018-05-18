016f632e4c054238a3d4d901c2f8a9d840ae5bc9
Andy Miller <andy@andyfmiller.com>
2018-05-17 22:05:44 -0700

    Resolve conflicts.

--- Src/Support/Google.Apis.Auth.AspNetCore/OAuth2/Mvc/Controllers/AuthCallbackController.cs
+++ Src/Support/Google.Apis.Auth.AspNetCore/OAuth2/Mvc/Controllers/AuthCallbackController.cs
@@ -1,32 +1,15 @@
-/*
-Copyright 2013 Google Inc
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web.Mvc;
-
-using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.AspNetCore.Mvc.Filters;
 using Google.Apis.Auth.OAuth2.Flows;
-using Google.Apis.Auth.OAuth2.Mvc.Filters;
 using Google.Apis.Auth.OAuth2.Responses;
 using Google.Apis.Auth.OAuth2.Web;
 using Google.Apis.Logging;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Google.Apis.Auth.OAuth2.Mvc.Controllers
+namespace Google.Apis.Auth.OAuth2.AspNetCore.Mvc.Controllers
 {
     /// <summary>
     /// Auth callback to process the authorization code or error response from the authorization redirect page.
@@ -63,18 +46,13 @@ namespace Google.Apis.Auth.OAuth2.Mvc.Controllers
         /// The authorization callback which receives an authorization code which contains an error or a code.
         /// If a code is available the method exchange the coed with an access token and redirect back to the original
         /// page which initialized the auth process (using the state parameter).
-        /// <para>
-        /// The current timeout is set to 10 seconds. You can change the default behavior by setting 
-        /// <see cref="System.Web.Mvc.AsyncTimeoutAttribute"/> with a different value on your controller.
-        /// </para>
         /// </summary>
         /// <param name="authorizationCode">Authorization code response which contains the code or an error.</param>
         /// <param name="taskCancellationToken">Cancellation token to cancel operation.</param>
         /// <returns>
         /// Redirect action to the state parameter or <see cref="OnTokenError"/> in case of an error.
         /// </returns>
-        [AsyncTimeout(10000)]
-        public async virtual Task<ActionResult> IndexAsync(AuthorizationCodeResponseUrl authorizationCode,
+        public virtual async Task<ActionResult> IndexAsync(AuthorizationCodeResponseUrl authorizationCode,
             CancellationToken taskCancellationToken)
         {
             if (string.IsNullOrEmpty(authorizationCode.Code))
@@ -87,20 +65,23 @@ namespace Google.Apis.Auth.OAuth2.Mvc.Controllers
 
             Logger.Debug("Received \"{0}\" code", authorizationCode.Code);
 
-            var returnUrl = Request.Url.ToString();
-            returnUrl = returnUrl.Substring(0, returnUrl.IndexOf("?"));
-            
+            var returnUrl = Request.GetDisplayUrl();
+            returnUrl = returnUrl.Substring(0, returnUrl.IndexOf("?", StringComparison.Ordinal));
+
             // If include granted scopes is specified, then pass the current scopes
             // to ExchangeCodeForTokenAsync.
             string[] grantedScopes = null;
             if (Flow.IncludeGrantedScopes.HasValue && Flow.IncludeGrantedScopes.Value)
             {
                 var token = await Flow.LoadTokenAsync(UserId, taskCancellationToken).ConfigureAwait(false);
-                grantedScopes = token.Scope?.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                if (token != null)
+                {
+                    grantedScopes = token.Scope?.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                }
             }
 
-            await Flow.ExchangeCodeForTokenAsync(UserId, authorizationCode.Code, grantedScopes, 
-                returnUrl, taskCancellationToken).ConfigureAwait(false);
+            await Flow.ExchangeCodeForTokenAsync(UserId, authorizationCode.Code, grantedScopes, returnUrl,
+                taskCancellationToken).ConfigureAwait(false);
 
             // Extract the right state.
             var oauthState = await AuthWebUtility.ExtracRedirectFromState(Flow.DataStore, UserId,
