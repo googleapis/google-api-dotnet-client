7fe1a2307583e00ca866fb4254ea1c1506fde4e7
Chris Bacon <chris@dunelm.org.uk>
2018-10-16 11:08:16 +0100

    Remove download tests for netcore1.x; they are too slow on Travis

--- Src/Support/Google.Apis.Tests/Apis/Download/MediaDownloaderTest.cs
+++ Src/Support/Google.Apis.Tests/Apis/Download/MediaDownloaderTest.cs
@@ -14,6 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Don't run these tests on netcoreapp1.0 or netcoreapp1.1
+// They are too slow.
+#if !NETCOREAPP1_0 && !NETCOREAPP1_1
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -585,3 +589,5 @@ namespace Google.Apis.Tests.Apis.Download
         }
     }
 }
+
+#endif
