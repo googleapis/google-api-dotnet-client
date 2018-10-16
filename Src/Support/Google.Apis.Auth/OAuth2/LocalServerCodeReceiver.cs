cf16ca25ce7f5ed7b3a9f8b0b71cff956c44d764
Chris Bacon <chris@dunelm.org.uk>
2018-10-16 10:07:47 +0100

    Increase local server header and line-length limits
    Fixes #1301

--- Src/Support/Google.Apis.Auth/OAuth2/LocalServerCodeReceiver.cs
+++ Src/Support/Google.Apis.Auth/OAuth2/LocalServerCodeReceiver.cs
@@ -141,8 +141,10 @@ namespace Google.Apis.Auth.OAuth2
         /// </summary>
         internal class LimitedLocalhostHttpServer : IDisposable
         {
-            private const int MaxRequestLineLength = 256;
-            private const int MaxHeadersLength = 8192;
+            // RFC7230 recommends supporting a request-line length of at least 8,000 octets
+            // https://tools.ietf.org/html/rfc7230#section-3.1.1
+            private const int MaxRequestLineLength = 16 * 1024;
+            private const int MaxHeadersLength = 64 * 1024;
             private const int NetworkReadBufferSize = 1024;
 
             private static ILogger Logger = ApplicationContext.Logger.ForType<LimitedLocalhostHttpServer>();
