a86045920020e54a896d1af754e3d6a48b0082fb
arithmetic1728 <liusijun1985@gmail.com>
2021-05-11 09:50:10 -0700

    update ImpersonationOptions.cs

--- Src/Support/Google.Apis.Auth/OAuth2/ImpersonationOptions.cs
+++ Src/Support/Google.Apis.Auth/OAuth2/ImpersonationOptions.cs
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using Google.Apis.Util;
 using System;
 using System.Collections.Generic;
 
