4941fe92053d1ca1e65bf4b745193564795b86be
Jon Skeet <jonskeet@google.com>
2022-07-13 10:50:03 +0100

    chore: Update docfx to 2.59.2
    I suspect that installing .NET 6 on the CI machine caused the
    previous version of docfx to fail. (It's always been somewhat picky
    about SDK versions.)

--- DocfxFunctions.sh
+++ DocfxFunctions.sh
@@ -3,7 +3,7 @@
 
 declare -r REPO_ROOT=$(readlink -f $(dirname ${BASH_SOURCE}))
 declare -r TOOL_PACKAGES=$REPO_ROOT/packages
-declare -r DOCFX_VERSION=2.56.6
+declare -r DOCFX_VERSION=2.59.2
 declare -r DOCFX=$TOOL_PACKAGES/docfx.$DOCFX_VERSION/docfx.exe
 
 # Appears to fix some issues in docfx.
