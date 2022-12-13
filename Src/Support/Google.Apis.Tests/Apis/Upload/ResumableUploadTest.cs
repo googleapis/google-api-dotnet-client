ffc6669f2a8332ccd23260af53f39368db9b0abe
Jon Skeet <jonskeet@google.com>
2022-12-13 07:57:48 +0000

    Try fewer again

--- Src/Support/Google.Apis.Tests/Apis/Upload/ResumableUploadTest.cs
+++ Src/Support/Google.Apis.Tests/Apis/Upload/ResumableUploadTest.cs
@@ -349,7 +349,7 @@ namespace Google.Apis.Tests.Apis.Upload
             [CombinatorialValues(true, false)] bool knownSize,
             [CombinatorialValues(/*new[] { 0 }, new[] { 100 }, new[] { 410 }, */new[] { 0, 410 })] int[] dodgyBytes,
             [CombinatorialValues(/*100, 400, */1000)] int chunkSize,
-            [CombinatorialValues(4096, 51, 100)] int bufferSize)
+            [CombinatorialValues(/*4096, 51, */100)] int bufferSize)
         {
             var expectedCallCount = 1 + (uploadLength + chunkSize - 1) / chunkSize
                 + dodgyBytes.Length * 2;
