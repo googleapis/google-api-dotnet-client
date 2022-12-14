a2433d1383bc97d947b438c43fa29304d5d9d952
Jon Skeet <jonskeet@google.com>
2022-12-14 17:39:46 +0000

    chore: Just refactor ResumableUploadTest to multiple files

---
+++ Src/Support/Google.Apis.Tests/Apis/Upload/ResumableUploadTest.SingleChunk.cs
@@ -0,0 +1,163 @@
+﻿/*
+Copyright 2017 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using Google.Apis.Tests.Mocks;
+using Google.Apis.Upload;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Apis.Tests.Apis.Upload
+{
+    public partial class ResumableUploadTest
+    {
+        /// <summary>
+        /// Server that only handles an upload completing in a single chunk.
+        /// </summary>
+        private class SingleChunkServer : TestServer.Handler
+        {
+            public SingleChunkServer(TestServer server) : base(server) { }
+
+            protected override Task<IEnumerable<byte>> HandleCall(HttpListenerRequest request, HttpListenerResponse response)
+            {
+                switch (RemovePrefix(request.Url.PathAndQuery))
+                {
+                    case "SingleChunk?uploadType=resumable":
+                        response.Headers[HttpResponseHeader.Location] = $"{HttpPrefix}{UploadPath}";
+                        break;
+                    case UploadPath:
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+                return Task.FromResult<IEnumerable<byte>>(null);
+            }
+        }
+
+        /// <summary>
+        /// Upload completes in a single chunk.
+        /// </summary>
+        [Theory, CombinatorialData]
+        public void TestUploadInSingleChunk(
+            [CombinatorialValues(true, false)] bool knownSize,
+            [CombinatorialValues("", "text/plain")] string contentType,
+            [CombinatorialValues(0, 10)] int chunkSizeDelta)
+        {
+            using (var server = new SingleChunkServer(_server))
+            using (var service = new MockClientService(server.HttpPrefix))
+            {
+                var content = knownSize ? new MemoryStream(UploadTestBytes) : new UnknownSizeMemoryStream(UploadTestBytes);
+                var uploader = new TestResumableUpload(
+                    service, "SingleChunk", "POST", content, contentType, UploadLength + chunkSizeDelta);
+                var progress = uploader.Upload();
+                Assert.Equal(2, server.Requests.Count);
+                var r0 = server.Requests[0];
+                Assert.Equal(contentType, r0.Headers["X-Upload-Content-Type"]);
+                Assert.Equal(knownSize ? UploadTestBytes.Length.ToString() : null, r0.Headers["X-Upload-Content-Length"]);
+                var r1 = server.Requests[1];
+                Assert.Equal(UploadPath, server.RemovePrefix(r1.Url.AbsolutePath));
+                Assert.Equal($"bytes 0-{UploadLength - 1}/{UploadLength}", r1.Headers["Content-Range"]);
+                Assert.Equal(UploadStatus.Completed, progress.Status);
+                Assert.Equal(UploadTestBytes.Length, progress.BytesSent);
+            }
+        }
+
+        /// <summary>
+        /// An upload using a pre-established session.
+        /// </summary>
+        [Theory, CombinatorialData]
+        public async Task TestInitiatedResumableUpload(
+            [CombinatorialValues("", "text/plain")] string contentType)
+        {
+            using (var server = new SingleChunkServer(_server))
+            using (var service = new MockClientService(server.HttpPrefix))
+            {
+                var content = new MemoryStream(UploadTestBytes);
+                var tmpUploader = new TestResumableUpload(
+                    service, "SingleChunk", "POST", content, contentType, UploadLength);
+                var uploadUri = await tmpUploader.InitiateSessionAsync();
+
+                var uploader = ResumableUpload.CreateFromUploadUri(uploadUri, content);
+                var progress = uploader.Upload();
+
+                Assert.Equal(2, server.Requests.Count);
+                var r0 = server.Requests[0];
+                Assert.Equal(contentType, r0.Headers["X-Upload-Content-Type"]);
+                Assert.Equal(UploadTestBytes.Length.ToString(), r0.Headers["X-Upload-Content-Length"]);
+                var r1 = server.Requests[1];
+                Assert.Equal(UploadPath, server.RemovePrefix(r1.Url.AbsolutePath));
+                Assert.Equal($"bytes 0-{UploadLength - 1}/{UploadLength}", r1.Headers["Content-Range"]);
+                Assert.Equal(UploadStatus.Completed, progress.Status);
+                Assert.Equal(UploadTestBytes.Length, progress.BytesSent);
+            }
+        }
+
+        /// <summary>
+        /// Upload of an empty file.
+        /// </summary>
+        [Theory, CombinatorialData]
+        public void TestUploadEmptyFile(
+            [CombinatorialValues(true, false)] bool knownSize)
+        {
+            using (var server = new SingleChunkServer(_server))
+            using (var service = new MockClientService(server.HttpPrefix))
+            {
+                var content = knownSize ? new MemoryStream(new byte[0]) : new UnknownSizeMemoryStream(new byte[0]);
+                var uploader = new TestResumableUpload(service, "SingleChunk", "POST", content, "text/plain", 100);
+                var progress = uploader.Upload();
+                Assert.Equal(2, server.Requests.Count);
+                var r0 = server.Requests[0];
+                Assert.Equal(knownSize ? "0" : null, r0.Headers["X-Upload-Content-Length"]);
+                var r1 = server.Requests[1];
+                Assert.Equal(UploadPath, server.RemovePrefix(r1.Url.AbsolutePath));
+                Assert.Equal("bytes *" + "/0", r1.Headers["Content-Range"]);
+                Assert.Equal(UploadStatus.Completed, progress.Status);
+                Assert.Equal(0, progress.BytesSent);
+            }
+        }
+
+        [Fact]
+        public void TestUploadStreamDisposedBeforeConstruction()
+        {
+            using (var server = new SingleChunkServer(_server))
+            using (var service = new MockClientService(server.HttpPrefix))
+            {
+                var content = new MemoryStream(new byte[10]);
+                content.Dispose();
+                var uploader = new TestResumableUpload(service, "SingleChunk", "POST", content, "text/plain", 100);
+                var progress = uploader.Upload();
+                Assert.IsType<ObjectDisposedException>(progress.Exception);
+            }
+        }
+
+        [Fact]
+        public void TestUploadStreamDisposedBetweenConstructionAndUpload()
+        {
+            using (var server = new SingleChunkServer(_server))
+            using (var service = new MockClientService(server.HttpPrefix))
+            {
+                var content = new MemoryStream(new byte[10]);
+                var uploader = new TestResumableUpload(service, "SingleChunk", "POST", content, "text/plain", 100);
+                content.Dispose();
+                var progress = uploader.Upload();
+                Assert.IsType<ObjectDisposedException>(progress.Exception);
+            }
+        }
+    }
+}
---
+++ Src/Support/Google.Apis.Tests/Apis/Upload/ResumableUploadTest.TestServer.cs
@@ -0,0 +1,201 @@
+﻿/*
+Copyright 2017 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Apis.Tests.Apis.Upload
+{
+    public partial class ResumableUploadTest
+    {
+        /// <summary>
+        /// HTTP server which listens on localhost:&lt;random port&gt; for testing.
+        /// </summary>
+        /// <remarks>
+        /// <para>A single server is started for all tests, and is shutdown when all tests
+        /// have run.</para>
+        /// <para>Each test registers its own <see cref="Handler"/> with a unique
+        /// URL path prefix, with the handler designed to fake a server with the
+        /// required behaviour for the specific test. The test handler is unregistered
+        /// when the test ends.</para>
+        /// </remarks>
+        private class TestServer : IDisposable
+        {
+            internal TestLogger Logger { get; }
+
+            public TestServer(TestLogger logger)
+            {
+                Logger = logger;
+
+                var rnd = new Random();
+                // Find an available port and start an HttpListener.
+                do
+                {
+                    _httpListener = new HttpListener();
+                    HttpPrefix = $"http://localhost:{rnd.Next(49152, 65535)}/";
+                    _httpListener.Prefixes.Add(HttpPrefix);
+                    try
+                    {
+                        _httpListener.Start();
+                    }
+                    // Catch errors that mean the port is already in use
+                    catch (HttpListenerException e) when (e.ErrorCode == 183 || e.ErrorCode == 32 || e.Message.Contains("already in use"))
+                    {
+                        _httpListener.Close();
+                        _httpListener = null;
+                    }
+                    catch (SocketException e) when (e.SocketErrorCode == SocketError.AddressAlreadyInUse)
+                    {
+                        _httpListener.Close();
+                        _httpListener = null;
+                    }
+                } while (_httpListener == null);
+                _httpTask = RunServer();
+            }
+
+            private readonly HttpListener _httpListener;
+            private readonly Task _httpTask;
+
+            public string HttpPrefix { get; }
+
+            private async Task RunServer()
+            {
+                while (_httpListener.IsListening)
+                {
+                    var context = await _httpListener.GetContextAsync();
+                    var response = context.Response;
+                    if (context.Request.Url.AbsolutePath.EndsWith("/Quit"))
+                    {
+                        response.Close();
+                        _httpListener.Stop();
+                    }
+                    else
+                    {
+                        response.ContentType = "text/plain";
+                        IEnumerable<byte> body;
+                        try
+                        {
+                            body = await HandleCall(context.Request, response);
+                            var bodyBytes = body?.ToArray() ?? new byte[0];
+                            await response.OutputStream.WriteAsync(bodyBytes, 0, bodyBytes.Length);
+                        }
+                        catch (HttpListenerException ex)
+                        {
+                            Logger.WriteLine($"HttpListener failed while handling response: {ex}");
+                        }
+                        finally
+                        {
+                            try
+                            {
+                                response.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.WriteLine($"HttpListener failed while closing response: {ex}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            public abstract class Handler : IDisposable
+            {
+                private static int handlerId = 0;
+
+                private TestServer _server;
+
+                public Handler(TestServer server)
+                {
+                    _server = server;
+                    Id = Interlocked.Increment(ref handlerId).ToString();
+                    _server.RegisterHandler(this);
+                }
+
+                public string Id { get; }
+                public string HttpPrefix => $"{_server.HttpPrefix}{Id}/";
+
+                public string RemovePrefix(string s)
+                {
+                    var prefix = $"/{Id}/";
+                    if (s.StartsWith(prefix))
+                    {
+                        return s.Substring(prefix.Length);
+                    }
+                    throw new InvalidOperationException("Doesn't start with prefix");
+                }
+
+                public List<RequestInfo> Requests { get; } = new List<RequestInfo>();
+
+                public Task<IEnumerable<byte>> HandleCall0(
+                    HttpListenerRequest request, HttpListenerResponse response)
+                {
+                    Requests.Add(new RequestInfo(request));
+                    return HandleCall(request, response);
+                }
+
+                protected abstract Task<IEnumerable<byte>> HandleCall(
+                    HttpListenerRequest request, HttpListenerResponse response);
+
+                public void Dispose()
+                {
+                    _server.UnregisterHandler(Id);
+                }
+            }
+
+            private ConcurrentDictionary<string, Handler> _handlers =
+                new ConcurrentDictionary<string, Handler>();
+
+            private Task<IEnumerable<byte>> HandleCall(
+                HttpListenerRequest request, HttpListenerResponse response)
+            {
+                var id = request.Url.Segments[1].TrimEnd('/');
+                var handler = _handlers[id];
+                return handler.HandleCall0(request, response);
+            }
+
+            private void RegisterHandler(Handler handler)
+            {
+                _handlers.TryAdd(handler.Id, handler);
+            }
+
+            private void UnregisterHandler(string id)
+            {
+                Handler handler;
+                _handlers.TryRemove(id, out handler);
+            }
+
+            public void Dispose()
+            {
+                var timeout = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+                _httpTask.ContinueWith(task => timeout.Cancel());
+                Task.Run(async () =>
+                {
+                    await new HttpClient().GetAsync(HttpPrefix + "Quit", timeout.Token);
+                    _httpListener.Stop();
+                });
+                _httpTask.Wait();
+            }
+        }
+
+    }
+}
