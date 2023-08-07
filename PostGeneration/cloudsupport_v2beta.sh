cf32b6676f7731b1ebab9c0f058b18234c66f138
Jon Skeet <jonskeet@google.com>
2023-08-07 07:55:24 +0100

    chore: Fix doc generation of CloudSupport v2Beta

---
+++ PostGeneration/cloudsupport_v2beta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docfx fails completely (throws an exception) with the
+# triple-backticks in Dialogflow. The Discovery Doc comments
+# aren't formatted anyway, making Markdown basically broken.
+# (That needs investigating separately.)
+sed -i 's/```//g' Src/Generated/Google.Apis.CloudSupport.v2beta/Google.Apis.CloudSupport.v2beta.cs
