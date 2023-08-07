a72ddac9649c7ffd7caf9e6eb4fc288e74c64e8b
Amanda Tarafa Mas <atarafamas@google.com>
2023-08-07 12:02:23 -0700

    fix: Have postgeneration execute again. Fixes #2490

--- PostGeneration/cloudsupport_v2.sh
+++ PostGeneration/cloudsupport_v2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+CODE_GENERATION_DIR="$1"
+
 # docfx fails completely (throws an exception) with the
 # triple-backticks in Dialogflow. The Discovery Doc comments
 # aren't formatted anyway, making Markdown basically broken.
 # (That needs investigating separately.)
-sed -i 's/```//g' Src/Generated/Google.Apis.CloudSupport.v2/Google.Apis.CloudSupport.v2.cs
+sed -i 's/```//g' "$CODE_GENERATION_DIR"/Google.Apis.CloudSupport.v2/Google.Apis.CloudSupport.v2.cs
