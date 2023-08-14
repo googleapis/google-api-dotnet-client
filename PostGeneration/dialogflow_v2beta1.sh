b207fccca8b2640341814098ddf572a5a72120ac
Jon Skeet <jonskeet@google.com>
2023-08-14 15:49:29 +0100

    chore: Remove obsolete post-generation changes

--- PostGeneration/dialogflow_v2beta1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CODE_GENERATION_DIR="$1"
-
-# docfx fails completely (throws an exception) with the
-# triple-backticks in Dialogflow. The Discovery Doc comments
-# aren't formatted anyway, making Markdown basically broken.
-# (That needs investigating separately.)
-sed -i 's/```//g' "$CODE_GENERATION_DIR"/Google.Apis.Dialogflow.v2beta1/Google.Apis.Dialogflow.v2beta1.cs
