df48a67875270ff8d2940a94a54d5d9db7e87914
Jon Skeet <jonskeet@google.com>
2024-04-09 03:02:35 +0100

    tools: Add package map generator
    This incrementally updates features.json by trying to guess mappings (fairly crudely).
    The resulting changes should be validated manually, but that's not too hard.
    
    Any existing entries are retained, which is useful for APIs such as Firestore where we want to map to the higher-level abstraction library.

---
+++ Src/Tools/CloudPackageMapGenerator/ApiCatalog.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudPackageMapGenerator;
+
+/// <summary>
+/// Massively reduced version of the full API catalog in google-cloud-dotnet.
+/// </summary>
+public class ApiCatalog
+{
+    /// <summary>
+    /// The APIs within the catalog.
+    /// </summary>
+    public List<ApiMetadata> Apis { get; set; }
+}
