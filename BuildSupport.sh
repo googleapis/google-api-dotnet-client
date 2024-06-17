09385f383336e96ad01546cbdc2c4afe9f1538eb
Jon Skeet <jonskeet@google.com>
2024-06-17 12:16:59 +0100

    feat: Improve SourceLink for NuGet packages
    - Use deterministic builds
    - Use a newer version of SourceLink (and centralize it)
    - Fix issues with unexpected output files
    - Use a newer version of refernece assemblies (and centralize it)
    
    This will generate warnings for Src/Generated projects until we adopt a new version of the generator which doesn't include the reference assemblies and SourceLink package directly.

--- BuildSupport.sh
+++ BuildSupport.sh
@@ -4,6 +4,7 @@ set -e
 
 # Forces sourcelink to work during the build.
 export CI=true
+export DeterministicSourcePaths=true
 
 dotnet build Src/Support/GoogleApisClient.sln -c Release
 dotnet pack Src/Support/GoogleApisClient.sln -c Release -o NuPkgs/Support
