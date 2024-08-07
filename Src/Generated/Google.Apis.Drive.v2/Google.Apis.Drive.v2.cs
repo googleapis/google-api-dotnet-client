100dbc40f02281ec2162c425b0db14c1ba02a768
Amanda Tarafa Mas <atarafamas@google.com>
2024-08-07 01:09:35 -0700

    feat: Generate Google.Apis.Drive.v2 version 1.68.0.3498

--- Src/Generated/Google.Apis.Drive.v2/Google.Apis.Drive.v2.cs
+++ Src/Generated/Google.Apis.Drive.v2/Google.Apis.Drive.v2.cs
@@ -10261,8 +10261,10 @@ namespace Google.Apis.Drive.v2.Data
 
         /// <summary>
         /// Output only. A short-lived link to the file's thumbnail, if available. Typically lasts on the order of
-        /// hours. Only populated when the requesting app can access the file's content. If the file isn't shared
-        /// publicly, the URL returned in `Files.thumbnailLink` must be fetched using a credentialed request.
+        /// hours. Not intended for direct usage on web applications due to [Cross-Origin Resource Sharing
+        /// (CORS)](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS), consider using a proxy server. Only
+        /// populated when the requesting app can access the file's content. If the file isn't shared publicly, the URL
+        /// returned in `Files.thumbnailLink` must be fetched using a credentialed request.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("thumbnailLink")]
         public virtual string ThumbnailLink { get; set; }
