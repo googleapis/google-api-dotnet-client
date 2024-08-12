8a9f63ada2081f24bd14a32a8edf90be0445e2c6
Amanda Tarafa Mas <atarafamas@google.com>
2024-08-12 01:11:13 -0700

    feat: Generate Google.Apis.ServiceControl.v1 version 1.68.0.3501

--- Src/Generated/Google.Apis.ServiceControl.v1/Google.Apis.ServiceControl.v1.cs
+++ Src/Generated/Google.Apis.ServiceControl.v1/Google.Apis.ServiceControl.v1.cs
@@ -3103,6 +3103,13 @@ namespace Google.Apis.ServiceControl.v1.Data
     public class V1ResourceEvent : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
+        /// The ESF unique context id of the api request, from which this resource event originated. This field is only
+        /// needed for CAIS integration via api annotation. See go/cais-lro-delete for more details.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("contextId")]
+        public virtual System.Nullable<long> ContextId { get; set; }
+
+        /// <summary>
         /// The destinations field determines which backend services should handle the event. This should be specified
         /// as a comma-delimited string.
         /// </summary>
