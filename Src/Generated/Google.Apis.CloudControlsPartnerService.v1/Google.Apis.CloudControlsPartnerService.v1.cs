4f15cfaf5edd7aecea3bba9f2d128e7e1f505ebd
Amanda Tarafa Mas <atarafamas@google.com>
2024-08-12 01:09:39 -0700

    feat: Generate Google.Apis.CloudControlsPartnerService.v1 version 1.68.0.3499

--- Src/Generated/Google.Apis.CloudControlsPartnerService.v1/Google.Apis.CloudControlsPartnerService.v1.cs
+++ Src/Generated/Google.Apis.CloudControlsPartnerService.v1/Google.Apis.CloudControlsPartnerService.v1.cs
@@ -1346,7 +1346,7 @@ namespace Google.Apis.CloudControlsPartnerService.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("customerOnboardingState")]
         public virtual CustomerOnboardingState CustomerOnboardingState { get; set; }
 
-        /// <summary>Output only. The customer organization's display name. E.g. "google.com".</summary>
+        /// <summary>Optional. The customer organization's display name. E.g. "Google".</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("displayName")]
         public virtual string DisplayName { get; set; }
 
