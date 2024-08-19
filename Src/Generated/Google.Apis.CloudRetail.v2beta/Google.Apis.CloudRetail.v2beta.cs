7e08799e6e2383639a477a4f3d8b0255871d8839
Amanda Tarafa Mas <atarafamas@google.com>
2024-08-19 01:12:14 -0700

    feat: Generate Google.Apis.CloudRetail.v2beta version 1.68.0.3515

--- Src/Generated/Google.Apis.CloudRetail.v2beta/Google.Apis.CloudRetail.v2beta.cs
+++ Src/Generated/Google.Apis.CloudRetail.v2beta/Google.Apis.CloudRetail.v2beta.cs
@@ -11834,7 +11834,7 @@ namespace Google.Apis.CloudRetail.v2beta.Data
     public class GoogleCloudRetailV2betaRuleForceReturnFacetAction : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
-        /// Each instance corresponds to a force return attribute for the given condition. There can't be more 3
+        /// Each instance corresponds to a force return attribute for the given condition. There can't be more 15
         /// instances here.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("facetPositionAdjustments")]
