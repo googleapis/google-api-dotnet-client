f37f9914c41f33ed359ee10456d782132344d39b
Amanda Tarafa Mas <atarafamas@google.com>
2024-08-26 01:10:58 -0700

    feat: Generate Google.Apis.GKEHub.v1alpha version 1.68.0.3514

--- Src/Generated/Google.Apis.GKEHub.v1alpha/Google.Apis.GKEHub.v1alpha.cs
+++ Src/Generated/Google.Apis.GKEHub.v1alpha/Google.Apis.GKEHub.v1alpha.cs
@@ -5822,7 +5822,7 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>CommonFeatureSpec contains Hub-wide configuration information</summary>
+    /// <summary>CommonFeatureSpec contains Fleet-wide configuration information</summary>
     public class CommonFeatureSpec : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>Anthos Observability spec</summary>
@@ -5865,7 +5865,7 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>CommonFeatureState contains Hub-wide Feature status information.</summary>
+    /// <summary>CommonFeatureState contains Fleet-wide Feature status information.</summary>
     public class CommonFeatureState : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>Appdevexperience specific state.</summary>
@@ -5888,7 +5888,7 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         [Newtonsoft.Json.JsonPropertyAttribute("servicemesh")]
         public virtual ServiceMeshFeatureState Servicemesh { get; set; }
 
-        /// <summary>Output only. The "running state" of the Feature in this Hub.</summary>
+        /// <summary>Output only. The "running state" of the Feature in this Fleet.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("state")]
         public virtual FeatureState State { get; set; }
 
@@ -6378,7 +6378,11 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         [Newtonsoft.Json.JsonPropertyAttribute("configSync")]
         public virtual ConfigManagementConfigSync ConfigSync { get; set; }
 
-        /// <summary>Hierarchy Controller configuration for the cluster.</summary>
+        /// <summary>
+        /// Hierarchy Controller configuration for the cluster. Deprecated: Configuring Hierarchy Controller through the
+        /// configmanagement feature is no longer recommended. Use
+        /// https://github.com/kubernetes-sigs/hierarchical-namespaces instead.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("hierarchyController")]
         public virtual ConfigManagementHierarchyControllerConfig HierarchyController { get; set; }
 
@@ -6894,7 +6898,7 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>Feature represents the settings and status of any Hub Feature.</summary>
+    /// <summary>Feature represents the settings and status of any Fleet Feature.</summary>
     public class Feature : Google.Apis.Requests.IDirectResponseSchema
     {
         private string _createTimeRaw;
@@ -7034,13 +7038,13 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         public virtual System.Collections.Generic.IDictionary<string, ScopeFeatureState> ScopeStates { get; set; }
 
         /// <summary>
-        /// Optional. Hub-wide Feature configuration. If this Feature does not support any Hub-wide configuration, this
-        /// field may be unused.
+        /// Optional. Fleet-wide Feature configuration. If this Feature does not support any Fleet-wide configuration,
+        /// this field may be unused.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("spec")]
         public virtual CommonFeatureSpec Spec { get; set; }
 
-        /// <summary>Output only. The Hub-wide Feature state.</summary>
+        /// <summary>Output only. The Fleet-wide Feature state.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("state")]
         public virtual CommonFeatureState State { get; set; }
 
@@ -7091,7 +7095,7 @@ namespace Google.Apis.GKEHub.v1alpha.Data
 
     /// <summary>
     /// FeatureResourceState describes the state of a Feature *resource* in the GkeHub API. See `FeatureState` for the
-    /// "running state" of the Feature in the Hub and across Memberships.
+    /// "running state" of the Feature in the Fleet and across Memberships.
     /// </summary>
     public class FeatureResourceState : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -8783,10 +8787,7 @@ namespace Google.Apis.GKEHub.v1alpha.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>
-    /// MembershipFeatureSpec contains configuration information for a single Membership. NOTE: Please use snake case in
-    /// your feature name.
-    /// </summary>
+    /// <summary>MembershipFeatureSpec contains configuration information for a single Membership.</summary>
     public class MembershipFeatureSpec : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>Anthos Observability-specific spec</summary>
