5c9c3e9530c6f5a07046d224f8750d9b305277b4
Amanda Tarafa Mas <atarafamas@google.com>
2024-08-26 01:11:42 -0700

    feat: Generate Google.Apis.SecretManager.v1 version 1.68.0.3515

--- Src/Generated/Google.Apis.SecretManager.v1/Google.Apis.SecretManager.v1.cs
+++ Src/Generated/Google.Apis.SecretManager.v1/Google.Apis.SecretManager.v1.cs
@@ -3589,7 +3589,7 @@ namespace Google.Apis.SecretManager.v1.Data
     public class Topic : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
-        /// Required. The resource name of the Pub/Sub topic that will be published to, in the following format:
+        /// Identifier. The resource name of the Pub/Sub topic that will be published to, in the following format:
         /// `projects/*/topics/*`. For publication to succeed, the Secret Manager service agent must have the
         /// `pubsub.topic.publish` permission on the topic. The Pub/Sub Publisher role (`roles/pubsub.publisher`)
         /// includes this permission.
