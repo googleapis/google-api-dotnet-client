a79fa8d46220fc99db7c0efa5b51a6624ac19d4d
Amanda Tarafa Mas <atarafamas@google.com>
2024-09-09 01:13:18 -0700

    feat: Generate Google.Apis.BigtableAdmin.v2 version 1.68.0.3523

--- Src/Generated/Google.Apis.BigtableAdmin.v2/Google.Apis.BigtableAdmin.v2.cs
+++ Src/Generated/Google.Apis.BigtableAdmin.v2/Google.Apis.BigtableAdmin.v2.cs
@@ -4793,10 +4793,6 @@ namespace Google.Apis.BigtableAdmin.v2.Data
     /// <summary>A backup of a Cloud Bigtable table.</summary>
     public class Backup : Google.Apis.Requests.IDirectResponseSchema
     {
-        /// <summary>Indicates the backup type of the backup.</summary>
-        [Newtonsoft.Json.JsonPropertyAttribute("backupType")]
-        public virtual string BackupType { get; set; }
-
         /// <summary>Output only. The encryption information for the backup.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("encryptionInfo")]
         public virtual EncryptionInfo EncryptionInfo { get; set; }
@@ -4880,50 +4876,6 @@ namespace Google.Apis.BigtableAdmin.v2.Data
         {
             get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(ExpireTimeRaw);
             set => ExpireTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
-        }
-
-        private string _hotToStandardTimeRaw;
-
-        private object _hotToStandardTime;
-
-        /// <summary>
-        /// The time at which the hot backup will be converted to a standard backup. Once the `hot_to_standard_time` has
-        /// passed, Cloud Bigtable will convert the hot backup to a standard backup. This value must be greater than the
-        /// backup creation time by: - At least 24 hours This field only applies for hot backups. When creating or
-        /// updating a standard backup, attempting to set this field will fail the request.
-        /// </summary>
-        [Newtonsoft.Json.JsonPropertyAttribute("hotToStandardTime")]
-        public virtual string HotToStandardTimeRaw
-        {
-            get => _hotToStandardTimeRaw;
-            set
-            {
-                _hotToStandardTime = Google.Apis.Util.Utilities.DeserializeForGoogleFormat(value);
-                _hotToStandardTimeRaw = value;
-            }
-        }
-
-        /// <summary><seealso cref="object"/> representation of <see cref="HotToStandardTimeRaw"/>.</summary>
-        [Newtonsoft.Json.JsonIgnoreAttribute]
-        [System.ObsoleteAttribute("This property is obsolete and may behave unexpectedly; please use HotToStandardTimeDateTimeOffset instead.")]
-        public virtual object HotToStandardTime
-        {
-            get => _hotToStandardTime;
-            set
-            {
-                _hotToStandardTimeRaw = Google.Apis.Util.Utilities.SerializeForGoogleFormat(value);
-                _hotToStandardTime = value;
-            }
-        }
-
-        /// <summary>
-        /// <seealso cref="System.DateTimeOffset"/> representation of <see cref="HotToStandardTimeRaw"/>.
-        /// </summary>
-        [Newtonsoft.Json.JsonIgnoreAttribute]
-        public virtual System.DateTimeOffset? HotToStandardTimeDateTimeOffset
-        {
-            get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(HotToStandardTimeRaw);
-            set => HotToStandardTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
         }
 
         /// <summary>
