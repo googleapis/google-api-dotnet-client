bc6c122dc16a0f35b1b3f13a92f8813e9135e570
Amanda Tarafa Mas <atarafamas@google.com>
2024-09-09 01:13:23 -0700

    feat: Generate Google.Apis.ChecksService.v1alpha version 1.68.0.3534

--- Src/Generated/Google.Apis.ChecksService.v1alpha/Google.Apis.ChecksService.v1alpha.cs
+++ Src/Generated/Google.Apis.ChecksService.v1alpha/Google.Apis.ChecksService.v1alpha.cs
@@ -259,6 +259,7 @@ namespace Google.Apis.ChecksService.v1alpha
         {
             this.service = service;
             Apps = new AppsResource(service);
+            Repos = new ReposResource(service);
         }
 
         /// <summary>Gets the Apps resource.</summary>
@@ -949,6 +950,94 @@ namespace Google.Apis.ChecksService.v1alpha
                         DefaultValue = null,
                         Pattern = null,
                     });
+                }
+            }
+        }
+
+        /// <summary>Gets the Repos resource.</summary>
+        public virtual ReposResource Repos { get; }
+
+        /// <summary>The "repos" collection of methods.</summary>
+        public class ReposResource
+        {
+            private const string Resource = "repos";
+
+            /// <summary>The service which this resource belongs to.</summary>
+            private readonly Google.Apis.Services.IClientService service;
+
+            /// <summary>Constructs a new resource.</summary>
+            public ReposResource(Google.Apis.Services.IClientService service)
+            {
+                this.service = service;
+                Operations = new OperationsResource(service);
+            }
+
+            /// <summary>Gets the Operations resource.</summary>
+            public virtual OperationsResource Operations { get; }
+
+            /// <summary>The "operations" collection of methods.</summary>
+            public class OperationsResource
+            {
+                private const string Resource = "operations";
+
+                /// <summary>The service which this resource belongs to.</summary>
+                private readonly Google.Apis.Services.IClientService service;
+
+                /// <summary>Constructs a new resource.</summary>
+                public OperationsResource(Google.Apis.Services.IClientService service)
+                {
+                    this.service = service;
+                }
+
+                /// <summary>
+                /// Gets the latest state of a long-running operation. Clients can use this method to poll the operation
+                /// result at intervals as recommended by the API service.
+                /// </summary>
+                /// <param name="name">The name of the operation resource.</param>
+                public virtual GetRequest Get(string name)
+                {
+                    return new GetRequest(this.service, name);
+                }
+
+                /// <summary>
+                /// Gets the latest state of a long-running operation. Clients can use this method to poll the operation
+                /// result at intervals as recommended by the API service.
+                /// </summary>
+                public class GetRequest : ChecksServiceBaseServiceRequest<Google.Apis.ChecksService.v1alpha.Data.Operation>
+                {
+                    /// <summary>Constructs a new Get request.</summary>
+                    public GetRequest(Google.Apis.Services.IClientService service, string name) : base(service)
+                    {
+                        Name = name;
+                        InitParameters();
+                    }
+
+                    /// <summary>The name of the operation resource.</summary>
+                    [Google.Apis.Util.RequestParameterAttribute("name", Google.Apis.Util.RequestParameterType.Path)]
+                    public virtual string Name { get; private set; }
+
+                    /// <summary>Gets the method name.</summary>
+                    public override string MethodName => "get";
+
+                    /// <summary>Gets the HTTP method.</summary>
+                    public override string HttpMethod => "GET";
+
+                    /// <summary>Gets the REST path.</summary>
+                    public override string RestPath => "v1alpha/{+name}";
+
+                    /// <summary>Initializes Get parameter list.</summary>
+                    protected override void InitParameters()
+                    {
+                        base.InitParameters();
+                        RequestParameters.Add("name", new Google.Apis.Discovery.Parameter
+                        {
+                            Name = "name",
+                            IsRequired = true,
+                            ParameterType = "path",
+                            DefaultValue = null,
+                            Pattern = @"^accounts/[^/]+/repos/[^/]+/operations/[^/]+$",
+                        });
+                    }
                 }
             }
         }
