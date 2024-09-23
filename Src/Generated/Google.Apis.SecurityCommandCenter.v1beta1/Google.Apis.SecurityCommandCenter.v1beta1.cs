95438d47b092b60d6d3d3a97976afd725cb8d6b3
Amanda Tarafa Mas <atarafamas@google.com>
2024-09-23 01:11:36 -0700

    feat: Generate Google.Apis.SecurityCommandCenter.v1beta1 version 1.68.0.3546

--- Src/Generated/Google.Apis.SecurityCommandCenter.v1beta1/Google.Apis.SecurityCommandCenter.v1beta1.cs
+++ Src/Generated/Google.Apis.SecurityCommandCenter.v1beta1/Google.Apis.SecurityCommandCenter.v1beta1.cs
@@ -2808,6 +2808,10 @@ namespace Google.Apis.SecurityCommandCenter.v1beta1.Data
     /// <summary>Represents a Microsoft Entra tenant.</summary>
     public class AzureTenant : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>The display name of the Azure tenant.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("displayName")]
+        public virtual string DisplayName { get; set; }
+
         /// <summary>The ID of the Microsoft Entra tenant, for example, "a11aaa11-aa11-1aa1-11aa-1aaa11a".</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("id")]
         public virtual string Id { get; set; }
@@ -6441,6 +6445,10 @@ namespace Google.Apis.SecurityCommandCenter.v1beta1.Data
     /// <summary>Represents a Microsoft Entra tenant.</summary>
     public class GoogleCloudSecuritycenterV2AzureTenant : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>The display name of the Azure tenant.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("displayName")]
+        public virtual string DisplayName { get; set; }
+
         /// <summary>The ID of the Microsoft Entra tenant, for example, "a11aaa11-aa11-1aa1-11aa-1aaa11a".</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("id")]
         public virtual string Id { get; set; }
