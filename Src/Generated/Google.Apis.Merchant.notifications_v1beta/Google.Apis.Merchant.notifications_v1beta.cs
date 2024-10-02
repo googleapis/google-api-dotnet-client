5fd0f64b8e5963ac69ce7f5791a20632c6f37369
Amanda Tarafa Mas <atarafamas@google.com>
2024-10-02 01:06:54 -0700

    feat: Generate Google.Apis.Merchant.notifications_v1beta version 1.68.0.3560

--- Src/Generated/Google.Apis.Merchant.notifications_v1beta/Google.Apis.Merchant.notifications_v1beta.cs
+++ Src/Generated/Google.Apis.Merchant.notifications_v1beta/Google.Apis.Merchant.notifications_v1beta.cs
@@ -289,12 +289,15 @@ namespace Google.Apis.Merchant.notifications_v1beta
             }
 
             /// <summary>
-            /// Creates a notification subscription for a merchant. We will allow the following types of notification
-            /// subscriptions to exist together (per merchant as a subscriber per event type): 1. Subscription for all
-            /// managed accounts + subscription for self 2. Multiple "partial" subscriptions for managed accounts +
-            /// subscription for self we will not allow (per merchant as a subscriber per event type): 1. multiple self
-            /// subscriptions. 2. multiple "all managed accounts" subscriptions. 3. all and partial subscriptions at the
-            /// same time. 4. multiple partial subscriptions for the same target account
+            /// Creates a notification subscription for a business. For standalone or subaccounts accounts, the business
+            /// can create a subscription for self. For MCAs, the business can create a subscription for all managed
+            /// accounts or for a specific subaccount. We will allow the following types of notification subscriptions
+            /// to exist together (per business as a subscriber per event type): 1. Subscription for all managed
+            /// accounts + subscription for self. 2. Multiple "partial" subscriptions for managed accounts +
+            /// subscription for self. we will not allow (per business as a subscriber per event type): 1. Multiple self
+            /// subscriptions. 2. Multiple "all managed accounts" subscriptions. 3. "All managed accounts" subscription
+            /// and partial subscriptions at the same time. 4. Multiple partial subscriptions for the same target
+            /// account.
             /// </summary>
             /// <param name="body">The body of the request.</param>
             /// <param name="parent">
@@ -306,12 +309,15 @@ namespace Google.Apis.Merchant.notifications_v1beta
             }
 
             /// <summary>
-            /// Creates a notification subscription for a merchant. We will allow the following types of notification
-            /// subscriptions to exist together (per merchant as a subscriber per event type): 1. Subscription for all
-            /// managed accounts + subscription for self 2. Multiple "partial" subscriptions for managed accounts +
-            /// subscription for self we will not allow (per merchant as a subscriber per event type): 1. multiple self
-            /// subscriptions. 2. multiple "all managed accounts" subscriptions. 3. all and partial subscriptions at the
-            /// same time. 4. multiple partial subscriptions for the same target account
+            /// Creates a notification subscription for a business. For standalone or subaccounts accounts, the business
+            /// can create a subscription for self. For MCAs, the business can create a subscription for all managed
+            /// accounts or for a specific subaccount. We will allow the following types of notification subscriptions
+            /// to exist together (per business as a subscriber per event type): 1. Subscription for all managed
+            /// accounts + subscription for self. 2. Multiple "partial" subscriptions for managed accounts +
+            /// subscription for self. we will not allow (per business as a subscriber per event type): 1. Multiple self
+            /// subscriptions. 2. Multiple "all managed accounts" subscriptions. 3. "All managed accounts" subscription
+            /// and partial subscriptions at the same time. 4. Multiple partial subscriptions for the same target
+            /// account.
             /// </summary>
             public class CreateRequest : MerchantBaseServiceRequest<Google.Apis.Merchant.notifications_v1beta.Data.NotificationSubscription>
             {
