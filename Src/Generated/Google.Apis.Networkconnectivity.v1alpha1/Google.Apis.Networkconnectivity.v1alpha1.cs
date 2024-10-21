227fe2b7bdc4c08390976a5d95ac6826b11e39c3
Amanda Tarafa Mas <atarafamas@google.com>
2024-10-21 01:13:15 -0700

    feat: Generate Google.Apis.Networkconnectivity.v1alpha1 version 1.68.0.3569

--- Src/Generated/Google.Apis.Networkconnectivity.v1alpha1/Google.Apis.Networkconnectivity.v1alpha1.cs
+++ Src/Generated/Google.Apis.Networkconnectivity.v1alpha1/Google.Apis.Networkconnectivity.v1alpha1.cs
@@ -3052,7 +3052,9 @@ namespace Google.Apis.Networkconnectivity.v1alpha1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("labels")]
         public virtual System.Collections.Generic.IDictionary<string, string> Labels { get; set; }
 
-        /// <summary>Optional. Should be present if usage is set to FOR_MIGRATION.</summary>
+        /// <summary>
+        /// Optional. Must be present if usage is set to FOR_MIGRATION. This field is for internal use.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("migration")]
         public virtual Migration Migration { get; set; }
 
