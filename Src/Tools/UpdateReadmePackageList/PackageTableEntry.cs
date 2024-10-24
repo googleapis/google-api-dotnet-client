0cab81e5e96efea27d7088c138c0e6892b2c0e52
jon <jonskeet@google.com>
2024-10-24 10:30:09 +0100

    tools: Add tooling to generate package table in README.md

---
+++ Src/Tools/UpdateReadmePackageList/PackageTableEntry.cs
@@ -0,0 +1,27 @@
+﻿
+using Google.Apis.Discovery.v1.Data;
+using ToolUtilities;
+
+namespace UpdateReadmePackageList;
+
+internal class PackageTableEntry
+{
+    public string PackageName { get; }
+    public string NugetLink => $"https://www.nuget.org/packages/{PackageName}";
+    public string ReferenceDocsLink => $"https://googleapis.dev/dotnet/{PackageName}/latest/api/{PackageName}.html";
+    public string ApiDocsLink { get; }
+    public string ApiName { get; }
+
+    private PackageTableEntry(RestDescription discoveryDoc)
+    {
+        PackageName = Discovery.GetPackageName(discoveryDoc);
+        ApiDocsLink = discoveryDoc.DocumentationLink;
+        ApiName = $"{discoveryDoc.Title} {discoveryDoc.Version.Replace('.', '_')}";
+    }
+
+    public static PackageTableEntry Load(string discoveryFilePath) =>
+        new(Discovery.ParseDiscoveryJson(File.ReadAllText(discoveryFilePath)));
+
+    internal string ToMarkdown() =>
+        $"|[{ApiName}]({ApiDocsLink})|[{PackageName}]({NugetLink})|[Library documentation]({ReferenceDocsLink})|";
+}
