74fe3cdb266d75bf2f89093eae23757fb603ac78
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-06 01:09:44 -0800

    feat: Generate Google.Apis.Playdeveloperreporting.v1beta1 version 1.68.0.3593

--- Src/Generated/Google.Apis.Playdeveloperreporting.v1beta1/Google.Apis.Playdeveloperreporting.v1beta1.cs
+++ Src/Generated/Google.Apis.Playdeveloperreporting.v1beta1/Google.Apis.Playdeveloperreporting.v1beta1.cs
@@ -4061,9 +4061,9 @@ namespace Google.Apis.Playdeveloperreporting.v1beta1.Data
 
     /// <summary>
     /// A representation of a decimal value, such as 2.5. Clients may convert values into language-native decimal
-    /// formats, such as Java's BigDecimal or Python's decimal.Decimal. [BigDecimal]:
-    /// https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/math/BigDecimal.html [decimal.Decimal]:
-    /// https://docs.python.org/3/library/decimal.html
+    /// formats, such as Java's
+    /// [BigDecimal](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/math/BigDecimal.html) or Python's
+    /// [decimal.Decimal](https://docs.python.org/3/library/decimal.html).
     /// </summary>
     public class GoogleTypeDecimal : Google.Apis.Requests.IDirectResponseSchema
     {
