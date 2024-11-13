c53eaf56a4d4926134753b3e0bca4bd6dd99b2fc
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-13 01:07:38 -0800

    feat: Generate Google.Apis.Cloudbilling.v1beta version 1.68.0.3597

--- Src/Generated/Google.Apis.Cloudbilling.v1beta/Google.Apis.Cloudbilling.v1beta.cs
+++ Src/Generated/Google.Apis.Cloudbilling.v1beta/Google.Apis.Cloudbilling.v1beta.cs
@@ -2132,9 +2132,9 @@ namespace Google.Apis.Cloudbilling.v1beta.Data
 
     /// <summary>
     /// A representation of a decimal value, such as 2.5. Clients may convert values into language-native decimal
-    /// formats, such as Java's BigDecimal or Python's decimal.Decimal. [BigDecimal]:
-    /// https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/math/BigDecimal.html [decimal.Decimal]:
-    /// https://docs.python.org/3/library/decimal.html
+    /// formats, such as Java's
+    /// [BigDecimal](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/math/BigDecimal.html) or Python's
+    /// [decimal.Decimal](https://docs.python.org/3/library/decimal.html).
     /// </summary>
     public class Decimal : Google.Apis.Requests.IDirectResponseSchema
     {
