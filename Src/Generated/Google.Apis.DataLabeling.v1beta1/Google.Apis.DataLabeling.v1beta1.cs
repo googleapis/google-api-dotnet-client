eee0850ce5ea9381df4f5a1f2454bf20dcf3471c
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-20 01:08:26 -0800

    feat: Generate Google.Apis.DataLabeling.v1beta1 version 1.68.0.3608

--- Src/Generated/Google.Apis.DataLabeling.v1beta1/Google.Apis.DataLabeling.v1beta1.cs
+++ Src/Generated/Google.Apis.DataLabeling.v1beta1/Google.Apis.DataLabeling.v1beta1.cs
@@ -3424,7 +3424,7 @@ namespace Google.Apis.DataLabeling.v1beta1
             /// `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check
             /// whether the cancellation succeeded or whether the operation completed despite cancellation. On
             /// successful cancellation, the operation is not deleted; instead, it becomes an operation with an
-            /// Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`.
+            /// Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`.
             /// </summary>
             /// <param name="name">The name of the operation resource to be cancelled.</param>
             public virtual CancelRequest Cancel(string name)
@@ -3438,7 +3438,7 @@ namespace Google.Apis.DataLabeling.v1beta1
             /// `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check
             /// whether the cancellation succeeded or whether the operation completed despite cancellation. On
             /// successful cancellation, the operation is not deleted; instead, it becomes an operation with an
-            /// Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`.
+            /// Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`.
             /// </summary>
             public class CancelRequest : DataLabelingBaseServiceRequest<Google.Apis.DataLabeling.v1beta1.Data.GoogleProtobufEmpty>
             {
