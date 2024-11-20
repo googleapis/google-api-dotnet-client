15d09fa86feb82b74328be31cfa905efafc16402
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-20 01:08:19 -0800

    feat: Generate Google.Apis.Container.v1 version 1.68.0.3596

--- Src/Generated/Google.Apis.Container.v1/Google.Apis.Container.v1.cs
+++ Src/Generated/Google.Apis.Container.v1/Google.Apis.Container.v1.cs
@@ -8728,7 +8728,7 @@ namespace Google.Apis.Container.v1.Data
         /// The Linux kernel parameters to be applied to the nodes and all pods running on the nodes. The following
         /// parameters are supported. net.core.busy_poll net.core.busy_read net.core.netdev_max_backlog
         /// net.core.rmem_max net.core.wmem_default net.core.wmem_max net.core.optmem_max net.core.somaxconn
-        /// net.ipv4.tcp_rmem net.ipv4.tcp_wmem net.ipv4.tcp_tw_reuse
+        /// net.ipv4.tcp_rmem net.ipv4.tcp_wmem net.ipv4.tcp_tw_reuse kernel.shmmni kernel.shmmax kernel.shmall
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("sysctls")]
         public virtual System.Collections.Generic.IDictionary<string, string> Sysctls { get; set; }
