59aa671052a734ce732f110a47630693d22157c8
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-25 01:14:48 -0800

    feat: Generate Google.Apis.PubsubLite.v1 version 1.68.0.3607

--- Src/Generated/Google.Apis.PubsubLite.v1/Google.Apis.PubsubLite.v1.cs
+++ Src/Generated/Google.Apis.PubsubLite.v1/Google.Apis.PubsubLite.v1.cs
@@ -347,7 +347,7 @@ namespace Google.Apis.PubsubLite.v1
                     /// it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other
                     /// methods to check whether the cancellation succeeded or whether the operation completed despite
                     /// cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an
-                    /// operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to
+                    /// operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to
                     /// `Code.CANCELLED`.
                     /// </summary>
                     /// <param name="body">The body of the request.</param>
@@ -363,7 +363,7 @@ namespace Google.Apis.PubsubLite.v1
                     /// it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other
                     /// methods to check whether the cancellation succeeded or whether the operation completed despite
                     /// cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an
-                    /// operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to
+                    /// operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to
                     /// `Code.CANCELLED`.
                     /// </summary>
                     public class CancelRequest : PubsubLiteBaseServiceRequest<Google.Apis.PubsubLite.v1.Data.Empty>
