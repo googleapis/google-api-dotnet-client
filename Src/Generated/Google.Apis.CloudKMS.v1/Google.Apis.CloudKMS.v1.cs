e02c8caa84bca96204b561581ae87ea0bfc61867
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-25 01:12:26 -0800

    feat: Generate Google.Apis.CloudKMS.v1 version 1.68.0.3602

--- Src/Generated/Google.Apis.CloudKMS.v1/Google.Apis.CloudKMS.v1.cs
+++ Src/Generated/Google.Apis.CloudKMS.v1/Google.Apis.CloudKMS.v1.cs
@@ -6676,7 +6676,7 @@ namespace Google.Apis.CloudKMS.v1.Data
         /// Optional. An optional CRC32C checksum of the MacVerifyRequest.mac. If specified, KeyManagementService will
         /// verify the integrity of the received MacVerifyRequest.mac using this checksum. KeyManagementService will
         /// report an error if the checksum verification fails. If you receive a checksum error, your client should
-        /// verify that CRC32C(MacVerifyRequest.tag) is equal to MacVerifyRequest.mac_crc32c, and if so, perform a
+        /// verify that CRC32C(MacVerifyRequest.mac) is equal to MacVerifyRequest.mac_crc32c, and if so, perform a
         /// limited number of retries. A persistent mismatch may indicate an issue in your computation of the CRC32C
         /// checksum. Note: This field is defined as int64 for reasons of compatibility across different languages.
         /// However, it is a non-negative integer, which will never exceed 2^32-1, and can be safely downconverted to
