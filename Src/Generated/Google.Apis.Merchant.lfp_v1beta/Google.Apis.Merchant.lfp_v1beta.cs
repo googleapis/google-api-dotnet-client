f2fec95628ba1a9f40a0b361041cdf25685b0ac8
Amanda Tarafa Mas <atarafamas@google.com>
2024-11-25 01:13:33 -0800

    feat: Generate Google.Apis.Merchant.lfp_v1beta version 1.68.0.3610

--- Src/Generated/Google.Apis.Merchant.lfp_v1beta/Google.Apis.Merchant.lfp_v1beta.cs
+++ Src/Generated/Google.Apis.Merchant.lfp_v1beta/Google.Apis.Merchant.lfp_v1beta.cs
@@ -1109,7 +1109,10 @@ namespace Google.Apis.Merchant.lfp_v1beta.Data
 
         private object _expirationTime;
 
-        /// <summary>The product expiration time.</summary>
+        /// <summary>
+        /// The product expiration time. This field will not bet set if the notification is sent for a product deletion
+        /// event.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("expirationTime")]
         public virtual string ExpirationTimeRaw
         {
