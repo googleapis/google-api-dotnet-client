be2ed5731bcb828cf4fa89a113adae7a53adaac3
Amanda Tarafa Mas <atarafamas@google.com>
2024-12-02 01:07:20 -0800

    feat: Generate Google.Apis.CloudHealthcare.v1beta1 version 1.68.0.3606

--- Src/Generated/Google.Apis.CloudHealthcare.v1beta1/Google.Apis.CloudHealthcare.v1beta1.cs
+++ Src/Generated/Google.Apis.CloudHealthcare.v1beta1/Google.Apis.CloudHealthcare.v1beta1.cs
@@ -13741,7 +13741,7 @@ namespace Google.Apis.CloudHealthcare.v1beta1
                     /// it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other
                     /// methods to check whether the cancellation succeeded or whether the operation completed despite
                     /// cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an
-                    /// operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to
+                    /// operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to
                     /// `Code.CANCELLED`.
                     /// </summary>
                     /// <param name="body">The body of the request.</param>
@@ -13757,7 +13757,7 @@ namespace Google.Apis.CloudHealthcare.v1beta1
                     /// it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other
                     /// methods to check whether the cancellation succeeded or whether the operation completed despite
                     /// cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an
-                    /// operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to
+                    /// operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to
                     /// `Code.CANCELLED`.
                     /// </summary>
                     public class CancelRequest : CloudHealthcareBaseServiceRequest<Google.Apis.CloudHealthcare.v1beta1.Data.Empty>
