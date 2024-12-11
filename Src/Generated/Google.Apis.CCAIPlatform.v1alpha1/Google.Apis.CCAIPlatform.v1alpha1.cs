296d496a5914da65d52dc0a7c7883f56d1a4340c
Amanda Tarafa Mas <atarafamas@google.com>
2024-12-11 01:07:53 -0800

    feat: Generate Google.Apis.CCAIPlatform.v1alpha1 version 1.68.0.3626

--- Src/Generated/Google.Apis.CCAIPlatform.v1alpha1/Google.Apis.CCAIPlatform.v1alpha1.cs
+++ Src/Generated/Google.Apis.CCAIPlatform.v1alpha1/Google.Apis.CCAIPlatform.v1alpha1.cs
@@ -724,7 +724,7 @@ namespace Google.Apis.CCAIPlatform.v1alpha1
                 /// returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to
                 /// check whether the cancellation succeeded or whether the operation completed despite cancellation. On
                 /// successful cancellation, the operation is not deleted; instead, it becomes an operation with an
-                /// Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`.
+                /// Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`.
                 /// </summary>
                 /// <param name="body">The body of the request.</param>
                 /// <param name="name">The name of the operation resource to be cancelled.</param>
@@ -739,7 +739,7 @@ namespace Google.Apis.CCAIPlatform.v1alpha1
                 /// returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to
                 /// check whether the cancellation succeeded or whether the operation completed despite cancellation. On
                 /// successful cancellation, the operation is not deleted; instead, it becomes an operation with an
-                /// Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`.
+                /// Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`.
                 /// </summary>
                 public class CancelRequest : CCAIPlatformBaseServiceRequest<Google.Apis.CCAIPlatform.v1alpha1.Data.Empty>
                 {
@@ -1429,8 +1429,8 @@ namespace Google.Apis.CCAIPlatform.v1alpha1.Data
 
         /// <summary>
         /// Output only. Identifies whether the user has requested cancellation of the operation. Operations that have
-        /// been cancelled successfully have Operation.error value with a google.rpc.Status.code of 1, corresponding to
-        /// `Code.CANCELLED`.
+        /// been cancelled successfully have google.longrunning.Operation.error value with a google.rpc.Status.code of
+        /// `1`, corresponding to `Code.CANCELLED`.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("cancelRequested")]
         public virtual System.Nullable<bool> CancelRequested { get; set; }
