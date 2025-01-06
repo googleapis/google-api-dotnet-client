c751646766e8bc5495b8f7738948959e8eabf8b9
Amanda Tarafa Mas <atarafamas@google.com>
2025-01-06 01:08:36 -0800

    feat: Generate Google.Apis.SQLAdmin.v1beta4 version 1.68.0.3631

--- Src/Generated/Google.Apis.SQLAdmin.v1beta4/Google.Apis.SQLAdmin.v1beta4.cs
+++ Src/Generated/Google.Apis.SQLAdmin.v1beta4/Google.Apis.SQLAdmin.v1beta4.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7660,7 +7660,6 @@ namespace Google.Apis.SQLAdmin.v1beta4.Data
         [Newtonsoft.Json.JsonPropertyAttribute("subOperationType")]
         public virtual SqlSubOperationType SubOperationType { get; set; }
 
-        /// <summary>Name of the database instance related to this operation.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("targetId")]
         public virtual string TargetId { get; set; }
 
