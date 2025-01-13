07c241b0133c076dc57dd6f4e7f1fe8014132d59
Amanda Tarafa Mas <atarafamas@google.com>
2025-01-13 01:14:23 -0800

    feat: Generate Google.Apis.Parallelstore.v1beta version 1.68.0.3654

--- Src/Generated/Google.Apis.Parallelstore.v1beta/Google.Apis.Parallelstore.v1beta.cs
+++ Src/Generated/Google.Apis.Parallelstore.v1beta/Google.Apis.Parallelstore.v1beta.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1402,10 +1402,6 @@ namespace Google.Apis.Parallelstore.v1beta.Data
             get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(CreateTimeRaw);
             set => CreateTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
         }
-
-        /// <summary>Output only. The version of DAOS software running in the instance.</summary>
-        [Newtonsoft.Json.JsonPropertyAttribute("daosVersion")]
-        public virtual string DaosVersion { get; set; }
 
         /// <summary>
         /// Optional. The deployment type of the instance. Allowed values are: * `SCRATCH`: the instance is a scratch
