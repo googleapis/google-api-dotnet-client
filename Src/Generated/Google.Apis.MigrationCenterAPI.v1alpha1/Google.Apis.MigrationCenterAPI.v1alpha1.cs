c07c0b751ba9b164a4f0ef5685bfd5216eab5ccf
Amanda Tarafa Mas <atarafamas@google.com>
2025-01-20 01:11:51 -0800

    feat: Generate Google.Apis.MigrationCenterAPI.v1alpha1 version 1.68.0.3667

--- Src/Generated/Google.Apis.MigrationCenterAPI.v1alpha1/Google.Apis.MigrationCenterAPI.v1alpha1.cs
+++ Src/Generated/Google.Apis.MigrationCenterAPI.v1alpha1/Google.Apis.MigrationCenterAPI.v1alpha1.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11654,7 +11654,7 @@ namespace Google.Apis.MigrationCenterAPI.v1alpha1.Data
         public virtual string CommitmentPlan { get; set; }
 
         /// <summary>
-        /// Compute Engine preferences concern insights and recommendations for Compute Engine target.
+        /// Optional. Compute Engine preferences concern insights and recommendations for Compute Engine target.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("computeEnginePreferences")]
         public virtual ComputeEnginePreferences ComputeEnginePreferences { get; set; }
