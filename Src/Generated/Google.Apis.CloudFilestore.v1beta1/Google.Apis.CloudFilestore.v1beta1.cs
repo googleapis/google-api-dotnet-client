c7a701d167afc6854bb757e5d23bcb5804aa4a0e
Amanda Tarafa Mas <atarafamas@google.com>
2025-02-10 01:11:51 -0800

    feat: Generate Google.Apis.CloudFilestore.v1beta1 version 1.69.0.3681

--- Src/Generated/Google.Apis.CloudFilestore.v1beta1/Google.Apis.CloudFilestore.v1beta1.cs
+++ Src/Generated/Google.Apis.CloudFilestore.v1beta1/Google.Apis.CloudFilestore.v1beta1.cs
@@ -4002,11 +4002,11 @@ namespace Google.Apis.CloudFilestore.v1beta1.Data
         public virtual FixedIOPS FixedIops { get; set; }
 
         /// <summary>
-        /// Provision IOPS dynamically based on the capacity of the instance. Provisioned read IOPS will be calculated
-        /// by multiplying the capacity of the instance in TiB by the `iops_per_tb` value. For example, for a 2 TiB
-        /// instance with an `iops_per_tb` value of 17000 the provisioned read IOPS will be 34000. If the calculated
-        /// value is outside the supported range for the instance's capacity during instance creation, instance creation
-        /// will fail with an `InvalidArgument` error. Similarly, if an instance capacity update would result in a value
+        /// Provision IOPS dynamically based on the capacity of the instance. Provisioned IOPS will be calculated by
+        /// multiplying the capacity of the instance in TiB by the `iops_per_tb` value. For example, for a 2 TiB
+        /// instance with an `iops_per_tb` value of 17000 the provisioned IOPS will be 34000. If the calculated value is
+        /// outside the supported range for the instance's capacity during instance creation, instance creation will
+        /// fail with an `InvalidArgument` error. Similarly, if an instance capacity update would result in a value
         /// outside the supported range, the update will fail with an `InvalidArgument` error.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("iopsPerTb")]
