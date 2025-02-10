ee84c57d6f770d68846c3f17444b77eeeb13edf0
Amanda Tarafa Mas <atarafamas@google.com>
2025-02-10 01:13:05 -0800

    feat: Generate Google.Apis.CloudRetail.v2beta version 1.69.0.3682

--- Src/Generated/Google.Apis.CloudRetail.v2beta/Google.Apis.CloudRetail.v2beta.cs
+++ Src/Generated/Google.Apis.CloudRetail.v2beta/Google.Apis.CloudRetail.v2beta.cs
@@ -13061,8 +13061,11 @@ namespace Google.Apis.CloudRetail.v2beta.Data
     public class GoogleCloudRetailV2betaSearchRequestTileNavigationSpec : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
-        /// This field specifies the tiles which are already clicked in client side. NOTE: This field is not being used
-        /// for filtering search products. Client side should also put all the applied tiles in SearchRequest.filter.
+        /// This optional field specifies the tiles which are already clicked in client side. While the feature works
+        /// without this field set, particularly for an initial query, it is highly recommended to set this field
+        /// because it can improve the quality of the search response and removes possible duplicate tiles. NOTE: This
+        /// field is not being used for filtering search products. Client side should also put all the applied tiles in
+        /// SearchRequest.filter.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("appliedTiles")]
         public virtual System.Collections.Generic.IList<GoogleCloudRetailV2betaTile> AppliedTiles { get; set; }
