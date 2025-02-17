63e4b1e6661e1a6c95134657d1d8a1717bc1ea9f
Amanda Tarafa Mas <atarafamas@google.com>
2025-02-17 01:10:43 -0800

    feat: Generate Google.Apis.Container.v1 version 1.69.0.3687

--- Src/Generated/Google.Apis.Container.v1/Google.Apis.Container.v1.cs
+++ Src/Generated/Google.Apis.Container.v1/Google.Apis.Container.v1.cs
@@ -6561,6 +6561,17 @@ namespace Google.Apis.Container.v1.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>AutopilotConfig contains configuration of autopilot feature for this nodepool.</summary>
+    public class AutopilotConfig : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>Denotes that nodes belonging to this node pool are Autopilot nodes.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("enabled")]
+        public virtual System.Nullable<bool> Enabled { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>AutoprovisioningNodePoolDefaults contains defaults for a node pool created by NAP.</summary>
     public class AutoprovisioningNodePoolDefaults : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -9809,6 +9820,13 @@ namespace Google.Apis.Container.v1.Data
     /// </summary>
     public class NodePool : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>
+        /// Specifies the autopilot configuration for this node pool. This field is exclusively reserved for Cluster
+        /// Autoscaler to implement go/gke-managed-nodes-ccc-api
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("autopilotConfig")]
+        public virtual AutopilotConfig AutopilotConfig { get; set; }
+
         /// <summary>
         /// Autoscaler configuration for this NodePool. Autoscaler is enabled only if a valid configuration is present.
         /// </summary>
