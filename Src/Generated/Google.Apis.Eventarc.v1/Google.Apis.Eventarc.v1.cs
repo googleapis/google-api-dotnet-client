7ced46b1bb41b1a04f326e8ad17b7f9fef0900f7
Amanda Tarafa Mas <atarafamas@google.com>
2025-02-17 01:11:35 -0800

    feat: Generate Google.Apis.Eventarc.v1 version 1.69.0.3692

--- Src/Generated/Google.Apis.Eventarc.v1/Google.Apis.Eventarc.v1.cs
+++ Src/Generated/Google.Apis.Eventarc.v1/Google.Apis.Eventarc.v1.cs
@@ -6880,7 +6880,7 @@ namespace Google.Apis.Eventarc.v1.Data
         public virtual string MessageBindingTemplate { get; set; }
 
         /// <summary>
-        /// Required. The URI of the HTTP enpdoint. The value must be a RFC2396 URI string. Examples:
+        /// Required. The URI of the HTTP endpoint. The value must be a RFC2396 URI string. Examples:
         /// `https://svc.us-central1.p.local:8080/route`. Only the HTTPS protocol is supported.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("uri")]
@@ -7134,7 +7134,7 @@ namespace Google.Apis.Eventarc.v1.Data
     public class HttpEndpoint : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
-        /// Required. The URI of the HTTP enpdoint. The value must be a RFC2396 URI string. Examples:
+        /// Required. The URI of the HTTP endpoint. The value must be a RFC2396 URI string. Examples:
         /// `http://10.10.10.8:80/route`, `http://svc.us-central1.p.local:8080/`. Only HTTP and HTTPS protocols are
         /// supported. The host can be either a static IP addressable from the VPC specified by the network config, or
         /// an internal DNS hostname of the service resolvable via Cloud DNS.
