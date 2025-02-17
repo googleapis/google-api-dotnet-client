f92322e6c822dd8ead967066048ec9a858768cc0
Amanda Tarafa Mas <atarafamas@google.com>
2025-02-17 01:12:45 -0800

    feat: Generate Google.Apis.OnDemandScanning.v1 version 1.69.0.3693

--- Src/Generated/Google.Apis.OnDemandScanning.v1/Google.Apis.OnDemandScanning.v1.cs
+++ Src/Generated/Google.Apis.OnDemandScanning.v1/Google.Apis.OnDemandScanning.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1046,6 +1046,25 @@ namespace Google.Apis.OnDemandScanning.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("signatures")]
         public virtual System.Collections.Generic.IList<Signature> Signatures { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>BaseImage describes a base image of a container image.</summary>
+    public class BaseImage : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>The name of the base image.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("name")]
+        public virtual string Name { get; set; }
+
+        /// <summary>The number of layers that the base image is composed of.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("numLayers")]
+        public virtual string NumLayers { get; set; }
+
+        /// <summary>The repository name in which the base image is from.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("repository")]
+        public virtual string Repository { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
@@ -2336,6 +2355,35 @@ namespace Google.Apis.OnDemandScanning.v1.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>
+    /// Details about the layer a package was found in. This should be the same as the LayerDetails message in
+    /// google3/third_party/scalibr/binary/proto/scan_result.proto.
+    /// </summary>
+    public class LayerDetails : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>The base images the layer is found within.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("baseImages")]
+        public virtual System.Collections.Generic.IList<BaseImage> BaseImages { get; set; }
+
+        /// <summary>
+        /// The layer build command that was used to build the layer. This may not be found in all layers depending on
+        /// how the container image is built.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("command")]
+        public virtual string Command { get; set; }
+
+        /// <summary>The diff ID (sha256 hash) of the layer in the container image.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("diffId")]
+        public virtual string DiffId { get; set; }
+
+        /// <summary>The index of the layer in the container image.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("index")]
+        public virtual System.Nullable<int> Index { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>License information.</summary>
     public class License : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -2805,6 +2853,9 @@ namespace Google.Apis.OnDemandScanning.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("hashDigest")]
         public virtual string HashDigest { get; set; }
+
+        [Newtonsoft.Json.JsonPropertyAttribute("layerDetails")]
+        public virtual LayerDetails LayerDetails { get; set; }
 
         /// <summary>
         /// The list of licenses found that are related to a given package. Note that licenses may also be stored on the
