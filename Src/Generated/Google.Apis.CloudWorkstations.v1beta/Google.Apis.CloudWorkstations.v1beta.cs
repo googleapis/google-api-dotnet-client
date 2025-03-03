1cb54ba843d00f31e3ceba02fa84b18185eccc6f
Amanda Tarafa Mas <atarafamas@google.com>
2025-03-03 01:12:22 -0800

    feat: Generate Google.Apis.CloudWorkstations.v1beta version 1.69.0.3702

--- Src/Generated/Google.Apis.CloudWorkstations.v1beta/Google.Apis.CloudWorkstations.v1beta.cs
+++ Src/Generated/Google.Apis.CloudWorkstations.v1beta/Google.Apis.CloudWorkstations.v1beta.cs
@@ -3053,7 +3053,10 @@ namespace Google.Apis.CloudWorkstations.v1beta.Data
         /// <summary>
         /// Optional. Name of the disk image to use as the source for the disk. Must be empty if source_snapshot is set.
         /// Updating source_image will update content in the ephemeral directory after the workstation is restarted.
-        /// This field is mutable.
+        /// Only file systems supported by Container-Optimized OS (COS) are explicitly supported. For a list of
+        /// supported file systems, please refer to the [COS
+        /// documentation](https://cloud.google.com/container-optimized-os/docs/concepts/supported-filesystems). This
+        /// field is mutable.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("sourceImage")]
         public virtual string SourceImage { get; set; }
@@ -3061,7 +3064,10 @@ namespace Google.Apis.CloudWorkstations.v1beta.Data
         /// <summary>
         /// Optional. Name of the snapshot to use as the source for the disk. Must be empty if source_image is set. Must
         /// be empty if read_only is false. Updating source_snapshot will update content in the ephemeral directory
-        /// after the workstation is restarted. This field is mutable.
+        /// after the workstation is restarted. Only file systems supported by Container-Optimized OS (COS) are
+        /// explicitly supported. For a list of supported file systems, please refer to the [COS
+        /// documentation](https://cloud.google.com/container-optimized-os/docs/concepts/supported-filesystems). This
+        /// field is mutable.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("sourceSnapshot")]
         public virtual string SourceSnapshot { get; set; }
@@ -3704,7 +3710,7 @@ namespace Google.Apis.CloudWorkstations.v1beta.Data
         public virtual System.Nullable<bool> EnablePrivateEndpoint { get; set; }
 
         /// <summary>
-        /// Output only. Service attachment URI for the workstation cluster. The service attachemnt is created when
+        /// Output only. Service attachment URI for the workstation cluster. The service attachment is created when
         /// private endpoint is enabled. To access workstations in the workstation cluster, configure access to the
         /// managed service using [Private Service
         /// Connect](https://cloud.google.com/vpc/docs/configure-private-service-connect-services).
