2f91ff09d6e0de7ad5974a95797ee9301c859263
Amanda Tarafa Mas <atarafamas@google.com>
2025-03-05 01:09:54 -0800

    feat: Generate Google.Apis.ServiceConsumerManagement.v1beta1 version 1.69.0.3710

--- Src/Generated/Google.Apis.ServiceConsumerManagement.v1beta1/Google.Apis.ServiceConsumerManagement.v1beta1.cs
+++ Src/Generated/Google.Apis.ServiceConsumerManagement.v1beta1/Google.Apis.ServiceConsumerManagement.v1beta1.cs
@@ -4378,20 +4378,14 @@ namespace Google.Apis.ServiceConsumerManagement.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>
-    /// Usage configuration rules for the service. NOTE: Under development. Use this rule to configure unregistered
-    /// calls for the service. Unregistered calls are calls that do not contain consumer project identity. (Example:
-    /// calls that do not contain an API key). By default, API methods do not allow unregistered calls, and each method
-    /// call must be identified by a consumer project identity. Use this rule to allow/disallow unregistered calls.
-    /// Example of an API that wants to allow unregistered calls for entire service. usage: rules: - selector: "*"
-    /// allow_unregistered_calls: true Example of a method that wants to allow unregistered calls. usage: rules: -
-    /// selector: "google.example.library.v1.LibraryService.CreateBook" allow_unregistered_calls: true
-    /// </summary>
+    /// <summary>Usage configuration rules for the service.</summary>
     public class UsageRule : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
-        /// If true, the selected method allows unregistered calls, e.g. calls that don't identify any user or
-        /// application.
+        ///  Use this rule to configure unregistered calls for the service. Unregistered calls are calls that do not
+        /// contain consumer project identity. (Example: calls that do not contain an API key). WARNING: By default, API
+        /// methods do not allow unregistered calls, and each method call must be identified by a consumer project
+        /// identity.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("allowUnregisteredCalls")]
         public virtual System.Nullable<bool> AllowUnregisteredCalls { get; set; }
