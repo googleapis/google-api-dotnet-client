cf5f0b909dcc1bbf6ca7ed75c539efcf539e5807
Amanda Tarafa Mas <atarafamas@google.com>
2025-03-17 01:14:39 -0700

    feat: Generate Google.Apis.MapsPlaces.v1 version 1.69.0.3723

--- Src/Generated/Google.Apis.MapsPlaces.v1/Google.Apis.MapsPlaces.v1.cs
+++ Src/Generated/Google.Apis.MapsPlaces.v1/Google.Apis.MapsPlaces.v1.cs
@@ -1721,6 +1721,10 @@ namespace Google.Apis.MapsPlaces.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("plusCode")]
         public virtual GoogleMapsPlacesV1PlacePlusCode PlusCode { get; set; }
 
+        /// <summary>The address in postal address format.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("postalAddress")]
+        public virtual GoogleTypePostalAddress PostalAddress { get; set; }
+
         /// <summary>Price level of the place.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("priceLevel")]
         public virtual string PriceLevel { get; set; }
@@ -2327,11 +2331,11 @@ namespace Google.Apis.MapsPlaces.v1.Data
     }
 
     /// <summary>
-    /// Sub destinations are specific places associated with a main place. These provide more specific destinations for
-    /// users who are searching for a large or complex place, like an airport, national park, university, or stadium.
-    /// For example, sub destinations at an airport might include associated terminals and parking lots. Sub
-    /// destinations return the place id and place resource name, which can be used in subsequent Place Details (New)
-    /// requests to fetch richer details, including the sub destination's displayName and location.
+    /// Sub-destinations are specific places associated with a main place. These provide more specific destinations for
+    /// users who are searching inside a large or complex place, like an airport, national park, university, or stadium.
+    /// For example, sub-destinations at an airport might include associated terminals and parking lots.
+    /// Sub-destinations return the place ID and place resource name, which can be used in subsequent Place Details
+    /// (new) requests to fetch richer details, including the sub-destination's display name and location.
     /// </summary>
     public class GoogleMapsPlacesV1PlaceSubDestination : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -3088,6 +3092,117 @@ namespace Google.Apis.MapsPlaces.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("units")]
         public virtual System.Nullable<long> Units { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a postal address. For example for postal delivery or payments addresses. Given a postal address, a
+    /// postal service can deliver items to a premise, P.O. Box or similar. It is not intended to model geographical
+    /// locations (roads, towns, mountains). In typical usage an address would be created by user input or from
+    /// importing existing data, depending on the type of process. Advice on address input / editing: - Use an
+    /// internationalization-ready address widget such as https://github.com/google/libaddressinput) - Users should not
+    /// be presented with UI elements for input or editing of fields outside countries where that field is used. For
+    /// more guidance on how to use this schema, see: https://support.google.com/business/answer/6397478
+    /// </summary>
+    public class GoogleTypePostalAddress : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>
+        /// Unstructured address lines describing the lower levels of an address. Because values in address_lines do not
+        /// have type information and may sometimes contain multiple values in a single field (For example "Austin,
+        /// TX"), it is important that the line order is clear. The order of address lines should be "envelope order"
+        /// for the country/region of the address. In places where this can vary (For example Japan), address_language
+        /// is used to make it explicit (For example "ja" for large-to-small ordering and "ja-Latn" or "en" for
+        /// small-to-large). This way, the most specific line of an address can be selected based on the language. The
+        /// minimum permitted structural representation of an address consists of a region_code with all remaining
+        /// information placed in the address_lines. It would be possible to format such an address very approximately
+        /// without geocoding, but no semantic reasoning could be made about any of the address components until it was
+        /// at least partially resolved. Creating an address only containing a region_code and address_lines, and then
+        /// geocoding is the recommended way to handle completely unstructured addresses (as opposed to guessing which
+        /// parts of the address should be localities or administrative areas).
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("addressLines")]
+        public virtual System.Collections.Generic.IList<string> AddressLines { get; set; }
+
+        /// <summary>
+        /// Optional. Highest administrative subdivision which is used for postal addresses of a country or region. For
+        /// example, this can be a state, a province, an oblast, or a prefecture. Specifically, for Spain this is the
+        /// province and not the autonomous community (For example "Barcelona" and not "Catalonia"). Many countries
+        /// don't use an administrative area in postal addresses. For example in Switzerland this should be left
+        /// unpopulated.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("administrativeArea")]
+        public virtual string AdministrativeArea { get; set; }
+
+        /// <summary>
+        /// Optional. BCP-47 language code of the contents of this address (if known). This is often the UI language of
+        /// the input form or is expected to match one of the languages used in the address' country/region, or their
+        /// transliterated equivalents. This can affect formatting in certain countries, but is not critical to the
+        /// correctness of the data and will never affect any validation or other non-formatting related operations. If
+        /// this value is not known, it should be omitted (rather than specifying a possibly incorrect default).
+        /// Examples: "zh-Hant", "ja", "ja-Latn", "en".
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("languageCode")]
+        public virtual string LanguageCode { get; set; }
+
+        /// <summary>
+        /// Optional. Generally refers to the city/town portion of the address. Examples: US city, IT comune, UK post
+        /// town. In regions of the world where localities are not well defined or do not fit into this structure well,
+        /// leave locality empty and use address_lines.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("locality")]
+        public virtual string Locality { get; set; }
+
+        /// <summary>Optional. The name of the organization at the address.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("organization")]
+        public virtual string Organization { get; set; }
+
+        /// <summary>
+        /// Optional. Postal code of the address. Not all countries use or require postal codes to be present, but where
+        /// they are used, they may trigger additional validation with other parts of the address (For example state/zip
+        /// validation in the U.S.A.).
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("postalCode")]
+        public virtual string PostalCode { get; set; }
+
+        /// <summary>
+        /// Optional. The recipient at the address. This field may, under certain circumstances, contain multiline
+        /// information. For example, it might contain "care of" information.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("recipients")]
+        public virtual System.Collections.Generic.IList<string> Recipients { get; set; }
+
+        /// <summary>
+        /// Required. CLDR region code of the country/region of the address. This is never inferred and it is up to the
+        /// user to ensure the value is correct. See https://cldr.unicode.org/ and
+        /// https://www.unicode.org/cldr/charts/30/supplemental/territory_information.html for details. Example: "CH"
+        /// for Switzerland.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("regionCode")]
+        public virtual string RegionCode { get; set; }
+
+        /// <summary>
+        /// The schema revision of the `PostalAddress`. This must be set to 0, which is the latest revision. All new
+        /// revisions **must** be backward compatible with old revisions.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("revision")]
+        public virtual System.Nullable<int> Revision { get; set; }
+
+        /// <summary>
+        /// Optional. Additional, country-specific, sorting code. This is not used in most regions. Where it is used,
+        /// the value is either a string like "CEDEX", optionally followed by a number (For example "CEDEX 7"), or just
+        /// a number alone, representing the "sector code" (Jamaica), "delivery area indicator" (Malawi) or "post office
+        /// indicator" (For example Côte d'Ivoire).
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("sortingCode")]
+        public virtual string SortingCode { get; set; }
+
+        /// <summary>
+        /// Optional. Sublocality of the address. For example, this can be neighborhoods, boroughs, districts.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("sublocality")]
+        public virtual string Sublocality { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
