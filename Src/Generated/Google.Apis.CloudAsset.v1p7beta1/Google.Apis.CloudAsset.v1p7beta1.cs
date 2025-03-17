661929ef86b67890c825d9f992de2871e6a6e492
Amanda Tarafa Mas <atarafamas@google.com>
2025-03-17 01:12:05 -0700

    feat: Generate Google.Apis.CloudAsset.v1p7beta1 version 1.69.0.3718

--- Src/Generated/Google.Apis.CloudAsset.v1p7beta1/Google.Apis.CloudAsset.v1p7beta1.cs
+++ Src/Generated/Google.Apis.CloudAsset.v1p7beta1/Google.Apis.CloudAsset.v1p7beta1.cs
@@ -1715,6 +1715,13 @@ namespace Google.Apis.CloudAsset.v1p7beta1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("resources")]
         public virtual System.Collections.Generic.IList<string> Resources { get; set; }
 
+        /// <summary>
+        /// IAM roles that represent the set of operations that the sources specified in the corresponding EgressFrom.
+        /// are allowed to perform in this ServicePerimeter.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("roles")]
+        public virtual System.Collections.Generic.IList<string> Roles { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
@@ -1834,6 +1841,13 @@ namespace Google.Apis.CloudAsset.v1p7beta1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("resources")]
         public virtual System.Collections.Generic.IList<string> Resources { get; set; }
+
+        /// <summary>
+        /// IAM roles that represent the set of operations that the sources specified in the corresponding IngressFrom
+        /// are allowed to perform in this ServicePerimeter.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("roles")]
+        public virtual System.Collections.Generic.IList<string> Roles { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
