192ce53341f789b74830e85b502ce147210f2f96
Amanda Tarafa Mas <atarafamas@google.com>
2025-03-31 01:14:49 -0700

    feat: Generate Google.Apis.CloudTalentSolution.v3 version 1.69.0.3737

--- Src/Generated/Google.Apis.CloudTalentSolution.v3/Google.Apis.CloudTalentSolution.v3.cs
+++ Src/Generated/Google.Apis.CloudTalentSolution.v3/Google.Apis.CloudTalentSolution.v3.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -3135,39 +3135,38 @@ namespace Google.Apis.CloudTalentSolution.v3.Data
     }
 
     /// <summary>
-    /// Represents a postal address. For example for postal delivery or payments addresses. Given a postal address, a
-    /// postal service can deliver items to a premise, P.O. Box or similar. It is not intended to model geographical
-    /// locations (roads, towns, mountains). In typical usage an address would be created by user input or from
-    /// importing existing data, depending on the type of process. Advice on address input / editing: - Use an
-    /// internationalization-ready address widget such as https://github.com/google/libaddressinput) - Users should not
+    /// Represents a postal address (for example, for postal delivery or payments addresses). Given a postal address, a
+    /// postal service can deliver items to a premise, P.O. box or similar. It is not intended to model geographical
+    /// locations (roads, towns, mountains). In typical usage, an address would be created by user input or from
+    /// importing existing data, depending on the type of process. Advice on address input or editing: - Use an
+    /// internationalization-ready address widget such as https://github.com/google/libaddressinput. - Users should not
     /// be presented with UI elements for input or editing of fields outside countries where that field is used. For
-    /// more guidance on how to use this schema, see: https://support.google.com/business/answer/6397478
+    /// more guidance on how to use this schema, see: https://support.google.com/business/answer/6397478.
     /// </summary>
     public class PostalAddress : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
-        /// Unstructured address lines describing the lower levels of an address. Because values in address_lines do not
-        /// have type information and may sometimes contain multiple values in a single field (For example "Austin,
+        /// Unstructured address lines describing the lower levels of an address. Because values in `address_lines` do
+        /// not have type information and may sometimes contain multiple values in a single field (for example, "Austin,
         /// TX"), it is important that the line order is clear. The order of address lines should be "envelope order"
-        /// for the country/region of the address. In places where this can vary (For example Japan), address_language
-        /// is used to make it explicit (For example "ja" for large-to-small ordering and "ja-Latn" or "en" for
-        /// small-to-large). This way, the most specific line of an address can be selected based on the language. The
-        /// minimum permitted structural representation of an address consists of a region_code with all remaining
-        /// information placed in the address_lines. It would be possible to format such an address very approximately
-        /// without geocoding, but no semantic reasoning could be made about any of the address components until it was
-        /// at least partially resolved. Creating an address only containing a region_code and address_lines, and then
-        /// geocoding is the recommended way to handle completely unstructured addresses (as opposed to guessing which
-        /// parts of the address should be localities or administrative areas).
+        /// for the country or region of the address. In places where this can vary (for example, Japan),
+        /// `address_language` is used to make it explicit (for example, "ja" for large-to-small ordering and "ja-Latn"
+        /// or "en" for small-to-large). In this way, the most specific line of an address can be selected based on the
+        /// language. The minimum permitted structural representation of an address consists of a `region_code` with all
+        /// remaining information placed in the `address_lines`. It would be possible to format such an address very
+        /// approximately without geocoding, but no semantic reasoning could be made about any of the address components
+        /// until it was at least partially resolved. Creating an address only containing a `region_code` and
+        /// `address_lines` and then geocoding is the recommended way to handle completely unstructured addresses (as
+        /// opposed to guessing which parts of the address should be localities or administrative areas).
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("addressLines")]
         public virtual System.Collections.Generic.IList<string> AddressLines { get; set; }
 
         /// <summary>
         /// Optional. Highest administrative subdivision which is used for postal addresses of a country or region. For
-        /// example, this can be a state, a province, an oblast, or a prefecture. Specifically, for Spain this is the
-        /// province and not the autonomous community (For example "Barcelona" and not "Catalonia"). Many countries
-        /// don't use an administrative area in postal addresses. For example in Switzerland this should be left
-        /// unpopulated.
+        /// example, this can be a state, a province, an oblast, or a prefecture. For Spain, this is the province and
+        /// not the autonomous community (for example, "Barcelona" and not "Catalonia"). Many countries don't use an
+        /// administrative area in postal addresses. For example, in Switzerland, this should be left unpopulated.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("administrativeArea")]
         public virtual string AdministrativeArea { get; set; }
@@ -3184,9 +3183,9 @@ namespace Google.Apis.CloudTalentSolution.v3.Data
         public virtual string LanguageCode { get; set; }
 
         /// <summary>
-        /// Optional. Generally refers to the city/town portion of the address. Examples: US city, IT comune, UK post
+        /// Optional. Generally refers to the city or town portion of the address. Examples: US city, IT comune, UK post
         /// town. In regions of the world where localities are not well defined or do not fit into this structure well,
-        /// leave locality empty and use address_lines.
+        /// leave `locality` empty and use `address_lines`.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("locality")]
         public virtual string Locality { get; set; }
@@ -3197,8 +3196,8 @@ namespace Google.Apis.CloudTalentSolution.v3.Data
 
         /// <summary>
         /// Optional. Postal code of the address. Not all countries use or require postal codes to be present, but where
-        /// they are used, they may trigger additional validation with other parts of the address (For example state/zip
-        /// validation in the U.S.A.).
+        /// they are used, they may trigger additional validation with other parts of the address (for example, state or
+        /// zip code validation in the United States).
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("postalCode")]
         public virtual string PostalCode { get; set; }
@@ -3228,15 +3227,15 @@ namespace Google.Apis.CloudTalentSolution.v3.Data
 
         /// <summary>
         /// Optional. Additional, country-specific, sorting code. This is not used in most regions. Where it is used,
-        /// the value is either a string like "CEDEX", optionally followed by a number (For example "CEDEX 7"), or just
+        /// the value is either a string like "CEDEX", optionally followed by a number (for example, "CEDEX 7"), or just
         /// a number alone, representing the "sector code" (Jamaica), "delivery area indicator" (Malawi) or "post office
-        /// indicator" (For example Côte d'Ivoire).
+        /// indicator" (Côte d'Ivoire).
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("sortingCode")]
         public virtual string SortingCode { get; set; }
 
         /// <summary>
-        /// Optional. Sublocality of the address. For example, this can be neighborhoods, boroughs, districts.
+        /// Optional. Sublocality of the address. For example, this can be a neighborhood, borough, or district.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("sublocality")]
         public virtual string Sublocality { get; set; }
