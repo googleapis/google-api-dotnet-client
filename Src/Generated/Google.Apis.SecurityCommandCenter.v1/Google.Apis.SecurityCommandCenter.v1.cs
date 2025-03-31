5d516449c134ce122ac62bf8b54a7792da1958bc
Amanda Tarafa Mas <atarafamas@google.com>
2025-03-31 01:16:50 -0700

    feat: Generate Google.Apis.SecurityCommandCenter.v1 version 1.69.0.3737

--- Src/Generated/Google.Apis.SecurityCommandCenter.v1/Google.Apis.SecurityCommandCenter.v1.cs
+++ Src/Generated/Google.Apis.SecurityCommandCenter.v1/Google.Apis.SecurityCommandCenter.v1.cs
@@ -15245,6 +15245,17 @@ namespace Google.Apis.SecurityCommandCenter.v1.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>Details about resources affected by this finding.</summary>
+    public class AffectedResources : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>The count of resources affected by the finding.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("count")]
+        public virtual System.Nullable<long> Count { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>Allowed IP rule.</summary>
     public class Allowed : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -17449,6 +17460,10 @@ namespace Google.Apis.SecurityCommandCenter.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("access")]
         public virtual Access Access { get; set; }
 
+        /// <summary>AffectedResources associated with the finding.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("affectedResources")]
+        public virtual AffectedResources AffectedResources { get; set; }
+
         /// <summary>Represents an application associated with the finding.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("application")]
         public virtual Application Application { get; set; }
@@ -19466,6 +19481,17 @@ namespace Google.Apis.SecurityCommandCenter.v1.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>Details about resources affected by this finding.</summary>
+    public class GoogleCloudSecuritycenterV2AffectedResources : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>The count of resources affected by the finding.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("count")]
+        public virtual System.Nullable<long> Count { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>Allowed IP rule.</summary>
     public class GoogleCloudSecuritycenterV2Allowed : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -21271,6 +21297,10 @@ namespace Google.Apis.SecurityCommandCenter.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("access")]
         public virtual GoogleCloudSecuritycenterV2Access Access { get; set; }
+
+        /// <summary>AffectedResources associated with the finding.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("affectedResources")]
+        public virtual GoogleCloudSecuritycenterV2AffectedResources AffectedResources { get; set; }
 
         /// <summary>Represents an application associated with the finding.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("application")]
