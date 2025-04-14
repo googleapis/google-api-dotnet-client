e195a67533430f1b39c7adacbef21421ba0ecbad
Amanda Tarafa Mas <atarafamas@google.com>
2025-04-14 01:06:44 -0700

    feat: Generate Google.Apis.HangoutsChat.v1 version 1.69.0.3748

--- Src/Generated/Google.Apis.HangoutsChat.v1/Google.Apis.HangoutsChat.v1.cs
+++ Src/Generated/Google.Apis.HangoutsChat.v1/Google.Apis.HangoutsChat.v1.cs
@@ -1314,16 +1314,23 @@ namespace Google.Apis.HangoutsChat.v1
             /// Preview](https://developers.google.com/workspace/preview) - [User
             /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-user) You can
             /// authenticate and authorize this method with administrator privileges by setting the `use_admin_access`
-            /// field in the request.
+            /// field in the request. To delete memberships for space managers, the requester must be a space manager.
+            /// If you're using [app
+            /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-app) the
+            /// application must be the space creator.
             /// </summary>
             /// <param name="name">
             /// Required. Resource name of the membership to delete. Chat apps can delete human users' or their own
             /// memberships. Chat apps can't delete other apps' memberships. When deleting a human membership, requires
-            /// the `chat.memberships` scope and `spaces/{space}/members/{member}` format. You can use the email as an
-            /// alias for `{member}`. For example, `spaces/{space}/members/example@gmail.com` where `example@gmail.com`
-            /// is the email of the Google Chat user. When deleting an app membership, requires the
-            /// `chat.memberships.app` scope and `spaces/{space}/members/app` format. Format:
-            /// `spaces/{space}/members/{member}` or `spaces/{space}/members/app`.
+            /// the `chat.memberships` scope with [user
+            /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-user) or the
+            /// `chat.memberships.app` scope with [app
+            /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-app) and the
+            /// `spaces/{space}/members/{member}` format. You can use the email as an alias for `{member}`. For example,
+            /// `spaces/{space}/members/example@gmail.com` where `example@gmail.com` is the email of the Google Chat
+            /// user. When deleting an app membership, requires the `chat.memberships.app` scope and
+            /// `spaces/{space}/members/app` format. Format: `spaces/{space}/members/{member}` or
+            /// `spaces/{space}/members/app`.
             /// </param>
             public virtual DeleteRequest Delete(string name)
             {
@@ -1339,7 +1346,10 @@ namespace Google.Apis.HangoutsChat.v1
             /// Preview](https://developers.google.com/workspace/preview) - [User
             /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-user) You can
             /// authenticate and authorize this method with administrator privileges by setting the `use_admin_access`
-            /// field in the request.
+            /// field in the request. To delete memberships for space managers, the requester must be a space manager.
+            /// If you're using [app
+            /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-app) the
+            /// application must be the space creator.
             /// </summary>
             public class DeleteRequest : HangoutsChatBaseServiceRequest<Google.Apis.HangoutsChat.v1.Data.Membership>
             {
@@ -1353,11 +1363,15 @@ namespace Google.Apis.HangoutsChat.v1
                 /// <summary>
                 /// Required. Resource name of the membership to delete. Chat apps can delete human users' or their own
                 /// memberships. Chat apps can't delete other apps' memberships. When deleting a human membership,
-                /// requires the `chat.memberships` scope and `spaces/{space}/members/{member}` format. You can use the
-                /// email as an alias for `{member}`. For example, `spaces/{space}/members/example@gmail.com` where
-                /// `example@gmail.com` is the email of the Google Chat user. When deleting an app membership, requires
-                /// the `chat.memberships.app` scope and `spaces/{space}/members/app` format. Format:
-                /// `spaces/{space}/members/{member}` or `spaces/{space}/members/app`.
+                /// requires the `chat.memberships` scope with [user
+                /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-user) or
+                /// the `chat.memberships.app` scope with [app
+                /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-app) and
+                /// the `spaces/{space}/members/{member}` format. You can use the email as an alias for `{member}`. For
+                /// example, `spaces/{space}/members/example@gmail.com` where `example@gmail.com` is the email of the
+                /// Google Chat user. When deleting an app membership, requires the `chat.memberships.app` scope and
+                /// `spaces/{space}/members/app` format. Format: `spaces/{space}/members/{member}` or
+                /// `spaces/{space}/members/app`.
                 /// </summary>
                 [Google.Apis.Util.RequestParameterAttribute("name", Google.Apis.Util.RequestParameterType.Path)]
                 public virtual string Name { get; private set; }
