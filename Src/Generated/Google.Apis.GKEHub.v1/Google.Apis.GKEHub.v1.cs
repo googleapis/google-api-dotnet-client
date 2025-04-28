40bf29503a6fd3a99496c271ae2800cd5def6b04
Amanda Tarafa Mas <atarafamas@google.com>
2025-04-28 01:10:16 -0700

    feat: Generate Google.Apis.GKEHub.v1 version 1.69.0.3765

--- Src/Generated/Google.Apis.GKEHub.v1/Google.Apis.GKEHub.v1.cs
+++ Src/Generated/Google.Apis.GKEHub.v1/Google.Apis.GKEHub.v1.cs
@@ -9863,10 +9863,7 @@ namespace Google.Apis.GKEHub.v1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>
-    /// Condition being reported. TODO b/395151419: Remove this message once the membership-level conditions field uses
-    /// the common Condition message.
-    /// </summary>
+    /// <summary>Condition being reported.</summary>
     public class ServiceMeshCondition : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
@@ -9949,10 +9946,7 @@ namespace Google.Apis.GKEHub.v1.Data
     /// </summary>
     public class ServiceMeshMembershipState : Google.Apis.Requests.IDirectResponseSchema
     {
-        /// <summary>
-        /// Output only. List of conditions reported for this membership. TODO b/395151419: Use the common Condition
-        /// message.
-        /// </summary>
+        /// <summary>Output only. List of conditions reported for this membership.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("conditions")]
         public virtual System.Collections.Generic.IList<ServiceMeshCondition> Conditions { get; set; }
 
