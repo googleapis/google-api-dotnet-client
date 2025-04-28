a91ca94595fc7d6cfa7a84b986674809b0951d48
Amanda Tarafa Mas <atarafamas@google.com>
2025-04-28 01:11:09 -0700

    feat: Generate Google.Apis.Parallelstore.v1 version 1.69.0.3758

--- Src/Generated/Google.Apis.Parallelstore.v1/Google.Apis.Parallelstore.v1.cs
+++ Src/Generated/Google.Apis.Parallelstore.v1/Google.Apis.Parallelstore.v1.cs
@@ -356,7 +356,7 @@ namespace Google.Apis.Parallelstore.v1
                     /// Optional. An optional request ID to identify requests. Specify a unique request ID so that if
                     /// you must retry your request, the server will know to ignore the request if it has already been
                     /// completed. The server will guarantee that for at least 60 minutes since the first request. For
-                    /// example, consider a situation where you make an initial request and t he request times out. If
+                    /// example, consider a situation where you make an initial request and the request times out. If
                     /// you make the request again with the same request ID, the server can check if original operation
                     /// with the same request ID was received, and if so, will ignore the second request. This prevents
                     /// clients from accidentally creating duplicate commitments. The request ID must be a valid UUID
@@ -436,7 +436,7 @@ namespace Google.Apis.Parallelstore.v1
                     /// Optional. An optional request ID to identify requests. Specify a unique request ID so that if
                     /// you must retry your request, the server will know to ignore the request if it has already been
                     /// completed. The server will guarantee that for at least 60 minutes after the first request. For
-                    /// example, consider a situation where you make an initial request and t he request times out. If
+                    /// example, consider a situation where you make an initial request and the request times out. If
                     /// you make the request again with the same request ID, the server can check if original operation
                     /// with the same request ID was received, and if so, will ignore the second request. This prevents
                     /// clients from accidentally creating duplicate commitments. The request ID must be a valid UUID
@@ -771,7 +771,7 @@ namespace Google.Apis.Parallelstore.v1
                     /// Optional. An optional request ID to identify requests. Specify a unique request ID so that if
                     /// you must retry your request, the server will know to ignore the request if it has already been
                     /// completed. The server will guarantee that for at least 60 minutes since the first request. For
-                    /// example, consider a situation where you make an initial request and t he request times out. If
+                    /// example, consider a situation where you make an initial request and the request times out. If
                     /// you make the request again with the same request ID, the server can check if original operation
                     /// with the same request ID was received, and if so, will ignore the second request. This prevents
                     /// clients from accidentally creating duplicate commitments. The request ID must be a valid UUID
@@ -1312,7 +1312,7 @@ namespace Google.Apis.Parallelstore.v1.Data
         /// Optional. An optional request ID to identify requests. Specify a unique request ID so that if you must retry
         /// your request, the server will know to ignore the request if it has already been completed. The server will
         /// guarantee that for at least 60 minutes since the first request. For example, consider a situation where you
-        /// make an initial request and t he request times out. If you make the request again with the same request ID,
+        /// make an initial request and the request times out. If you make the request again with the same request ID,
         /// the server can check if original operation with the same request ID was received, and if so, will ignore the
         /// second request. This prevents clients from accidentally creating duplicate commitments. The request ID must
         /// be a valid UUID with the exception that zero UUID is not supported (00000000-0000-0000-0000-000000000000).
@@ -1364,7 +1364,7 @@ namespace Google.Apis.Parallelstore.v1.Data
         /// Optional. An optional request ID to identify requests. Specify a unique request ID so that if you must retry
         /// your request, the server will know to ignore the request if it has already been completed. The server will
         /// guarantee that for at least 60 minutes since the first request. For example, consider a situation where you
-        /// make an initial request and t he request times out. If you make the request again with the same request ID,
+        /// make an initial request and the request times out. If you make the request again with the same request ID,
         /// the server can check if original operation with the same request ID was received, and if so, will ignore the
         /// second request. This prevents clients from accidentally creating duplicate commitments. The request ID must
         /// be a valid UUID with the exception that zero UUID is not supported (00000000-0000-0000-0000-000000000000).
@@ -1441,10 +1441,7 @@ namespace Google.Apis.Parallelstore.v1.Data
             set => CreateTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
         }
 
-        /// <summary>
-        /// Output only. Deprecated 'daos_version' field. Output only. The version of DAOS software running in the
-        /// instance.
-        /// </summary>
+        /// <summary>Output only. Deprecated: The version of DAOS software running in the instance.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("daosVersion")]
         public virtual string DaosVersion { get; set; }
 
