69202c2f60b56b8eed3233de28914c4e82da43ab
Amanda Tarafa Mas <atarafamas@google.com>
2025-05-05 19:51:16 -0700

    feat: Generate Google.Apis.ServiceControl.v2 version 1.69.0.3767

--- Src/Generated/Google.Apis.ServiceControl.v2/Google.Apis.ServiceControl.v2.cs
+++ Src/Generated/Google.Apis.ServiceControl.v2/Google.Apis.ServiceControl.v2.cs
@@ -283,8 +283,7 @@ namespace Google.Apis.ServiceControl.v2
         }
 
         /// <summary>
-        /// Private Preview. This feature is only available for approved services. This method provides admission
-        /// control for services that are integrated with [Service
+        /// This method provides admission control for services that are integrated with [Service
         /// Infrastructure](https://cloud.google.com/service-infrastructure). It checks whether an operation should be
         /// allowed based on the service configuration and relevant policies. It must be called before the operation is
         /// executed. For more information, see [Admission
@@ -308,8 +307,7 @@ namespace Google.Apis.ServiceControl.v2
         }
 
         /// <summary>
-        /// Private Preview. This feature is only available for approved services. This method provides admission
-        /// control for services that are integrated with [Service
+        /// This method provides admission control for services that are integrated with [Service
         /// Infrastructure](https://cloud.google.com/service-infrastructure). It checks whether an operation should be
         /// allowed based on the service configuration and relevant policies. It must be called before the operation is
         /// executed. For more information, see [Admission
@@ -370,8 +368,7 @@ namespace Google.Apis.ServiceControl.v2
         }
 
         /// <summary>
-        /// Private Preview. This feature is only available for approved services. This method provides telemetry
-        /// reporting for services that are integrated with [Service
+        /// This method provides telemetry reporting for services that are integrated with [Service
         /// Infrastructure](https://cloud.google.com/service-infrastructure). It reports a list of operations that have
         /// occurred on a service. It must be called after the operations have been executed. For more information, see
         /// [Telemetry Reporting](https://cloud.google.com/service-infrastructure/docs/telemetry-reporting). NOTE: The
@@ -392,8 +389,7 @@ namespace Google.Apis.ServiceControl.v2
         }
 
         /// <summary>
-        /// Private Preview. This feature is only available for approved services. This method provides telemetry
-        /// reporting for services that are integrated with [Service
+        /// This method provides telemetry reporting for services that are integrated with [Service
         /// Infrastructure](https://cloud.google.com/service-infrastructure). It reports a list of operations that have
         /// occurred on a service. It must be called after the operations have been executed. For more information, see
         /// [Telemetry Reporting](https://cloud.google.com/service-infrastructure/docs/telemetry-reporting). NOTE: The
