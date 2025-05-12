b1d8c4e6acd8e883702d7ff56676c3d65e47e900
Amanda Tarafa Mas <atarafamas@google.com>
2025-05-12 01:10:34 -0700

    feat: Generate Google.Apis.Container.v1 version 1.69.0.3771

--- Src/Generated/Google.Apis.Container.v1/Google.Apis.Container.v1.cs
+++ Src/Generated/Google.Apis.Container.v1/Google.Apis.Container.v1.cs
@@ -9361,6 +9361,24 @@ namespace Google.Apis.Container.v1.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>
+    /// The option enables the Kubernetes NUMA-aware Memory Manager feature. Detailed description about the feature can
+    /// be found [here](https://kubernetes.io/docs/tasks/administer-cluster/memory-manager/).
+    /// </summary>
+    public class MemoryManager : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>
+        /// Controls the memory management policy on the Node. See
+        /// https://kubernetes.io/docs/tasks/administer-cluster/memory-manager/#policies The following values are
+        /// allowed. * "none" * "static" The default value is 'none' if unspecified.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("policy")]
+        public virtual string Policy { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>Configuration for issuance of mTLS keys and certificates to Kubernetes pods.</summary>
     public class MeshCertificates : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -10013,12 +10031,26 @@ namespace Google.Apis.Container.v1.Data
         public virtual System.Nullable<bool> InsecureKubeletReadonlyPortEnabled { get; set; }
 
         /// <summary>
+        /// Optional. Controls NUMA-aware Memory Manager configuration on the node. For more information, see:
+        /// https://kubernetes.io/docs/tasks/administer-cluster/memory-manager/
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("memoryManager")]
+        public virtual MemoryManager MemoryManager { get; set; }
+
+        /// <summary>
         /// Set the Pod PID limits. See https://kubernetes.io/docs/concepts/policy/pid-limiting/#pod-pid-limits Controls
         /// the maximum number of processes allowed to run in a pod. The value must be greater than or equal to 1024 and
         /// less than 4194304.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("podPidsLimit")]
         public virtual System.Nullable<long> PodPidsLimit { get; set; }
+
+        /// <summary>
+        /// Optional. Controls Topology Manager configuration on the node. For more information, see:
+        /// https://kubernetes.io/docs/tasks/administer-cluster/topology-manager/
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("topologyManager")]
+        public virtual TopologyManager TopologyManager { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
@@ -12133,6 +12165,40 @@ namespace Google.Apis.Container.v1.Data
             get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(StartTimeRaw);
             set => StartTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
         }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>
+    /// TopologyManager defines the configuration options for Topology Manager feature. See
+    /// https://kubernetes.io/docs/tasks/administer-cluster/topology-manager/
+    /// </summary>
+    public class TopologyManager : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>
+        /// Configures the strategy for resource alignment. Allowed values are: * none: the default policy, and does not
+        /// perform any topology alignment. * restricted: the topology manager stores the preferred NUMA node affinity
+        /// for the container, and will reject the pod if the affinity if not preferred. * best-effort: the topology
+        /// manager stores the preferred NUMA node affinity for the container. If the affinity is not preferred, the
+        /// topology manager will admit the pod to the node anyway. * single-numa-node: the topology manager determines
+        /// if the single NUMA node affinity is possible. If it is, Topology Manager will store this and the Hint
+        /// Providers can then use this information when making the resource allocation decision. If, however, this is
+        /// not possible then the Topology Manager will reject the pod from the node. This will result in a pod in a
+        /// Terminated state with a pod admission failure. The default policy value is 'none' if unspecified. Details
+        /// about each strategy can be found
+        /// [here](https://kubernetes.io/docs/tasks/administer-cluster/topology-manager/#topology-manager-policies).
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("policy")]
+        public virtual string Policy { get; set; }
+
+        /// <summary>
+        /// The Topology Manager aligns resources in following scopes: * container * pod The default scope is
+        /// 'container' if unspecified. See
+        /// https://kubernetes.io/docs/tasks/administer-cluster/topology-manager/#topology-manager-scopes
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("scope")]
+        public virtual string Scope { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
