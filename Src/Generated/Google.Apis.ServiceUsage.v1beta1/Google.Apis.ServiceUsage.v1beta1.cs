5f220ee066161e5bd7e4a13e72c66319563e1f9d
Amanda Tarafa Mas <atarafamas@google.com>
2025-05-19 01:13:21 -0700

    feat: Generate Google.Apis.ServiceUsage.v1beta1 version 1.69.0.3783

--- Src/Generated/Google.Apis.ServiceUsage.v1beta1/Google.Apis.ServiceUsage.v1beta1.cs
+++ Src/Generated/Google.Apis.ServiceUsage.v1beta1/Google.Apis.ServiceUsage.v1beta1.cs
@@ -2617,7 +2617,7 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
 
         /// <summary>
         /// Content of the configuration. The underlying schema should be defined by Aspect owners as protobuf message
-        /// under `apiserving/configaspects/proto`.
+        /// under `google/api/configaspects/proto`.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("spec")]
         public virtual System.Collections.Generic.IDictionary<string, object> Spec { get; set; }
