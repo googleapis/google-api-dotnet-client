770f979249e741207cb9be4892dc61f5dd24ab90
Amanda Tarafa Mas <atarafamas@google.com>
2025-05-28 01:13:14 -0700

    feat: Generate Google.Apis.Document.v1beta3 version 1.69.0.3793

--- Src/Generated/Google.Apis.Document.v1beta3/Google.Apis.Document.v1beta3.cs
+++ Src/Generated/Google.Apis.Document.v1beta3/Google.Apis.Document.v1beta3.cs
@@ -6486,9 +6486,9 @@ namespace Google.Apis.Document.v1beta3.Data
         [Newtonsoft.Json.JsonPropertyAttribute("displayName")]
         public virtual string DisplayName { get; set; }
 
-        /// <summary>Grounding config of the entity type.</summary>
-        [Newtonsoft.Json.JsonPropertyAttribute("groundingConfig")]
-        public virtual string GroundingConfig { get; set; }
+        /// <summary>Specifies how the entity's value is obtained.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("method")]
+        public virtual string Method { get; set; }
 
         /// <summary>The name of the property. Follows the same guidelines as the EntityType name.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("name")]
