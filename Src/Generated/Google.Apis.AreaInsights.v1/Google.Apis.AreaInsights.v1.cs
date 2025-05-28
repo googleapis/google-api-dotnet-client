e54fd08117b809500ae1a0781bb66301b995dda2
Amanda Tarafa Mas <atarafamas@google.com>
2025-05-28 01:11:00 -0700

    feat: Generate Google.Apis.AreaInsights.v1 version 1.69.0.3793

--- Src/Generated/Google.Apis.AreaInsights.v1/Google.Apis.AreaInsights.v1.cs
+++ Src/Generated/Google.Apis.AreaInsights.v1/Google.Apis.AreaInsights.v1.cs
@@ -528,7 +528,12 @@ namespace Google.Apis.AreaInsights.v1.Data
     /// <summary>A region is a geographic boundary such as: cities, postal codes, counties, states, etc.</summary>
     public class Region : Google.Apis.Requests.IDirectResponseSchema
     {
-        /// <summary>The unique identifier of a specific geographic region.</summary>
+        /// <summary>
+        /// The [place ID](https://developers.google.com/maps/documentation/places/web-service/place-id) of the
+        /// geographic region. Not all region types are supported; see documentation for details. **Format:** Must be in
+        /// the format `places/PLACE_ID`, where `PLACE_ID` is the unique identifier of a place. For example:
+        /// `places/ChIJPV4oX_65j4ARVW8IJ6IJUYs`.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("place")]
         public virtual string Place { get; set; }
 
