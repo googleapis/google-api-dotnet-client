a78005494ba954ed6d5fabaca404bb8e92c94a0f
Amanda Tarafa Mas <atarafamas@google.com>
2025-06-02 01:10:28 -0700

    feat: Generate Google.Apis.Backupdr.v1 version 1.69.0.3795

--- Src/Generated/Google.Apis.Backupdr.v1/Google.Apis.Backupdr.v1.cs
+++ Src/Generated/Google.Apis.Backupdr.v1/Google.Apis.Backupdr.v1.cs
@@ -5660,9 +5660,10 @@ namespace Google.Apis.Backupdr.v1.Data
         /// <summary>
         /// Required. Configures the duration for which backup data will be kept. It is defined in “days”. The value
         /// should be greater than or equal to minimum enforced retention of the backup vault. Minimum value is 1 and
-        /// maximum value is 90 for hourly backups. Minimum value is 1 and maximum value is 186 for daily backups.
-        /// Minimum value is 7 and maximum value is 366 for weekly backups. Minimum value is 30 and maximum value is 732
-        /// for monthly backups. Minimum value is 365 and maximum value is 36159 for yearly backups.
+        /// maximum value is 36159 for custom retention on-demand backup. Minimum value is 1 and maximum value is 90 for
+        /// hourly backups. Minimum value is 1 and maximum value is 186 for daily backups. Minimum value is 7 and
+        /// maximum value is 366 for weekly backups. Minimum value is 30 and maximum value is 732 for monthly backups.
+        /// Minimum value is 365 and maximum value is 36159 for yearly backups.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("backupRetentionDays")]
         public virtual System.Nullable<int> BackupRetentionDays { get; set; }
@@ -5675,7 +5676,7 @@ namespace Google.Apis.Backupdr.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("ruleId")]
         public virtual string RuleId { get; set; }
 
-        /// <summary>Required. Defines a schedule that runs within the confines of a defined window of time.</summary>
+        /// <summary>Optional. Defines a schedule that runs within the confines of a defined window of time.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("standardSchedule")]
         public virtual StandardSchedule StandardSchedule { get; set; }
 
