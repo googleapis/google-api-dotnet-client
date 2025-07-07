cc93dd27d2dab222045bf5260cc3b2a505ddc6b6
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-07 08:06:13 +0000

    feat: Generate Google.Apis.WorkspaceEvents.v1 version 1.70.0.3832

--- Src/Generated/Google.Apis.WorkspaceEvents.v1/Google.Apis.WorkspaceEvents.v1.cs
+++ Src/Generated/Google.Apis.WorkspaceEvents.v1/Google.Apis.WorkspaceEvents.v1.cs
@@ -822,8 +822,8 @@ namespace Google.Apis.WorkspaceEvents.v1
             /// Optional. The field to update. If omitted, updates any fields included in the request. You can update
             /// one of the following fields in a subscription: * `expire_time`: The timestamp when the subscription
             /// expires. * `ttl`: The time-to-live (TTL) or duration of the subscription. * `event_types`: The list of
-            /// event types to receive about the target resource. To fully replace the subscription (the equivalent of
-            /// `PUT`), use `*`. Any omitted fields are updated with empty values.
+            /// event types to receive about the target resource. When using the `*` wildcard (equivalent to `PUT`),
+            /// omitted fields are set to empty values and rejected if they're invalid.
             /// </summary>
             [Google.Apis.Util.RequestParameterAttribute("updateMask", Google.Apis.Util.RequestParameterType.Query)]
             public virtual object UpdateMask { get; set; }
