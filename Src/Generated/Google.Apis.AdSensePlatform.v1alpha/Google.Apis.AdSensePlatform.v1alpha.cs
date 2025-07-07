b78e79160a49c1760d00e65c7353014e25166fc0
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-07 08:04:54 +0000

    feat: Generate Google.Apis.AdSensePlatform.v1alpha version 1.70.0.3836

--- Src/Generated/Google.Apis.AdSensePlatform.v1alpha/Google.Apis.AdSensePlatform.v1alpha.cs
+++ Src/Generated/Google.Apis.AdSensePlatform.v1alpha/Google.Apis.AdSensePlatform.v1alpha.cs
@@ -693,6 +693,78 @@ namespace Google.Apis.AdSensePlatform.v1alpha
                         });
                     }
                 }
+
+                /// <summary>Update a Platform Group.</summary>
+                /// <param name="body">The body of the request.</param>
+                /// <param name="name">
+                /// Identifier. Format: accounts/{account}/platforms/{platform}/groups/{platform_group}
+                /// </param>
+                public virtual PatchRequest Patch(Google.Apis.AdSensePlatform.v1alpha.Data.PlatformGroup body, string name)
+                {
+                    return new PatchRequest(this.service, body, name);
+                }
+
+                /// <summary>Update a Platform Group.</summary>
+                public class PatchRequest : AdSensePlatformBaseServiceRequest<Google.Apis.AdSensePlatform.v1alpha.Data.PlatformGroup>
+                {
+                    /// <summary>Constructs a new Patch request.</summary>
+                    public PatchRequest(Google.Apis.Services.IClientService service, Google.Apis.AdSensePlatform.v1alpha.Data.PlatformGroup body, string name) : base(service)
+                    {
+                        Name = name;
+                        Body = body;
+                        InitParameters();
+                    }
+
+                    /// <summary>
+                    /// Identifier. Format: accounts/{account}/platforms/{platform}/groups/{platform_group}
+                    /// </summary>
+                    [Google.Apis.Util.RequestParameterAttribute("name", Google.Apis.Util.RequestParameterType.Path)]
+                    public virtual string Name { get; private set; }
+
+                    /// <summary>
+                    /// Optional. The list of fields to update - currently only supports updating the `description`
+                    /// field.
+                    /// </summary>
+                    [Google.Apis.Util.RequestParameterAttribute("updateMask", Google.Apis.Util.RequestParameterType.Query)]
+                    public virtual object UpdateMask { get; set; }
+
+                    /// <summary>Gets or sets the body of this request.</summary>
+                    Google.Apis.AdSensePlatform.v1alpha.Data.PlatformGroup Body { get; set; }
+
+                    /// <summary>Returns the body of the request.</summary>
+                    protected override object GetBody() => Body;
+
+                    /// <summary>Gets the method name.</summary>
+                    public override string MethodName => "patch";
+
+                    /// <summary>Gets the HTTP method.</summary>
+                    public override string HttpMethod => "PATCH";
+
+                    /// <summary>Gets the REST path.</summary>
+                    public override string RestPath => "v1alpha/{+name}";
+
+                    /// <summary>Initializes Patch parameter list.</summary>
+                    protected override void InitParameters()
+                    {
+                        base.InitParameters();
+                        RequestParameters.Add("name", new Google.Apis.Discovery.Parameter
+                        {
+                            Name = "name",
+                            IsRequired = true,
+                            ParameterType = "path",
+                            DefaultValue = null,
+                            Pattern = @"^accounts/[^/]+/platforms/[^/]+/groups/[^/]+$",
+                        });
+                        RequestParameters.Add("updateMask", new Google.Apis.Discovery.Parameter
+                        {
+                            Name = "updateMask",
+                            IsRequired = false,
+                            ParameterType = "query",
+                            DefaultValue = null,
+                            Pattern = null,
+                        });
+                    }
+                }
             }
 
             /// <summary>Gets a platform.</summary>
