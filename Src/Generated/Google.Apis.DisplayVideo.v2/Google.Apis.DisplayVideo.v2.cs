711cfab1924ff83a0ff17c9446199a3eaab07460
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-14 08:06:13 +0000

    feat: Generate Google.Apis.DisplayVideo.v2 version 1.70.0.3842

--- Src/Generated/Google.Apis.DisplayVideo.v2/Google.Apis.DisplayVideo.v2.cs
+++ Src/Generated/Google.Apis.DisplayVideo.v2/Google.Apis.DisplayVideo.v2.cs
@@ -27200,6 +27200,25 @@ namespace Google.Apis.DisplayVideo.v2.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>The video ad inventory control used in certain YouTube line item types.</summary>
+    public class VideoAdInventoryControl : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>Optional. Whether ads can serve as in-feed format.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("allowInFeed")]
+        public virtual System.Nullable<bool> AllowInFeed { get; set; }
+
+        /// <summary>Optional. Whether ads can serve as in-stream format.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("allowInStream")]
+        public virtual System.Nullable<bool> AllowInStream { get; set; }
+
+        /// <summary>Optional. Whether ads can serve as shorts format.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("allowShorts")]
+        public virtual System.Nullable<bool> AllowShorts { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>Settings related to VideoAdSequence.</summary>
     public class VideoAdSequenceSettings : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -27659,6 +27678,10 @@ namespace Google.Apis.DisplayVideo.v2.Data
         /// <summary>Optional. The third-party measurement settings of the line item.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("thirdPartyMeasurementSettings")]
         public virtual YoutubeAndPartnersThirdPartyMeasurementSettings ThirdPartyMeasurementSettings { get; set; }
+
+        /// <summary>Optional. The settings to control which inventory is allowed for this line item.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("videoAdInventoryControl")]
+        public virtual VideoAdInventoryControl VideoAdInventoryControl { get; set; }
 
         /// <summary>Optional. The settings related to VideoAdSequence.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("videoAdSequenceSettings")]
