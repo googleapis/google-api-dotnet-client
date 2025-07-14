e3af4e51df468add8dfb2aefd25d3d26cfc75651
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-14 08:05:34 +0000

    feat: Generate Google.Apis.ArtifactRegistry.v1 version 1.70.0.3837

--- Src/Generated/Google.Apis.ArtifactRegistry.v1/Google.Apis.ArtifactRegistry.v1.cs
+++ Src/Generated/Google.Apis.ArtifactRegistry.v1/Google.Apis.ArtifactRegistry.v1.cs
@@ -6307,7 +6307,7 @@ namespace Google.Apis.ArtifactRegistry.v1.Data
 
         /// <summary>
         /// Required. registry_location, project_id, repository_name and image id forms a unique image
-        /// name:`projects//locations//repository//dockerImages/`. For example,
+        /// name:`projects//locations//repositories//dockerImages/`. For example,
         /// "projects/test-project/locations/us-west4/repositories/test-repo/dockerImages/
         /// nginx@sha256:e9954c1fc875017be1c3e36eca16be2d9e9bccc4bf072163515467d6a823c7cf", where "us-west4" is the
         /// registry_location, "test-project" is the project_id, "test-repo" is the repository_name and
