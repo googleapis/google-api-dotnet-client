aba11793d65d18e308f9514628de2e177871ae27
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-16 08:04:59 +0000

    feat: Generate Google.Apis.CloudRetail.v2alpha version 1.70.0.3843

--- Src/Generated/Google.Apis.CloudRetail.v2alpha/Google.Apis.CloudRetail.v2alpha.cs
+++ Src/Generated/Google.Apis.CloudRetail.v2alpha/Google.Apis.CloudRetail.v2alpha.cs
@@ -11191,10 +11191,6 @@ namespace Google.Apis.CloudRetail.v2alpha.Data
         [Newtonsoft.Json.JsonPropertyAttribute("dataSourceId")]
         public virtual System.Nullable<long> DataSourceId { get; set; }
 
-        /// <summary>Merchant Center primary feed ID. Deprecated: use data_source_id instead.</summary>
-        [Newtonsoft.Json.JsonPropertyAttribute("primaryFeedId")]
-        public virtual System.Nullable<long> PrimaryFeedId { get; set; }
-
         /// <summary>Merchant Center primary feed name. The name is used for the display purposes only.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("primaryFeedName")]
         public virtual string PrimaryFeedName { get; set; }
