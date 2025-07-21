cb38032dc3efbf28f74940093b827023e07a925a
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-21 08:06:25 +0000

    feat: Generate Google.Apis.TPU.v2alpha1 version 1.70.0.3848

--- Src/Generated/Google.Apis.TPU.v2alpha1/Google.Apis.TPU.v2alpha1.cs
+++ Src/Generated/Google.Apis.TPU.v2alpha1/Google.Apis.TPU.v2alpha1.cs
@@ -2315,9 +2315,9 @@ namespace Google.Apis.TPU.v2alpha1.Data
     {
         /// <summary>
         /// The name of the encryption key that is stored in Google Cloud KMS. For example: "kmsKeyName":
-        /// "projects/kms_project_id/locations/region/keyRings/ key_region/cryptoKeys/key The fully-qualifed key name
-        /// may be returned for resource GET requests. For example: "kmsKeyName":
-        /// "projects/kms_project_id/locations/region/keyRings/ key_region/cryptoKeys/key /cryptoKeyVersions/1
+        /// "projects/KMS_PROJECT_ID/locations/REGION/keyRings/KEY_REGION/cryptoKeys/KEY The fully-qualifed key name may
+        /// be returned for resource GET requests. For example: "kmsKeyName":
+        /// "projects/KMS_PROJECT_ID/locations/REGION/keyRings/KEY_REGION/cryptoKeys/KEY/cryptoKeyVersions/1
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("kmsKeyName")]
         public virtual string KmsKeyName { get; set; }
