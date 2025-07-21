9d5f1559129bd6ca4297bdc01bc2b55d464bf714
Amanda Tarafa Mas <atarafamas@google.com>
2025-07-21 08:05:39 +0000

    feat: Generate Google.Apis.FirebaseML.v2beta version 1.70.0.3848

--- Src/Generated/Google.Apis.FirebaseML.v2beta/Google.Apis.FirebaseML.v2beta.cs
+++ Src/Generated/Google.Apis.FirebaseML.v2beta/Google.Apis.FirebaseML.v2beta.cs
@@ -2404,7 +2404,7 @@ namespace Google.Apis.FirebaseML.v2beta.Data
         /// Optional. Function tool type. One or more function declarations to be passed to the model along with the
         /// current user query. Model may decide to call a subset of these functions by populating FunctionCall in the
         /// response. User should provide a FunctionResponse for each function call in the next turn. Based on the
-        /// function responses, Model will generate the final response back to the user. Maximum 128 function
+        /// function responses, Model will generate the final response back to the user. Maximum 512 function
         /// declarations can be provided.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("functionDeclarations")]
