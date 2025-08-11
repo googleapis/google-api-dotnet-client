c335342c5576573bc6b94b7992cba591d23efb2c
Amanda Tarafa Mas <atarafamas@google.com>
2025-08-11 08:08:43 +0000

    feat: Generate Google.Apis.CloudRun.v2 version 1.70.0.3865

--- Src/Generated/Google.Apis.CloudRun.v2/Google.Apis.CloudRun.v2.cs
+++ Src/Generated/Google.Apis.CloudRun.v2/Google.Apis.CloudRun.v2.cs
@@ -6868,6 +6868,14 @@ namespace Google.Apis.CloudRun.v2.Data
         public virtual string ImageUri { get; set; }
 
         /// <summary>
+        /// Optional. The machine type from default pool to use for the build. If left blank, cloudbuild will use a
+        /// sensible default. Currently only E2_HIGHCPU_8 is supported. If worker_pool is set, this field will be
+        /// ignored.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("machineType")]
+        public virtual string MachineType { get; set; }
+
+        /// <summary>
         /// Optional. The service account to use for the build. If not set, the default Cloud Build service account for
         /// the project will be used.
         /// </summary>
@@ -7844,7 +7852,7 @@ namespace Google.Apis.CloudRun.v2.Data
         public virtual string LatestCreatedRevision { get; set; }
 
         /// <summary>
-        /// Output only. Name of the latest revision that is serving traffic. See comments in `reconciling` for
+        /// Output only. Name of the latest revision that is serving workloads. See comments in `reconciling` for
         /// additional information on reconciliation process in Cloud Run.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("latestReadyRevision")]
@@ -7870,9 +7878,9 @@ namespace Google.Apis.CloudRun.v2.Data
         public virtual string Name { get; set; }
 
         /// <summary>
-        /// Output only. The generation of this WorkerPool currently serving traffic. See comments in `reconciling` for
-        /// additional information on reconciliation process in Cloud Run. Please note that unlike v1, this is an int64
-        /// value. As with most Google APIs, its JSON representation will be a `string` instead of an `integer`.
+        /// Output only. The generation of this WorkerPool currently serving workloads. See comments in `reconciling`
+        /// for additional information on reconciliation process in Cloud Run. Please note that unlike v1, this is an
+        /// int64 value. As with most Google APIs, its JSON representation will be a `string` instead of an `integer`.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("observedGeneration")]
         public virtual System.Nullable<long> ObservedGeneration { get; set; }
@@ -7882,15 +7890,15 @@ namespace Google.Apis.CloudRun.v2.Data
         /// desired state. When a new WorkerPool is created, or an existing one is updated, Cloud Run will
         /// asynchronously perform all necessary steps to bring the WorkerPool to the desired serving state. This
         /// process is called reconciliation. While reconciliation is in process, `observed_generation`,
-        /// `latest_ready_revison`, `traffic_statuses`, and `uri` will have transient values that might mismatch the
-        /// intended state: Once reconciliation is over (and this field is false), there are two possible outcomes:
+        /// `latest_ready_revison`, `instance_split_statuses`, and `uri` will have transient values that might mismatch
+        /// the intended state: Once reconciliation is over (and this field is false), there are two possible outcomes:
         /// reconciliation succeeded and the serving state matches the WorkerPool, or there was an error, and
         /// reconciliation failed. This state can be found in `terminal_condition.state`. If reconciliation succeeded,
-        /// the following fields will match: `traffic` and `traffic_statuses`, `observed_generation` and `generation`,
-        /// `latest_ready_revision` and `latest_created_revision`. If reconciliation failed, `traffic_statuses`,
-        /// `observed_generation`, and `latest_ready_revision` will have the state of the last serving revision, or
-        /// empty for newly created WorkerPools. Additional information on the failure can be found in
-        /// `terminal_condition` and `conditions`.
+        /// the following fields will match: `instance_splits` and `instance_split_statuses`, `observed_generation` and
+        /// `generation`, `latest_ready_revision` and `latest_created_revision`. If reconciliation failed,
+        /// `instance_split_statuses`, `observed_generation`, and `latest_ready_revision` will have the state of the
+        /// last serving revision, or empty for newly created WorkerPools. Additional information on the failure can be
+        /// found in `terminal_condition` and `conditions`.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("reconciling")]
         public virtual System.Nullable<bool> Reconciling { get; set; }
