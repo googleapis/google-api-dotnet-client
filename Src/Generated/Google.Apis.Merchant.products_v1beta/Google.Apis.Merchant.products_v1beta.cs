1428273c9875f1a16315df902ea18d330dadbd12
Amanda Tarafa Mas <atarafamas@google.com>
2025-08-18 08:07:09 +0000

    feat: Generate Google.Apis.Merchant.products_v1beta version 1.70.0.3875

--- Src/Generated/Google.Apis.Merchant.products_v1beta/Google.Apis.Merchant.products_v1beta.cs
+++ Src/Generated/Google.Apis.Merchant.products_v1beta/Google.Apis.Merchant.products_v1beta.cs
@@ -368,12 +368,12 @@ namespace Google.Apis.Merchant.products_v1beta
 
             /// <summary>
             /// [Uploads a product input to your Merchant Center
-            /// account](/merchant/api/guides/products/overview#upload-product-input). You must have a products [data
-            /// source](/merchant/api/guides/data-sources/overview) to be able to insert a product. The unique
-            /// identifier of the data source is passed as a query parameter in the request URL. If a product input with
-            /// the same contentLanguage, offerId, and dataSource already exists, then the product input inserted by
-            /// this method replaces that entry. After inserting, updating, or deleting a product input, it may take
-            /// several minutes before the processed product can be retrieved.
+            /// account](/merchant/api/guides/products/add-manage#add_a_product). You must have a products [data
+            /// source](/merchant/api/guides/data-sources/api-sources#create-primary-data-source) to be able to insert a
+            /// product. The unique identifier of the data source is passed as a query parameter in the request URL. If
+            /// a product input with the same contentLanguage, offerId, and dataSource already exists, then the product
+            /// input inserted by this method replaces that entry. After inserting, updating, or deleting a product
+            /// input, it may take several minutes before the processed product can be retrieved.
             /// </summary>
             /// <param name="body">The body of the request.</param>
             /// <param name="parent">
@@ -386,12 +386,12 @@ namespace Google.Apis.Merchant.products_v1beta
 
             /// <summary>
             /// [Uploads a product input to your Merchant Center
-            /// account](/merchant/api/guides/products/overview#upload-product-input). You must have a products [data
-            /// source](/merchant/api/guides/data-sources/overview) to be able to insert a product. The unique
-            /// identifier of the data source is passed as a query parameter in the request URL. If a product input with
-            /// the same contentLanguage, offerId, and dataSource already exists, then the product input inserted by
-            /// this method replaces that entry. After inserting, updating, or deleting a product input, it may take
-            /// several minutes before the processed product can be retrieved.
+            /// account](/merchant/api/guides/products/add-manage#add_a_product). You must have a products [data
+            /// source](/merchant/api/guides/data-sources/api-sources#create-primary-data-source) to be able to insert a
+            /// product. The unique identifier of the data source is passed as a query parameter in the request URL. If
+            /// a product input with the same contentLanguage, offerId, and dataSource already exists, then the product
+            /// input inserted by this method replaces that entry. After inserting, updating, or deleting a product
+            /// input, it may take several minutes before the processed product can be retrieved.
             /// </summary>
             public class InsertRequest : MerchantBaseServiceRequest<Google.Apis.Merchant.products_v1beta.Data.ProductInput>
             {
@@ -412,9 +412,10 @@ namespace Google.Apis.Merchant.products_v1beta
                 /// <summary>
                 /// Required. The primary or supplemental product data source name. If the product already exists and
                 /// data source provided is different, then the product will be moved to a new data source. For more
-                /// information, see [Overview of Data sources sub-API](/merchant/api/guides/data-sources/overview).
-                /// Only API data sources are supported. Format: `accounts/{account}/dataSources/{datasource}`. For
-                /// example, `accounts/123456/dataSources/104628`.
+                /// information, see [Create a primary data
+                /// source](/merchant/api/guides/data-sources/api-sources#create-primary-data-source). Only API data
+                /// sources are supported. Format: `accounts/{account}/dataSources/{datasource}`. For example,
+                /// `accounts/123456/dataSources/104628`.
                 /// </summary>
                 [Google.Apis.Util.RequestParameterAttribute("dataSource", Google.Apis.Util.RequestParameterType.Query)]
                 public virtual string DataSource { get; set; }
