2a4f84342b2461f667e04e4b99b8b2be462ec024
Amanda Tarafa Mas <atarafamas@google.com>
2025-08-25 08:11:57 +0000

    feat: Generate Google.Apis.Testing.v1 version 1.70.0.3883

--- Src/Generated/Google.Apis.Testing.v1/Google.Apis.Testing.v1.cs
+++ Src/Generated/Google.Apis.Testing.v1/Google.Apis.Testing.v1.cs
@@ -2727,12 +2727,11 @@ namespace Google.Apis.Testing.v1.Data
         /// <summary>
         /// Required. Where to put the content on the device. Must be an absolute, allowlisted path. If the file exists,
         /// it will be replaced. The following device-side directories and any of their subdirectories are allowlisted:
-        /// ${EXTERNAL_STORAGE}, /sdcard, or /storage ${ANDROID_DATA}/local/tmp, or /data/local/tmp Specifying a path
-        /// outside of these directory trees is invalid. The paths /sdcard and /data will be made available and treated
-        /// as implicit path substitutions. E.g. if /sdcard on a particular device does not map to external storage, the
-        /// system will replace it with the external storage path prefix for that device and copy the file there. It is
-        /// strongly advised to use the Environment API in app and test code to access files on the device in a portable
-        /// way.
+        /// ${EXTERNAL_STORAGE}, /sdcard ${ANDROID_DATA}/local/tmp, or /data/local/tmp Specifying a path outside of
+        /// these directory trees is invalid. The paths /sdcard and /data will be made available and treated as implicit
+        /// path substitutions. E.g. if /sdcard on a particular device does not map to external storage, the system will
+        /// replace it with the external storage path prefix for that device and copy the file there. It is strongly
+        /// advised to use the Environment API in app and test code to access files on the device in a portable way.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("devicePath")]
         public virtual string DevicePath { get; set; }
