565460862857511a02e9e2d8c1d7d33fec1a25d6
Amanda Tarafa Mas <atarafamas@google.com>
2025-09-08 08:07:00 +0000

    feat: Generate Google.Apis.CloudBuild.v1 version 1.70.0.3898

--- Src/Generated/Google.Apis.CloudBuild.v1/Google.Apis.CloudBuild.v1.cs
+++ Src/Generated/Google.Apis.CloudBuild.v1/Google.Apis.CloudBuild.v1.cs
@@ -8662,7 +8662,10 @@ namespace Google.Apis.CloudBuild.v1.Data
     /// <summary>Npm package to upload to Artifact Registry upon successful completion of all build steps.</summary>
     public class NpmPackage : Google.Apis.Requests.IDirectResponseSchema
     {
-        /// <summary>Path to the package.json. e.g. workspace/path/to/package</summary>
+        /// <summary>
+        /// Optional. Path to the package.json. e.g. workspace/path/to/package Only one of `archive` or `package_path`
+        /// can be specified.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("packagePath")]
         public virtual string PackagePath { get; set; }
 
