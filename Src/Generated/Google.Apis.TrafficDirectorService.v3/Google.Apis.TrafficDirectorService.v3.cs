d4c9dc61087369e6d54855cb0ee8b7c85600e522
Amanda Tarafa Mas <atarafamas@google.com>
2025-09-08 08:09:47 +0000

    feat: Generate Google.Apis.TrafficDirectorService.v3 version 1.70.0.3891

--- Src/Generated/Google.Apis.TrafficDirectorService.v3/Google.Apis.TrafficDirectorService.v3.cs
+++ Src/Generated/Google.Apis.TrafficDirectorService.v3/Google.Apis.TrafficDirectorService.v3.cs
@@ -1492,8 +1492,7 @@ namespace Google.Apis.TrafficDirectorService.v3.Data
         /// Filepath that specifies the Linux network namespace this socket will be created in (see ``man 7
         /// network_namespaces``). If this field is set, Envoy will create the socket in the specified network
         /// namespace. .. note:: Setting this parameter requires Envoy to run with the ``CAP_NET_ADMIN`` capability. ..
-        /// note:: Currently only used for Listener sockets. .. attention:: Network namespaces are only configurable on
-        /// Linux. Otherwise, this field has no effect.
+        /// attention:: Network namespaces are only configurable on Linux. Otherwise, this field has no effect.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("networkNamespaceFilepath")]
         public virtual string NetworkNamespaceFilepath { get; set; }
