8f4cc61dce8a5eb09b74e93308ab0118a1225ed7
Amanda Tarafa Mas <atarafamas@google.com>
2025-09-24 15:47:13 +0000

    feat: Generate Google.Apis.ServiceUsage.v1beta1 version 1.71.0.3914

--- Src/Generated/Google.Apis.ServiceUsage.v1beta1/Google.Apis.ServiceUsage.v1beta1.cs
+++ Src/Generated/Google.Apis.ServiceUsage.v1beta1/Google.Apis.ServiceUsage.v1beta1.cs
@@ -2556,10 +2556,17 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
     /// services" in some contexts, such as by the "service" keyword in a .proto file, but they are different from API
     /// Services, which represent a concrete implementation of an interface as opposed to simply a description of
     /// methods and bindings. They are also sometimes simply referred to as "APIs" in other contexts, such as the name
-    /// of this message itself. See https://cloud.google.com/apis/design/glossary for detailed terminology.
+    /// of this message itself. See https://cloud.google.com/apis/design/glossary for detailed terminology. New usages
+    /// of this message as an alternative to ServiceDescriptorProto are strongly discouraged. This message does not
+    /// reliability preserve all information necessary to model the schema and preserve semantics. Instead make use of
+    /// FileDescriptorSet which preserves the necessary information.
     /// </summary>
     public class Api : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>The source edition string, only valid when syntax is SYNTAX_EDITIONS.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("edition")]
+        public virtual string Edition { get; set; }
+
         /// <summary>The methods of this interface, in unspecified order.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("methods")]
         public virtual System.Collections.Generic.IList<Method> Methods { get; set; }
@@ -2847,6 +2854,7 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("overridesByRequestProtocol")]
         public virtual System.Collections.Generic.IDictionary<string, BackendRule> OverridesByRequestProtocol { get; set; }
 
+        /// <summary>no-lint</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("pathTranslation")]
         public virtual string PathTranslation { get; set; }
 
@@ -3305,6 +3313,33 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         /// <summary>The units in which the metric value is reported.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("unit")]
         public virtual string Unit { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>ContentSecurity defines the content security related fields of a MCP policy.</summary>
+    public class ContentSecurity : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>List of content security providers that are enabled for content scanning.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("contentSecurityProviders")]
+        public virtual System.Collections.Generic.IList<ContentSecurityProvider> ContentSecurityProviders { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>ContentSecurityProvider contains the name of content security provider.</summary>
+    public class ContentSecurityProvider : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>
+        /// Name of security service for content scanning, such as Google Cloud Model Armor or supported third-party ISV
+        /// solutions. If it is Google 1P service, the name should be prefixed with `services/`. If it is a 3P service,
+        /// the format needs to be documented. The currently supported values are: -
+        /// `services/modelarmor.googleapis.com` for Google Cloud Model Armor.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("name")]
+        public virtual string Name { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
@@ -3790,7 +3825,11 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>Enum type definition.</summary>
+    /// <summary>
+    /// Enum type definition. New usages of this message as an alternative to EnumDescriptorProto are strongly
+    /// discouraged. This message does not reliability preserve all information necessary to model the schema and
+    /// preserve semantics. Instead make use of FileDescriptorSet which preserves the necessary information.
+    /// </summary>
     public class Enum : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>The source edition string, only valid when syntax is SYNTAX_EDITIONS.</summary>
@@ -3821,7 +3860,11 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>Enum value definition.</summary>
+    /// <summary>
+    /// Enum value definition. New usages of this message as an alternative to EnumValueDescriptorProto are strongly
+    /// discouraged. This message does not reliability preserve all information necessary to model the schema and
+    /// preserve semantics. Instead make use of FileDescriptorSet which preserves the necessary information.
+    /// </summary>
     public class EnumValue : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>Enum value name.</summary>
@@ -3874,7 +3917,11 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>A single field of a message type.</summary>
+    /// <summary>
+    /// A single field of a message type. New usages of this message as an alternative to FieldDescriptorProto are
+    /// strongly discouraged. This message does not reliability preserve all information necessary to model the schema
+    /// and preserve semantics. Instead make use of FileDescriptorSet which preserves the necessary information.
+    /// </summary>
     public class Field : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>The field cardinality.</summary>
@@ -3995,7 +4042,7 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
 
         /// <summary>
         /// Map of service names to renamed services. Keys are the package relative service names and values are the
-        /// name to be used for the service client and call options. publishing: go_settings: renamed_services:
+        /// name to be used for the service client and call options. Example: publishing: go_settings: renamed_services:
         /// Publisher: TopicAdmin
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("renamedServices")]
@@ -4580,9 +4627,7 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("enableRules")]
         public virtual System.Collections.Generic.IList<GoogleApiServiceusageV2betaEnableRule> EnableRules { get; set; }
 
-        /// <summary>
-        /// Output only. An opaque tag indicating the current version of the policy, used for concurrency control.
-        /// </summary>
+        /// <summary>An opaque tag indicating the current version of the policy, used for concurrency control.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("etag")]
         public virtual string ETag { get; set; }
 
@@ -4653,6 +4698,14 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         /// <summary>Output only. The type of impact.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("impactType")]
         public virtual string ImpactType { get; set; }
+
+        /// <summary>
+        /// Output only. This field will be populated only for the `DEPENDENCY_MISSING_DEPENDENCIES` impact type.
+        /// Example: `services/compute.googleapis.com`. Impact.detail will be in format : `missing service dependency:
+        /// {missing_dependency}.`
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("missingDependency")]
+        public virtual string MissingDependency { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
@@ -5268,9 +5321,148 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>Method represents a method of an API interface.</summary>
+    /// <summary>McpEnableRule contains MCP enablement related rules.</summary>
+    public class McpEnableRule : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>List of enabled MCP services.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("mcpServices")]
+        public virtual System.Collections.Generic.IList<McpService> McpServices { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>
+    /// MCP Consumer Policy is a set of rules that define MCP related policy for a cloud resource hierarchy.
+    /// </summary>
+    public class McpPolicy : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>ContentSecurity contains the content security related fields of a MCP policy.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("contentSecurity")]
+        public virtual ContentSecurity ContentSecurity { get; set; }
+
+        private string _createTimeRaw;
+
+        private object _createTime;
+
+        /// <summary>
+        /// Output only. The time the policy was created. For singleton policies (such as the `default` policy), this is
+        /// the first touch of the policy.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("createTime")]
+        public virtual string CreateTimeRaw
+        {
+            get => _createTimeRaw;
+            set
+            {
+                _createTime = Google.Apis.Util.Utilities.DeserializeForGoogleFormat(value);
+                _createTimeRaw = value;
+            }
+        }
+
+        /// <summary><seealso cref="object"/> representation of <see cref="CreateTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        [System.ObsoleteAttribute("This property is obsolete and may behave unexpectedly; please use CreateTimeDateTimeOffset instead.")]
+        public virtual object CreateTime
+        {
+            get => _createTime;
+            set
+            {
+                _createTimeRaw = Google.Apis.Util.Utilities.SerializeForGoogleFormat(value);
+                _createTime = value;
+            }
+        }
+
+        /// <summary><seealso cref="System.DateTimeOffset"/> representation of <see cref="CreateTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        public virtual System.DateTimeOffset? CreateTimeDateTimeOffset
+        {
+            get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(CreateTimeRaw);
+            set => CreateTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
+        }
+
+        /// <summary>An opaque tag indicating the current version of the policy, used for concurrency control.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("etag")]
+        public virtual string ETag { get; set; }
+
+        /// <summary>McpEnableRules contains MCP enablement related rules.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("mcpEnableRules")]
+        public virtual System.Collections.Generic.IList<McpEnableRule> McpEnableRules { get; set; }
+
+        /// <summary>
+        /// Output only. The resource name of the policy. Only the `default` policy is supported. We allow the following
+        /// formats: `projects/{PROJECT_NUMBER}/mcpPolicies/default`, `projects/{PROJECT_ID}/mcpPolicies/default`,
+        /// `folders/{FOLDER_ID}/mcpPolicies/default`, `organizations/{ORG_ID}/mcpPolicies/default`.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("name")]
+        public virtual string Name { get; set; }
+
+        private string _updateTimeRaw;
+
+        private object _updateTime;
+
+        /// <summary>Output only. The time the policy was last updated.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("updateTime")]
+        public virtual string UpdateTimeRaw
+        {
+            get => _updateTimeRaw;
+            set
+            {
+                _updateTime = Google.Apis.Util.Utilities.DeserializeForGoogleFormat(value);
+                _updateTimeRaw = value;
+            }
+        }
+
+        /// <summary><seealso cref="object"/> representation of <see cref="UpdateTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        [System.ObsoleteAttribute("This property is obsolete and may behave unexpectedly; please use UpdateTimeDateTimeOffset instead.")]
+        public virtual object UpdateTime
+        {
+            get => _updateTime;
+            set
+            {
+                _updateTimeRaw = Google.Apis.Util.Utilities.SerializeForGoogleFormat(value);
+                _updateTime = value;
+            }
+        }
+
+        /// <summary><seealso cref="System.DateTimeOffset"/> representation of <see cref="UpdateTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        public virtual System.DateTimeOffset? UpdateTimeDateTimeOffset
+        {
+            get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(UpdateTimeRaw);
+            set => UpdateTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
+        }
+    }
+
+    /// <summary>McpService contains the service names that are enabled for MCP.</summary>
+    public class McpService : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>
+        /// The names of the services that are enabled for MCP. Example: `services/library-example.googleapis.com`
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("service")]
+        public virtual string Service { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>
+    /// Method represents a method of an API interface. New usages of this message as an alternative to
+    /// MethodDescriptorProto are strongly discouraged. This message does not reliability preserve all information
+    /// necessary to model the schema and preserve semantics. Instead make use of FileDescriptorSet which preserves the
+    /// necessary information.
+    /// </summary>
     public class Method : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>
+        /// The source edition string, only valid when syntax is SYNTAX_EDITIONS. This field should be ignored, instead
+        /// the edition should be inherited from Api. This is similar to Field and EnumValue.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("edition")]
+        public virtual string Edition { get; set; }
+
         /// <summary>The simple name of this method.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("name")]
         public virtual string Name { get; set; }
@@ -5295,7 +5487,10 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("responseTypeUrl")]
         public virtual string ResponseTypeUrl { get; set; }
 
-        /// <summary>The source syntax of this method.</summary>
+        /// <summary>
+        /// The source syntax of this method. This field should be ignored, instead the syntax should be inherited from
+        /// Api. This is similar to Field and EnumValue.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("syntax")]
         public virtual string Syntax { get; set; }
 
@@ -5776,7 +5971,11 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>A protocol buffer option, which can be attached to a message, field, enumeration, etc.</summary>
+    /// <summary>
+    /// A protocol buffer option, which can be attached to a message, field, enumeration, etc. New usages of this
+    /// message as an alternative to FileOptions, MessageOptions, FieldOptions, EnumOptions, EnumValueOptions,
+    /// ServiceOptions, or MethodOptions are strongly discouraged.
+    /// </summary>
     public class Option : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
@@ -6555,7 +6754,11 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
         public virtual string ETag { get; set; }
     }
 
-    /// <summary>A protocol buffer message type.</summary>
+    /// <summary>
+    /// A protocol buffer message type. New usages of this message as an alternative to DescriptorProto are strongly
+    /// discouraged. This message does not reliability preserve all information necessary to model the schema and
+    /// preserve semantics. Instead make use of FileDescriptorSet which preserves the necessary information.
+    /// </summary>
     public class Type : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>The source edition string, only valid when syntax is SYNTAX_EDITIONS.</summary>
@@ -6602,6 +6805,13 @@ namespace Google.Apis.ServiceUsage.v1beta1.Data
 
     /// <summary>Metadata for the `UpdateConsumerPolicy` method.</summary>
     public class UpdateConsumerPolicyMetadata : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>Metadata for the `UpdateMcpPolicy` method.</summary>
+    public class UpdateMcpPolicyMetadata : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
