5127ddc851d100c6169ce6b8f34acfce3266fcfb
Amanda Tarafa Mas <atarafamas@google.com>
2025-10-13 08:08:01 +0000

    feat: Generate Google.Apis.NetworkSecurity.v1 version 1.72.0.3926

--- Src/Generated/Google.Apis.NetworkSecurity.v1/Google.Apis.NetworkSecurity.v1.cs
+++ Src/Generated/Google.Apis.NetworkSecurity.v1/Google.Apis.NetworkSecurity.v1.cs
@@ -13279,6 +13279,10 @@ namespace Google.Apis.NetworkSecurity.v1.Data
     /// <summary>Settings for the endpoint.</summary>
     public class FirewallEndpointEndpointSettings : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>Optional. Immutable. Indicates whether Jumbo Frames are enabled. Default value is false.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("jumboFramesEnabled")]
+        public virtual System.Nullable<bool> JumboFramesEnabled { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
