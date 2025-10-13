a2fb3a10ad9e115d8ce47a5e60ff1bca5cadf92b
Amanda Tarafa Mas <atarafamas@google.com>
2025-10-13 08:07:15 +0000

    feat: Generate Google.Apis.CloudFilestore.v1beta1 version 1.72.0.3924

--- Src/Generated/Google.Apis.CloudFilestore.v1beta1/Google.Apis.CloudFilestore.v1beta1.cs
+++ Src/Generated/Google.Apis.CloudFilestore.v1beta1/Google.Apis.CloudFilestore.v1beta1.cs
@@ -2854,6 +2854,14 @@ namespace Google.Apis.CloudFilestore.v1beta1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("sourceBackup")]
         public virtual string SourceBackup { get; set; }
 
+        /// <summary>
+        /// The resource name of the BackupDR backup, in the format
+        /// `projects/{project_id}/locations/{location_id}/backupVaults/{backupvault_id}/dataSources/{datasource_id}/backups/{backup_id}`,
+        /// TODO (b/443690479) - Remove visibility restrictions once the feature is ready
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("sourceBackupdrBackup")]
+        public virtual string SourceBackupdrBackup { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
