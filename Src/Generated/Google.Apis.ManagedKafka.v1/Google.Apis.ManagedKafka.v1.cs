30a8abc0f6ad488d3e16a50615c1d323b30af2e0
Amanda Tarafa Mas <atarafamas@google.com>
2025-10-20 08:07:00 +0000

    feat: Generate Google.Apis.ManagedKafka.v1 version 1.72.0.3934

--- Src/Generated/Google.Apis.ManagedKafka.v1/Google.Apis.ManagedKafka.v1.cs
+++ Src/Generated/Google.Apis.ManagedKafka.v1/Google.Apis.ManagedKafka.v1.cs
@@ -3143,6 +3143,17 @@ namespace Google.Apis.ManagedKafka.v1
                     [Google.Apis.Util.RequestParameterAttribute("pageToken", Google.Apis.Util.RequestParameterType.Query)]
                     public virtual string PageToken { get; set; }
 
+                    /// <summary>
+                    /// When set to `true`, operations that are reachable are returned as normal, and those that are
+                    /// unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be
+                    /// `true` when reading across collections e.g. when `parent` is set to
+                    /// `"projects/example/locations/-"`. This field is not by default supported and will result in an
+                    /// `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product
+                    /// specific documentation.
+                    /// </summary>
+                    [Google.Apis.Util.RequestParameterAttribute("returnPartialSuccess", Google.Apis.Util.RequestParameterType.Query)]
+                    public virtual System.Nullable<bool> ReturnPartialSuccess { get; set; }
+
                     /// <summary>Gets the method name.</summary>
                     public override string MethodName => "list";
 
@@ -3183,6 +3194,14 @@ namespace Google.Apis.ManagedKafka.v1
                         RequestParameters.Add("pageToken", new Google.Apis.Discovery.Parameter
                         {
                             Name = "pageToken",
+                            IsRequired = false,
+                            ParameterType = "query",
+                            DefaultValue = null,
+                            Pattern = null,
+                        });
+                        RequestParameters.Add("returnPartialSuccess", new Google.Apis.Discovery.Parameter
+                        {
+                            Name = "returnPartialSuccess",
                             IsRequired = false,
                             ParameterType = "query",
                             DefaultValue = null,
@@ -7916,6 +7935,14 @@ namespace Google.Apis.ManagedKafka.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("operations")]
         public virtual System.Collections.Generic.IList<Operation> Operations { get; set; }
 
+        /// <summary>
+        /// Unordered list. Unreachable resources. Populated when the request sets
+        /// `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all
+        /// resources across all supported locations.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("unreachable")]
+        public virtual System.Collections.Generic.IList<string> Unreachable { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
@@ -8431,11 +8458,13 @@ namespace Google.Apis.ManagedKafka.v1.Data
     }
 
     /// <summary>
-    /// Task Retry Policy is implemented on a best-effort basis. Retry delay will be exponential based on provided
-    /// minimum and maximum backoffs. https://en.wikipedia.org/wiki/Exponential_backoff. Note that the delay between
-    /// consecutive task restarts may not always precisely match the configured settings. This can happen when the
-    /// ConnectCluster is in rebalancing state or if the ConnectCluster is unresponsive etc. The default values for
-    /// minimum and maximum backoffs are 60 seconds and 30 minutes respectively.
+    /// Task Retry Policy is implemented on a best-effort basis. The default policy retries tasks with a minimum_backoff
+    /// of 60 seconds, and a maximum_backoff of 12 hours. You can disable the policy by setting the task_retry_disabled
+    /// field to true. Retry delay will be exponential based on provided minimum and maximum backoffs.
+    /// https://en.wikipedia.org/wiki/Exponential_backoff. Note that the delay between consecutive task restarts may not
+    /// always precisely match the configured settings. This can happen when the ConnectCluster is in rebalancing state
+    /// or if the ConnectCluster is unresponsive etc. The default values for minimum and maximum backoffs are 60 seconds
+    /// and 12 hours respectively.
     /// </summary>
     public class TaskRetryPolicy : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -8452,6 +8481,10 @@ namespace Google.Apis.ManagedKafka.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("minimumBackoff")]
         public virtual object MinimumBackoff { get; set; }
+
+        /// <summary>Optional. If true, task retry is disabled.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("taskRetryDisabled")]
+        public virtual System.Nullable<bool> TaskRetryDisabled { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
