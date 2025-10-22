9ab2dd874cc994a71325dc8fbcfb1bf46dfb007c
Amanda Tarafa Mas <atarafamas@google.com>
2025-10-22 08:05:05 +0000

    feat: Generate Google.Apis.FirebaseML.v2beta version 1.72.0.3944

--- Src/Generated/Google.Apis.FirebaseML.v2beta/Google.Apis.FirebaseML.v2beta.cs
+++ Src/Generated/Google.Apis.FirebaseML.v2beta/Google.Apis.FirebaseML.v2beta.cs
@@ -2728,9 +2728,11 @@ namespace Google.Apis.FirebaseML.v2beta.Data
         public virtual string Environment { get; set; }
 
         /// <summary>
-        /// Optional. By default, predefined functions are included in the final model call. Some of them can be
-        /// explicitly excluded from being automatically included. This can serve two purposes: 1. Using a more
-        /// restricted / different action space. 2. Improving the definitions / instructions of predefined functions.
+        /// Optional. By default, [predefined
+        /// functions](https://cloud.google.com/vertex-ai/generative-ai/docs/computer-use#supported-actions) are
+        /// included in the final model call. Some of them can be explicitly excluded from being automatically included.
+        /// This can serve two purposes: 1. Using a more restricted / different action space. 2. Improving the
+        /// definitions / instructions of predefined functions.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("excludedPredefinedFunctions")]
         public virtual System.Collections.Generic.IList<string> ExcludedPredefinedFunctions { get; set; }
