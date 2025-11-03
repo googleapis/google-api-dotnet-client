cc7bee6a2a227a4571dbc541d6a93e10031eb411
Amanda Tarafa Mas <atarafamas@google.com>
2025-11-03 09:07:49 +0000

    feat: Generate Google.Apis.Datastream.v1 version 1.72.0.3946

--- Src/Generated/Google.Apis.Datastream.v1/Google.Apis.Datastream.v1.cs
+++ Src/Generated/Google.Apis.Datastream.v1/Google.Apis.Datastream.v1.cs
@@ -1052,6 +1052,17 @@ namespace Google.Apis.Datastream.v1
                     [Google.Apis.Util.RequestParameterAttribute("pageToken", Google.Apis.Util.RequestParameterType.Query)]
                     public virtual string PageToken { get; set; }
 
+                    /// <summary>
+                    /// When set to `true`, operations that are reachable are returned as normal, and those that are
+                    /// unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be
+                    /// `true` when reading across collections e.g. when `parent` is set to
+                    /// `"projects/example/locations/-"`. This field is not by default supported and will result in an
+                    /// `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product
+                    /// specific documentation.
+                    /// </summary>
+                    [Google.Apis.Util.RequestParameterAttribute("returnPartialSuccess", Google.Apis.Util.RequestParameterType.Query)]
+                    public virtual System.Nullable<bool> ReturnPartialSuccess { get; set; }
+
                     /// <summary>Gets the method name.</summary>
                     public override string MethodName => "list";
 
@@ -1092,6 +1103,14 @@ namespace Google.Apis.Datastream.v1
                         RequestParameters.Add("pageToken", new Google.Apis.Discovery.Parameter
                         {
                             Name = "pageToken",
+                            IsRequired = false,
+                            ParameterType = "query",
+                            DefaultValue = null,
+                            Pattern = null,
+                        });
+                        RequestParameters.Add("returnPartialSuccess", new Google.Apis.Discovery.Parameter
+                        {
+                            Name = "returnPartialSuccess",
                             IsRequired = false,
                             ParameterType = "query",
                             DefaultValue = null,
@@ -3528,6 +3547,20 @@ namespace Google.Apis.Datastream.v1.Data
         public virtual string ETag { get; set; }
     }
 
+    /// <summary>Represents a filter for included data on a stream object.</summary>
+    public class EventFilter : Google.Apis.Requests.IDirectResponseSchema
+    {
+        /// <summary>
+        /// An SQL-query Where clause selecting which data should be included, not including the "WHERE" keyword. E.g.,
+        /// "t.key1 = 'value1' AND t.key2 = 'value2'".
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("sqlWhereClause")]
+        public virtual string SqlWhereClause { get; set; }
+
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
     /// <summary>Response message for a 'FetchStaticIps' response.</summary>
     public class FetchStaticIpsResponse : Google.Apis.Requests.IDirectResponseSchema
     {
@@ -3706,6 +3739,14 @@ namespace Google.Apis.Datastream.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("operations")]
         public virtual System.Collections.Generic.IList<Operation> Operations { get; set; }
 
+        /// <summary>
+        /// Unordered list. Unreachable resources. Populated when the request sets
+        /// `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all
+        /// resources across all supported locations.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("unreachable")]
+        public virtual System.Collections.Generic.IList<string> Unreachable { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
@@ -3870,7 +3911,7 @@ namespace Google.Apis.Datastream.v1.Data
 
         private object _startTime;
 
-        /// <summary>Required. The timestamp (in epoch seconds) to start change stream from.</summary>
+        /// <summary>Required. The timestamp to start change stream from.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("startTime")]
         public virtual string StartTimeRaw
         {
@@ -5823,6 +5864,13 @@ namespace Google.Apis.Datastream.v1.Data
     /// <summary>Request for manually initiating a backfill job for a specific stream object.</summary>
     public class StartBackfillJobRequest : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>
+        /// Optional. Optional event filter. If not set, or empty, the backfill will be performed on the entire object.
+        /// This is currently used for partial backfill and only supported for SQL Server sources.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("eventFilter")]
+        public virtual EventFilter EventFilter { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
