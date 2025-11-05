58436cee57f39e4c3f6f9219cafc1aaaf62f7214
Amanda Tarafa Mas <atarafamas@google.com>
2025-11-05 09:03:59 +0000

    feat: Generate Google.Apis.Merchant.datasources_v1 version 1.72.0.3958

--- Src/Generated/Google.Apis.Merchant.datasources_v1/Google.Apis.Merchant.datasources_v1.cs
+++ Src/Generated/Google.Apis.Merchant.datasources_v1/Google.Apis.Merchant.datasources_v1.cs
@@ -1431,8 +1431,10 @@ namespace Google.Apis.Merchant.datasources_v1.Data
     }
 
     /// <summary>
-    /// The supplemental data source for local and online products. After creation, you should make sure to link the
-    /// supplemental product data source into one or more primary product data sources.
+    /// The [supplemental data
+    /// source](https://developers.google.com/merchant/api/guides/data-sources/api-sources#link-supplemental-data-source)
+    /// for local and online products. After creation,you should make sure to link the supplemental product data source
+    /// into one or more primary product data sources.
     /// </summary>
     public class SupplementalProductDataSource : Google.Apis.Requests.IDirectResponseSchema
     {
