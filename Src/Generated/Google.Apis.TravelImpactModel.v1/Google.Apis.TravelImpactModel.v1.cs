3db73cef3ea28b6bfe4df24a95cdbaac8db780a4
Amanda Tarafa Mas <atarafamas@google.com>
2025-11-05 09:04:10 +0000

    feat: Generate Google.Apis.TravelImpactModel.v1 version 1.72.0.3958

--- Src/Generated/Google.Apis.TravelImpactModel.v1/Google.Apis.TravelImpactModel.v1.cs
+++ Src/Generated/Google.Apis.TravelImpactModel.v1/Google.Apis.TravelImpactModel.v1.cs
@@ -809,9 +809,10 @@ namespace Google.Apis.TravelImpactModel.v1.Data
         public virtual string CabinClass { get; set; }
 
         /// <summary>
-        /// Optional. IATA carrier code, e.g. `KE`. This is required if specific flight matching is desired. Otherwise,
-        /// this is unused for typical flight and distance-based emissions models. This could be both operating and
-        /// marketing carrier code (i.e. codeshare is covered).
+        /// Optional. 2-character [IATA carrier code](https://www.iata.org/en/publications/directories/code-search/),
+        /// e.g. `KE`. This is required if specific flight matching is desired. Otherwise, this is unused for typical
+        /// flight and distance-based emissions models. This could be both operating and marketing carrier code (i.e.
+        /// codeshare is covered).
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("carrierCode")]
         public virtual string CarrierCode { get; set; }
@@ -826,37 +827,40 @@ namespace Google.Apis.TravelImpactModel.v1.Data
         public virtual Date DepartureDate { get; set; }
 
         /// <summary>
-        /// Optional. IATA airport code for flight destination, e.g. `ICN`. This is used to match specific flight if
-        /// provided alongside origin, carrier, and flight number. If there is no match, we will first try to match the
-        /// flight to a typical flight between the provided origin and destination airports. Otherwise, we will use the
-        /// distance-based emissions model if the flight distance is provided.
+        /// Optional. 3-character [IATA airport code](https://www.iata.org/en/publications/directories/code-search/) for
+        /// flight destination, e.g. `ICN`. This is used to match specific flight if provided alongside origin, carrier,
+        /// and flight number. If there is no match, we will first try to match the flight to a typical flight between
+        /// the provided origin and destination airports. Otherwise, we will use the distance-based emissions model if
+        /// the flight distance is provided.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("destination")]
         public virtual string Destination { get; set; }
 
         /// <summary>
-        /// Optional. Distance in kilometers, e.g. `2423`. This is used to match a flight to distance-based emissions
-        /// when origin and destination are not provided or there are no matching typical flights. This field supports
-        /// values between 0 and 2.5e16 km.
+        /// Optional. Distance in kilometers, e.g. `2423`, from [1, 2.5e16) km. This is used to match a flight to
+        /// distance-based emissions when origin and destination are not provided or there are no matching typical
+        /// flights.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("distanceKm")]
         public virtual System.Nullable<long> DistanceKm { get; set; }
 
         /// <summary>
-        /// Optional. Flight number, e.g. `71`. This is first used to match a specific flight if a flight number is
-        /// specified alongside origin, destination, and carrier. If a flight number is not specified, we will first try
-        /// to match the flight to a typical flight between the provided origin and destination airports. If that fails
-        /// and/or origin &amp;amp; destination are not provided, we will use the distance-based emissions model based
-        /// on the flight distance provided.
+        /// Optional. Up to 4-digit [flight number](https://en.wikipedia.org/wiki/Flight_number), e.g. `71`, from [1,
+        /// 9999]. This is first used to match a specific flight if a flight number is specified alongside origin,
+        /// destination, and carrier. If a flight number is not specified, we will first try to match the flight to a
+        /// typical flight between the provided origin and destination airports. If that fails and/or origin &amp;amp;
+        /// destination are not provided, we will use the distance-based emissions model based on the flight distance
+        /// provided.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("flightNumber")]
         public virtual System.Nullable<int> FlightNumber { get; set; }
 
         /// <summary>
-        /// Optional. IATA airport code for flight origin, e.g. `YVR`. This is used to match specific flight if provided
-        /// alongside destination, carrier, and flight number. If there is no match, we will first try to match the
-        /// flight to a typical flight between the provided origin and destination airports. Otherwise, we will use the
-        /// distance-based emissions model if the flight distance is provided.
+        /// Optional. 3-character [IATA airport code](https://www.iata.org/en/publications/directories/code-search/) for
+        /// flight origin, e.g. `YVR`. This is used to match specific flight if provided alongside destination, carrier,
+        /// and flight number. If there is no match, we will first try to match the flight to a typical flight between
+        /// the provided origin and destination airports. Otherwise, we will use the distance-based emissions model if
+        /// the flight distance is provided.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("origin")]
         public virtual string Origin { get; set; }
