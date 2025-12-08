c73ba5174ca6b7821747fc3d91a6fd91e0562427
Amanda Tarafa Mas <atarafamas@google.com>
2025-12-08 09:07:00 +0000

    feat: Generate Google.Apis.CloudDataplex.v1 version 1.73.0.3984

--- Src/Generated/Google.Apis.CloudDataplex.v1/Google.Apis.CloudDataplex.v1.cs
+++ Src/Generated/Google.Apis.CloudDataplex.v1/Google.Apis.CloudDataplex.v1.cs
@@ -1118,9 +1118,10 @@ namespace Google.Apis.CloudDataplex.v1
                     /// <summary>
                     /// When set to true, operations that are reachable are returned as normal, and those that are
                     /// unreachable are returned in the ListOperationsResponse.unreachable field.This can only be true
-                    /// when reading across collections e.g. when parent is set to "projects/example/locations/-".This
-                    /// field is not by default supported and will result in an UNIMPLEMENTED error if set unless
-                    /// explicitly documented otherwise in service or product specific documentation.
+                    /// when reading across collections. For example, when parent is set to
+                    /// "projects/example/locations/-".This field is not supported by default and will result in an
+                    /// UNIMPLEMENTED error if set unless explicitly documented otherwise in service or product specific
+                    /// documentation.
                     /// </summary>
                     [Google.Apis.Util.RequestParameterAttribute("returnPartialSuccess", Google.Apis.Util.RequestParameterType.Query)]
                     public virtual System.Nullable<bool> ReturnPartialSuccess { get; set; }
@@ -5537,7 +5538,10 @@ namespace Google.Apis.CloudDataplex.v1
                             [Google.Apis.Util.StringValueAttribute("CUSTOM")]
                             CUSTOM = 3,
 
-                            /// <summary></summary>
+                            /// <summary>
+                            /// Returns all aspects. If the number of aspects exceeds 100, the first 100 will be
+                            /// returned.
+                            /// </summary>
                             [Google.Apis.Util.StringValueAttribute("ALL")]
                             ALL = 4,
                         }
@@ -15994,9 +15998,10 @@ namespace Google.Apis.CloudDataplex.v1
                     /// <summary>
                     /// When set to true, operations that are reachable are returned as normal, and those that are
                     /// unreachable are returned in the ListOperationsResponse.unreachable field.This can only be true
-                    /// when reading across collections e.g. when parent is set to "projects/example/locations/-".This
-                    /// field is not by default supported and will result in an UNIMPLEMENTED error if set unless
-                    /// explicitly documented otherwise in service or product specific documentation.
+                    /// when reading across collections. For example, when parent is set to
+                    /// "projects/example/locations/-".This field is not supported by default and will result in an
+                    /// UNIMPLEMENTED error if set unless explicitly documented otherwise in service or product specific
+                    /// documentation.
                     /// </summary>
                     [Google.Apis.Util.RequestParameterAttribute("returnPartialSuccess", Google.Apis.Util.RequestParameterType.Query)]
                     public virtual System.Nullable<bool> ReturnPartialSuccess { get; set; }
@@ -16290,7 +16295,9 @@ namespace Google.Apis.CloudDataplex.v1
                     [Google.Apis.Util.StringValueAttribute("CUSTOM")]
                     CUSTOM = 3,
 
-                    /// <summary></summary>
+                    /// <summary>
+                    /// Returns all aspects. If the number of aspects exceeds 100, the first 100 will be returned.
+                    /// </summary>
                     [Google.Apis.Util.StringValueAttribute("ALL")]
                     ALL = 4,
                 }
@@ -18566,6 +18573,10 @@ namespace Google.Apis.CloudDataplex.v1.Data
     /// <summary>DataDocumentation scan related spec.</summary>
     public class GoogleCloudDataplexV1DataDocumentationSpec : Google.Apis.Requests.IDirectResponseSchema
     {
+        /// <summary>Optional. Whether to publish result to Dataplex Catalog.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("catalogPublishingEnabled")]
+        public virtual System.Nullable<bool> CatalogPublishingEnabled { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
@@ -25601,8 +25612,8 @@ namespace Google.Apis.CloudDataplex.v1.Data
 
         /// <summary>
         /// Unordered list. Unreachable resources. Populated when the request sets
-        /// ListOperationsRequest.return_partial_success and reads across collections e.g. when attempting to list all
-        /// resources across all supported locations.
+        /// ListOperationsRequest.return_partial_success and reads across collections. For example, when attempting to
+        /// list all resources across all supported locations.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("unreachable")]
         public virtual System.Collections.Generic.IList<string> Unreachable { get; set; }
