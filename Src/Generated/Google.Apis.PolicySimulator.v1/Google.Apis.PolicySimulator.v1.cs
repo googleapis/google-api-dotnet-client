e8c2f194ac14c3b442a73c54741ece85e492184b
Amanda Tarafa Mas <atarafamas@google.com>
2025-12-08 09:08:23 +0000

    feat: Generate Google.Apis.PolicySimulator.v1 version 1.73.0.3983

--- Src/Generated/Google.Apis.PolicySimulator.v1/Google.Apis.PolicySimulator.v1.cs
+++ Src/Generated/Google.Apis.PolicySimulator.v1/Google.Apis.PolicySimulator.v1.cs
@@ -613,9 +613,9 @@ namespace Google.Apis.PolicySimulator.v1
 
                         /// <summary>
                         /// When set to `true`, operations that are reachable are returned as normal, and those that are
-                        /// unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be
-                        /// `true` when reading across collections e.g. when `parent` is set to
-                        /// `"projects/example/locations/-"`. This field is not by default supported and will result in
+                        /// unreachable are returned in the ListOperationsResponse.unreachable field. This can only be
+                        /// `true` when reading across collections. For example, when `parent` is set to
+                        /// `"projects/example/locations/-"`. This field is not supported by default and will result in
                         /// an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product
                         /// specific documentation.
                         /// </summary>
@@ -1006,9 +1006,9 @@ namespace Google.Apis.PolicySimulator.v1
 
             /// <summary>
             /// When set to `true`, operations that are reachable are returned as normal, and those that are unreachable
-            /// are returned in the [ListOperationsResponse.unreachable] field. This can only be `true` when reading
-            /// across collections e.g. when `parent` is set to `"projects/example/locations/-"`. This field is not by
-            /// default supported and will result in an `UNIMPLEMENTED` error if set unless explicitly documented
+            /// are returned in the ListOperationsResponse.unreachable field. This can only be `true` when reading
+            /// across collections. For example, when `parent` is set to `"projects/example/locations/-"`. This field is
+            /// not supported by default and will result in an `UNIMPLEMENTED` error if set unless explicitly documented
             /// otherwise in service or product specific documentation.
             /// </summary>
             [Google.Apis.Util.RequestParameterAttribute("returnPartialSuccess", Google.Apis.Util.RequestParameterType.Query)]
@@ -1741,9 +1741,9 @@ namespace Google.Apis.PolicySimulator.v1
 
                         /// <summary>
                         /// When set to `true`, operations that are reachable are returned as normal, and those that are
-                        /// unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be
-                        /// `true` when reading across collections e.g. when `parent` is set to
-                        /// `"projects/example/locations/-"`. This field is not by default supported and will result in
+                        /// unreachable are returned in the ListOperationsResponse.unreachable field. This can only be
+                        /// `true` when reading across collections. For example, when `parent` is set to
+                        /// `"projects/example/locations/-"`. This field is not supported by default and will result in
                         /// an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product
                         /// specific documentation.
                         /// </summary>
@@ -2367,9 +2367,9 @@ namespace Google.Apis.PolicySimulator.v1
 
                         /// <summary>
                         /// When set to `true`, operations that are reachable are returned as normal, and those that are
-                        /// unreachable are returned in the [ListOperationsResponse.unreachable] field. This can only be
-                        /// `true` when reading across collections e.g. when `parent` is set to
-                        /// `"projects/example/locations/-"`. This field is not by default supported and will result in
+                        /// unreachable are returned in the ListOperationsResponse.unreachable field. This can only be
+                        /// `true` when reading across collections. For example, when `parent` is set to
+                        /// `"projects/example/locations/-"`. This field is not supported by default and will result in
                         /// an `UNIMPLEMENTED` error if set unless explicitly documented otherwise in service or product
                         /// specific documentation.
                         /// </summary>
@@ -2690,8 +2690,8 @@ namespace Google.Apis.PolicySimulator.v1.Data
 
         /// <summary>
         /// A Common Expression Language (CEL) condition which is used in the evaluation of the constraint. For example:
-        /// `resource.instanceName.matches("[production|test]_.*_(\d)+")` or, `resource.management.auto_upgrade == true`
-        /// The max length of the condition is 1000 characters.
+        /// `resource.instanceName.matches("(production|test)_(.+_)?[\d]+")` or, `resource.management.auto_upgrade ==
+        /// true` The max length of the condition is 1000 characters.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("condition")]
         public virtual string Condition { get; set; }
@@ -2908,14 +2908,22 @@ namespace Google.Apis.PolicySimulator.v1.Data
         public virtual System.Nullable<bool> AllowAll { get; set; }
 
         /// <summary>
-        /// A condition which determines whether this rule is used in the evaluation of the policy. When set, the
-        /// `expression` field in the `Expr' must include from 1 to 10 subexpressions, joined by the "||" or
-        /// "&amp;amp;&amp;amp;" operators. Each subexpression must be of the form
-        /// "resource.matchTag('/tag_key_short_name, 'tag_value_short_name')". or "resource.matchTagId('tagKeys/key_id',
-        /// 'tagValues/value_id')". where key_name and value_name are the resource names for Label Keys and Values.
-        /// These names are available from the Tag Manager Service. An example expression is:
-        /// "resource.matchTag('123456789/environment, 'prod')". or "resource.matchTagId('tagKeys/123',
-        /// 'tagValues/456')".
+        /// A condition that determines whether this rule is used to evaluate the policy. When set, the
+        /// google.type.Expr.expression field must contain 1 to 10 subexpressions, joined by the `||` or
+        /// `&amp;amp;&amp;amp;` operators. Each subexpression must use the `resource.matchTag()`,
+        /// `resource.matchTagId()`, `resource.hasTagKey()`, or `resource.hasTagKeyId()` Common Expression Language
+        /// (CEL) function. The `resource.matchTag()` function takes the following arguments: * `key_name`: the
+        /// namespaced name of the tag key, with the organization ID and a slash (`/`) as a prefix; for example,
+        /// `123456789012/environment` * `value_name`: the short name of the tag value For example:
+        /// `resource.matchTag('123456789012/environment, 'prod')` The `resource.matchTagId()` function takes the
+        /// following arguments: * `key_id`: the permanent ID of the tag key; for example, `tagKeys/123456789012` *
+        /// `value_id`: the permanent ID of the tag value; for example, `tagValues/567890123456` For example:
+        /// `resource.matchTagId('tagKeys/123456789012', 'tagValues/567890123456')` The `resource.hasTagKey()` function
+        /// takes the following argument: * `key_name`: the namespaced name of the tag key, with the organization ID and
+        /// a slash (`/`) as a prefix; for example, `123456789012/environment` For example:
+        /// `resource.hasTagKey('123456789012/environment')` The `resource.hasTagKeyId()` function takes the following
+        /// arguments: * `key_id`: the permanent ID of the tag key; for example, `tagKeys/123456789012` For example:
+        /// `resource.hasTagKeyId('tagKeys/123456789012')`
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("condition")]
         public virtual GoogleTypeExpr Condition { get; set; }
@@ -4290,8 +4298,8 @@ namespace Google.Apis.PolicySimulator.v1.Data
 
         /// <summary>
         /// Unordered list. Unreachable resources. Populated when the request sets
-        /// `ListOperationsRequest.return_partial_success` and reads across collections e.g. when attempting to list all
-        /// resources across all supported locations.
+        /// `ListOperationsRequest.return_partial_success` and reads across collections. For example, when attempting to
+        /// list all resources across all supported locations.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("unreachable")]
         public virtual System.Collections.Generic.IList<string> Unreachable { get; set; }
