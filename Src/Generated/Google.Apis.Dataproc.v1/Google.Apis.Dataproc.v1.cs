54862e65157c1daf54984b86ba424327cdef0df1
Amanda Tarafa Mas <atarafamas@google.com>
2025-12-15 09:08:16 +0000

    feat: Generate Google.Apis.Dataproc.v1 version 1.73.0.3989

--- Src/Generated/Google.Apis.Dataproc.v1/Google.Apis.Dataproc.v1.cs
+++ Src/Generated/Google.Apis.Dataproc.v1/Google.Apis.Dataproc.v1.cs
@@ -13946,6 +13946,18 @@ namespace Google.Apis.Dataproc.v1.Data
     public class GceClusterConfig : Google.Apis.Requests.IDirectResponseSchema
     {
         /// <summary>
+        /// Optional. An optional list of Compute Engine zones where the Dataproc cluster will not be located when Auto
+        /// Zone is enabled. Only one of zone_uri or auto_zone_exclude_zone_uris can be set. If both are omitted, the
+        /// service will pick a zone in the cluster Compute Engine region. If auto_zone_exclude_zone_uris is set and
+        /// there is more than one non-excluded zone, the service will pick one of the non-excluded zones. Otherwise,
+        /// cluster creation will fail with INVALID_ARGUMENT error.A full URL, partial URI, or short name are valid.
+        /// Examples: https://www.googleapis.com/compute/v1/projects/[project_id]/zones/[zone]
+        /// projects/[project_id]/zones/[zone] [zone]
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("autoZoneExcludeZoneUris")]
+        public virtual System.Collections.Generic.IList<string> AutoZoneExcludeZoneUris { get; set; }
+
+        /// <summary>
         /// Optional. Confidential Instance Config for clusters using Confidential VMs
         /// (https://cloud.google.com/compute/confidential-vm/docs).
         /// </summary>
