6eeb4619836b8f0991e3d0a7624dfdd40aac8833
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-07 09:04:23 +0000

    feat: Generate Google.Apis.HangoutsChat.v1 version 1.73.0.4018

--- Src/Generated/Google.Apis.HangoutsChat.v1/Google.Apis.HangoutsChat.v1.cs
+++ Src/Generated/Google.Apis.HangoutsChat.v1/Google.Apis.HangoutsChat.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8760,8 +8760,6 @@ namespace Google.Apis.HangoutsChat.v1.Data
         /// authentication](https://developers.google.com/workspace/chat/authenticate-authorize-chat-app) and omit the
         /// following: *
         /// [Attachments](https://developers.google.com/workspace/chat/api/reference/rest/v1/spaces.messages.attachments)
-        /// * [Accessory
-        /// widgets](https://developers.google.com/workspace/chat/api/reference/rest/v1/spaces.messages#Message.AccessoryWidget)
         /// For details, see [Send a message
         /// privately](https://developers.google.com/workspace/chat/create-messages#private).
         /// </summary>
