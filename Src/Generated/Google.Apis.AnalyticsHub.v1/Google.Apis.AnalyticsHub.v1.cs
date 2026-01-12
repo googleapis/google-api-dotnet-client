8ea54220a35bf4d4c78921665ac99cb21799cc74
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-12 09:06:25 +0000

    feat: Generate Google.Apis.AnalyticsHub.v1 version 1.73.0.3994

--- Src/Generated/Google.Apis.AnalyticsHub.v1/Google.Apis.AnalyticsHub.v1.cs
+++ Src/Generated/Google.Apis.AnalyticsHub.v1/Google.Apis.AnalyticsHub.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -3580,7 +3580,9 @@ namespace Google.Apis.AnalyticsHub.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("messageTransforms")]
         public virtual System.Collections.Generic.IList<MessageTransform> MessageTransforms { get; set; }
 
-        /// <summary>Required. Name of the subscription. Format is `projects/{project}/subscriptions/{sub}`.</summary>
+        /// <summary>
+        /// Required. Identifier. Name of the subscription. Format is `projects/{project}/subscriptions/{sub}`.
+        /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("name")]
         public virtual string Name { get; set; }
 
