456409aff4a2330c4afa968caf4f95782efb7a13
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-14 21:53:00 +0000

    feat: Generate Google.Apis.Firebaseappcheck.v1beta version 1.73.0.4027

--- Src/Generated/Google.Apis.Firebaseappcheck.v1beta/Google.Apis.Firebaseappcheck.v1beta.cs
+++ Src/Generated/Google.Apis.Firebaseappcheck.v1beta/Google.Apis.Firebaseappcheck.v1beta.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -5243,6 +5243,22 @@ namespace Google.Apis.Firebaseappcheck.v1beta.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("customToken")]
         public virtual string CustomToken { get; set; }
+
+        /// <summary>
+        /// Optional. When `limited_use` is set to `true`, this field specifies the desired `jti` claim (Section 4.1.7
+        /// of RFC 7519) in the returned App Check token. *Limited use* App Check tokens with the same `jti` will be
+        /// counted as the same token for the purposes of replay protection. An error is returned if this field is
+        /// specified without setting `limited_use` to `true`. The size of this field is limited to 500 bytes. If
+        /// specified, its length must be at least 16 bytes. If this field is omitted or is empty and `limited_use` is
+        /// set to `true`, a randomly generated `jti` claim with length between 16 and 500 bytes (inclusive) will be
+        /// used in the returned App Check token. Leaving this field empty is only recommended if your custom
+        /// attestation provider itself is not vulnerable to replay attacks. When `limited_use` is set to `false`,
+        /// neither the presence nor the contents of the `jti` claim in the returned App Check token is specified. To
+        /// ensure that the returned App Check token is eligible for limited use functionality, set `limited_use` to
+        /// `true`.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("jti")]
+        public virtual string Jti { get; set; }
 
         /// <summary>
         /// Specifies whether this attestation is for use in a *limited use* (`true`) or *session based* (`false`)
