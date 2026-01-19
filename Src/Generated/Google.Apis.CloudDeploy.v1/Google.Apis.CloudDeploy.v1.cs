e22d480424480c322fc3c018261e9a526660be29
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-19 09:06:04 +0000

    feat: Generate Google.Apis.CloudDeploy.v1 version 1.73.0.4026

--- Src/Generated/Google.Apis.CloudDeploy.v1/Google.Apis.CloudDeploy.v1.cs
+++ Src/Generated/Google.Apis.CloudDeploy.v1/Google.Apis.CloudDeploy.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6365,6 +6365,10 @@ namespace Google.Apis.CloudDeploy.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("timedPromoteReleaseOperation")]
         public virtual TimedPromoteReleaseOperation TimedPromoteReleaseOperation { get; set; }
+
+        /// <summary>Output only. Unique identifier of the `AutomationRun`.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("uid")]
+        public virtual string Uid { get; set; }
 
         private string _updateTimeRaw;
 
