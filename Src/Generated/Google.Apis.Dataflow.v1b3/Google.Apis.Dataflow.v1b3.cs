215b7866344d73f468c0c4c13c65065e0f2f5926
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-26 09:06:40 +0000

    feat: Generate Google.Apis.Dataflow.v1b3 version 1.73.0.4035

--- Src/Generated/Google.Apis.Dataflow.v1b3/Google.Apis.Dataflow.v1b3.cs
+++ Src/Generated/Google.Apis.Dataflow.v1b3/Google.Apis.Dataflow.v1b3.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10438,6 +10438,10 @@ namespace Google.Apis.Dataflow.v1b3.Data
         /// <summary>Set of computation configuration information.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("streamingComputationConfigs")]
         public virtual System.Collections.Generic.IList<StreamingComputationConfig> StreamingComputationConfigs { get; set; }
+
+        /// <summary>Optional. The state tag encoding format version for streaming engine jobs.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("streamingEngineStateTagEncodingVersion")]
+        public virtual System.Nullable<int> StreamingEngineStateTagEncodingVersion { get; set; }
 
         /// <summary>Map from user step names to state families.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("userStepToStateFamilyNameMap")]
