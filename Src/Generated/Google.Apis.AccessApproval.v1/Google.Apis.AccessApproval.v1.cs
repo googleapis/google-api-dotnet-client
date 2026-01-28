f853f36615b31c725a25e72dfc723179ef5c8845
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-28 09:05:39 +0000

    feat: Generate Google.Apis.AccessApproval.v1 version 1.73.0.4043

--- Src/Generated/Google.Apis.AccessApproval.v1/Google.Apis.AccessApproval.v1.cs
+++ Src/Generated/Google.Apis.AccessApproval.v1/Google.Apis.AccessApproval.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -2136,6 +2136,12 @@ namespace Google.Apis.AccessApproval.v1.Data
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("ancestorHasActiveKeyVersion")]
         public virtual System.Nullable<bool> AncestorHasActiveKeyVersion { get; set; }
+
+        /// <summary>
+        /// Output only. Field to differentiate ancestor enrolled services from locally enrolled services.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("ancestorsEnrolledServices")]
+        public virtual System.Collections.Generic.IList<EnrolledService> AncestorsEnrolledServices { get; set; }
 
         /// <summary>
         /// Optional. Policy configuration for Access Approval that sets the operating mode. The available policies are
