dffdfc17113b6c70f670e0f662937a05384f01a1
Amanda Tarafa Mas <atarafamas@google.com>
2026-02-02 09:08:00 +0000

    feat: Generate Google.Apis.Looker.v1 version 1.73.0.4036

--- Src/Generated/Google.Apis.Looker.v1/Google.Apis.Looker.v1.cs
+++ Src/Generated/Google.Apis.Looker.v1/Google.Apis.Looker.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1131,6 +1131,63 @@ namespace Google.Apis.Looker.v1
                         });
                     }
                 }
+
+                /// <summary>Undeletes Looker instance.</summary>
+                /// <param name="body">The body of the request.</param>
+                /// <param name="name">
+                /// Required. Format: projects/{project}/locations/{location}/instances/{instance}
+                /// </param>
+                public virtual UndeleteRequest Undelete(Google.Apis.Looker.v1.Data.UndeleteInstanceRequest body, string name)
+                {
+                    return new UndeleteRequest(this.service, body, name);
+                }
+
+                /// <summary>Undeletes Looker instance.</summary>
+                public class UndeleteRequest : LookerBaseServiceRequest<Google.Apis.Looker.v1.Data.Operation>
+                {
+                    /// <summary>Constructs a new Undelete request.</summary>
+                    public UndeleteRequest(Google.Apis.Services.IClientService service, Google.Apis.Looker.v1.Data.UndeleteInstanceRequest body, string name) : base(service)
+                    {
+                        Name = name;
+                        Body = body;
+                        InitParameters();
+                    }
+
+                    /// <summary>
+                    /// Required. Format: projects/{project}/locations/{location}/instances/{instance}
+                    /// </summary>
+                    [Google.Apis.Util.RequestParameterAttribute("name", Google.Apis.Util.RequestParameterType.Path)]
+                    public virtual string Name { get; private set; }
+
+                    /// <summary>Gets or sets the body of this request.</summary>
+                    Google.Apis.Looker.v1.Data.UndeleteInstanceRequest Body { get; set; }
+
+                    /// <summary>Returns the body of the request.</summary>
+                    protected override object GetBody() => Body;
+
+                    /// <summary>Gets the method name.</summary>
+                    public override string MethodName => "undelete";
+
+                    /// <summary>Gets the HTTP method.</summary>
+                    public override string HttpMethod => "POST";
+
+                    /// <summary>Gets the REST path.</summary>
+                    public override string RestPath => "v1/{+name}:undelete";
+
+                    /// <summary>Initializes Undelete parameter list.</summary>
+                    protected override void InitParameters()
+                    {
+                        base.InitParameters();
+                        RequestParameters.Add("name", new Google.Apis.Discovery.Parameter
+                        {
+                            Name = "name",
+                            IsRequired = true,
+                            ParameterType = "path",
+                            DefaultValue = null,
+                            Pattern = @"^projects/[^/]+/locations/[^/]+/instances/[^/]+$",
+                        });
+                    }
+                }
             }
 
             /// <summary>Gets the Operations resource.</summary>
@@ -1829,6 +1886,10 @@ namespace Google.Apis.Looker.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("adminSettings")]
         public virtual AdminSettings AdminSettings { get; set; }
 
+        /// <summary>Optional. Indicates whether catalog integration is enabled for the Looker instance.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("catalogIntegrationEnabled")]
+        public virtual System.Nullable<bool> CatalogIntegrationEnabled { get; set; }
+
         /// <summary>Optional. Storage class of the instance.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("classType")]
         public virtual string ClassType { get; set; }
@@ -1992,9 +2053,50 @@ namespace Google.Apis.Looker.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("satisfiesPzs")]
         public virtual System.Nullable<bool> SatisfiesPzs { get; set; }
 
+        /// <summary>Output only. The reason for the instance being in a soft-deleted state.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("softDeleteReason")]
+        public virtual string SoftDeleteReason { get; set; }
+
         /// <summary>Output only. The state of the instance.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("state")]
         public virtual string State { get; set; }
+
+        private string _suspendedTimeRaw;
+
+        private object _suspendedTime;
+
+        /// <summary>Output only. The time when the Looker instance was suspended (soft deleted).</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("suspendedTime")]
+        public virtual string SuspendedTimeRaw
+        {
+            get => _suspendedTimeRaw;
+            set
+            {
+                _suspendedTime = Google.Apis.Util.Utilities.DeserializeForGoogleFormat(value);
+                _suspendedTimeRaw = value;
+            }
+        }
+
+        /// <summary><seealso cref="object"/> representation of <see cref="SuspendedTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        [System.ObsoleteAttribute("This property is obsolete and may behave unexpectedly; please use SuspendedTimeDateTimeOffset instead.")]
+        public virtual object SuspendedTime
+        {
+            get => _suspendedTime;
+            set
+            {
+                _suspendedTimeRaw = Google.Apis.Util.Utilities.SerializeForGoogleFormat(value);
+                _suspendedTime = value;
+            }
+        }
+
+        /// <summary><seealso cref="System.DateTimeOffset"/> representation of <see cref="SuspendedTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        public virtual System.DateTimeOffset? SuspendedTimeDateTimeOffset
+        {
+            get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(SuspendedTimeRaw);
+            set => SuspendedTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
+        }
 
         private string _updateTimeRaw;
 
@@ -2684,6 +2786,13 @@ namespace Google.Apis.Looker.v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("seconds")]
         public virtual System.Nullable<int> Seconds { get; set; }
 
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>Request options for undeleting an instance.</summary>
+    public class UndeleteInstanceRequest : Google.Apis.Requests.IDirectResponseSchema
+    {
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
