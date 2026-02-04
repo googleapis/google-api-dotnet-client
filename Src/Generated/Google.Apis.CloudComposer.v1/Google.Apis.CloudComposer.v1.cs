f87ebb74dd45e1b7a86e7c6dbd7f921e64b44a2e
Amanda Tarafa Mas <atarafamas@google.com>
2026-02-04 09:04:39 +0000

    feat: Generate Google.Apis.CloudComposer.v1 version 1.73.0.4044

--- Src/Generated/Google.Apis.CloudComposer.v1/Google.Apis.CloudComposer.v1.cs
+++ Src/Generated/Google.Apis.CloudComposer.v1/Google.Apis.CloudComposer.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -3788,7 +3788,7 @@ namespace Google.Apis.CloudComposer.v1.Data
         /// <summary>
         /// Optional. If `true`, a Private IP Cloud Composer environment is created. If this field is set to true,
         /// `IPAllocationPolicy.use_ip_aliases` must be set to true for Cloud Composer environments in versions
-        /// composer-1.*.*-airflow-*.*.*.
+        /// composer-1.*.*-airflow-*.*.*. This field is going to be deprecated. Use `networking_type` instead.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("enablePrivateEnvironment")]
         public virtual System.Nullable<bool> EnablePrivateEnvironment { get; set; }
