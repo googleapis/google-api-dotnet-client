311fb1b36ed7a53db75a0414f0eeb09214e47a0c
Amanda Tarafa Mas <atarafamas@google.com>
2026-03-02 09:08:08 +0000

    feat: Generate Google.Apis.ServiceNetworking.v1beta version 1.73.0.4071

--- Src/Generated/Google.Apis.ServiceNetworking.v1beta/Google.Apis.ServiceNetworking.v1beta.cs
+++ Src/Generated/Google.Apis.ServiceNetworking.v1beta/Google.Apis.ServiceNetworking.v1beta.cs
@@ -1534,6 +1534,10 @@ namespace Google.Apis.ServiceNetworking.v1beta.Data
         [Newtonsoft.Json.JsonPropertyAttribute("consumerImportSubnetRoutesWithPublicIp")]
         public virtual System.Nullable<bool> ConsumerImportSubnetRoutesWithPublicIp { get; set; }
 
+        /// <summary>Output only. If this is true, consumer peering is active.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("consumerPeeringActive")]
+        public virtual System.Nullable<bool> ConsumerPeeringActive { get; set; }
+
         /// <summary>Export custom routes flag value for peering from producer to consumer.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("producerExportCustomRoutes")]
         public virtual System.Nullable<bool> ProducerExportCustomRoutes { get; set; }
