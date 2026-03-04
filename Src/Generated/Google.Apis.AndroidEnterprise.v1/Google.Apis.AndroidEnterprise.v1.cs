5506bdd4882f9a81ed16521500021ce87eb75f98
Amanda Tarafa Mas <atarafamas@google.com>
2026-03-04 09:05:47 +0000

    feat: Generate Google.Apis.AndroidEnterprise.v1 version 1.73.0.4078

--- Src/Generated/Google.Apis.AndroidEnterprise.v1/Google.Apis.AndroidEnterprise.v1.cs
+++ Src/Generated/Google.Apis.AndroidEnterprise.v1/Google.Apis.AndroidEnterprise.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -489,10 +489,14 @@ namespace Google.Apis.AndroidEnterprise.v1
         }
 
         /// <summary>
-        /// Retrieves whether a device's access to Google services is enabled or disabled. The device state takes effect
-        /// only if enforcing EMM policies on Android devices is enabled in the Google Admin Console. Otherwise, the
-        /// device state is ignored and all devices are allowed access to Google services. This is only supported for
-        /// Google-managed users.
+        /// Checks if a device can access Google apps and services for a user. Returns whether access is "enabled" or
+        /// "disabled". A "disabled" state prevents the user's Managed Google Account on the device from successfully
+        /// authenticating with Google. This blocks access to most Google applications and services, including Google
+        /// Play, as the device cannot prove its entitlement to access them. New devices default to "disabled".
+        /// Important: Enforcement of this state depends on the following conditions: * The user must be a managed
+        /// google account. * The enterprise must be a managed google domain. * Third-party Android mobile management
+        /// must be active in the Google Admin Console for the user's Organizational Unit. If these conditions aren't
+        /// met, access may still be possible even in a "disabled" state.
         /// </summary>
         /// <param name="enterpriseId">The ID of the enterprise.</param>
         /// <param name="userId">The ID of the user.</param>
@@ -503,10 +507,14 @@ namespace Google.Apis.AndroidEnterprise.v1
         }
 
         /// <summary>
-        /// Retrieves whether a device's access to Google services is enabled or disabled. The device state takes effect
-        /// only if enforcing EMM policies on Android devices is enabled in the Google Admin Console. Otherwise, the
-        /// device state is ignored and all devices are allowed access to Google services. This is only supported for
-        /// Google-managed users.
+        /// Checks if a device can access Google apps and services for a user. Returns whether access is "enabled" or
+        /// "disabled". A "disabled" state prevents the user's Managed Google Account on the device from successfully
+        /// authenticating with Google. This blocks access to most Google applications and services, including Google
+        /// Play, as the device cannot prove its entitlement to access them. New devices default to "disabled".
+        /// Important: Enforcement of this state depends on the following conditions: * The user must be a managed
+        /// google account. * The enterprise must be a managed google domain. * Third-party Android mobile management
+        /// must be active in the Google Admin Console for the user's Organizational Unit. If these conditions aren't
+        /// met, access may still be possible even in a "disabled" state.
         /// </summary>
         public class GetStateRequest : AndroidEnterpriseBaseServiceRequest<Google.Apis.AndroidEnterprise.v1.Data.DeviceState>
         {
@@ -631,10 +639,12 @@ namespace Google.Apis.AndroidEnterprise.v1
         }
 
         /// <summary>
-        /// Sets whether a device's access to Google services is enabled or disabled. The device state takes effect only
-        /// if enforcing EMM policies on Android devices is enabled in the Google Admin Console. Otherwise, the device
-        /// state is ignored and all devices are allowed access to Google services. This is only supported for
-        /// Google-managed users.
+        /// Sets whether a device's access to Google services (including Google Play) is enabled or disabled for the
+        /// specified user. Setting the state to "enabled" allows the Google Account to access Google services, while
+        /// "disabled" blocks access by preventing OAuth token issuance. Preconditions for Enforcement: 1. This setting
+        /// is only effective for Google-managed users. 2. The enterprise must be linked to a Google Managed Domain. 3.
+        /// Enforcement requires third-party Android mobile management to be enabled within the Google Admin Console for
+        /// the user's Organizational Unit. If these preconditions are not met, changes to this state may be ignored.
         /// </summary>
         /// <param name="body">The body of the request.</param>
         /// <param name="enterpriseId">The ID of the enterprise.</param>
@@ -646,10 +656,12 @@ namespace Google.Apis.AndroidEnterprise.v1
         }
 
         /// <summary>
-        /// Sets whether a device's access to Google services is enabled or disabled. The device state takes effect only
-        /// if enforcing EMM policies on Android devices is enabled in the Google Admin Console. Otherwise, the device
-        /// state is ignored and all devices are allowed access to Google services. This is only supported for
-        /// Google-managed users.
+        /// Sets whether a device's access to Google services (including Google Play) is enabled or disabled for the
+        /// specified user. Setting the state to "enabled" allows the Google Account to access Google services, while
+        /// "disabled" blocks access by preventing OAuth token issuance. Preconditions for Enforcement: 1. This setting
+        /// is only effective for Google-managed users. 2. The enterprise must be linked to a Google Managed Domain. 3.
+        /// Enforcement requires third-party Android mobile management to be enabled within the Google Admin Console for
+        /// the user's Organizational Unit. If these preconditions are not met, changes to this state may be ignored.
         /// </summary>
         public class SetStateRequest : AndroidEnterpriseBaseServiceRequest<Google.Apis.AndroidEnterprise.v1.Data.DeviceState>
         {
