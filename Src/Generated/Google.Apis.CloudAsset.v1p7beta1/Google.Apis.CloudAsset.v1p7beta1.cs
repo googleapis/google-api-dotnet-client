2da200636b94799afc6c612cdfb4fd6acf873fb8
Amanda Tarafa Mas <atarafamas@google.com>
2026-03-23 08:06:32 +0000

    feat: Generate Google.Apis.CloudAsset.v1p7beta1 version 1.73.0.4090

--- Src/Generated/Google.Apis.CloudAsset.v1p7beta1/Google.Apis.CloudAsset.v1p7beta1.cs
+++ Src/Generated/Google.Apis.CloudAsset.v1p7beta1/Google.Apis.CloudAsset.v1p7beta1.cs
@@ -1584,10 +1584,8 @@ namespace Google.Apis.CloudAsset.v1p7beta1.Data
     {
         /// <summary>
         /// A list of identities that are allowed access through [EgressPolicy]. Identities can be an individual user,
-        /// service account, Google group, or third-party identity. For third-party identity, only single identities are
-        /// supported and other identity types are not supported. The `v1` identities that have the prefix `user`,
-        /// `group`, `serviceAccount`, and `principal` in https://cloud.google.com/iam/docs/principal-identifiers#v1 are
-        /// supported.
+        /// service account, Google group, or third-party identity. For the list of supported identity types, see
+        /// https://docs.cloud.google.com/vpc-service-controls/docs/supported-identities.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("identities")]
         public virtual System.Collections.Generic.IList<string> Identities { get; set; }
@@ -1735,10 +1733,8 @@ namespace Google.Apis.CloudAsset.v1p7beta1.Data
     {
         /// <summary>
         /// A list of identities that are allowed access through [IngressPolicy]. Identities can be an individual user,
-        /// service account, Google group, or third-party identity. For third-party identity, only single identities are
-        /// supported and other identity types are not supported. The `v1` identities that have the prefix `user`,
-        /// `group`, `serviceAccount`, and `principal` in https://cloud.google.com/iam/docs/principal-identifiers#v1 are
-        /// supported.
+        /// service account, Google group, or third-party identity. For the list of supported identity types, see
+        /// https://docs.cloud.google.com/vpc-service-controls/docs/supported-identities.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("identities")]
         public virtual System.Collections.Generic.IList<string> Identities { get; set; }
