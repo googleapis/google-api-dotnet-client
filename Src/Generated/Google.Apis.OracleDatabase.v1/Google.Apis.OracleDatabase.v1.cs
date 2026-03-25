9f02fc977bac9653dc54841edd32c81c2ebfb583
Amanda Tarafa Mas <atarafamas@google.com>
2026-03-25 08:05:03 +0000

    feat: Generate Google.Apis.OracleDatabase.v1 version 1.73.0.4096

--- Src/Generated/Google.Apis.OracleDatabase.v1/Google.Apis.OracleDatabase.v1.cs
+++ Src/Generated/Google.Apis.OracleDatabase.v1/Google.Apis.OracleDatabase.v1.cs
@@ -5751,10 +5751,14 @@ namespace Google.Apis.OracleDatabase.v1
             }
 
             /// <summary>
-            /// Lists information about the supported locations for this service. This method can be called in two ways:
-            /// * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:**
-            /// Use the path `GET /v1/projects/{project_id}/locations`. This may include public locations as well as
-            /// private or other locations specifically visible to the project.
+            /// Lists information about the supported locations for this service. This method lists locations based on
+            /// the resource scope provided in the [ListLocationsRequest.name] field: * **Global locations**: If `name`
+            /// is empty, the method lists the public locations available to all projects. * **Project-specific
+            /// locations**: If `name` follows the format `projects/{project}`, the method lists locations visible to
+            /// that specific project. This includes public, private, or other project-specific locations enabled for
+            /// the project. For gRPC and client library implementations, the resource name is passed as the `name`
+            /// field. For direct service calls, the resource name is incorporated into the request path based on the
+            /// specific service implementation and version.
             /// </summary>
             /// <param name="name">The resource that owns the locations collection, if applicable.</param>
             public virtual ListRequest List(string name)
@@ -5763,10 +5767,14 @@ namespace Google.Apis.OracleDatabase.v1
             }
 
             /// <summary>
-            /// Lists information about the supported locations for this service. This method can be called in two ways:
-            /// * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:**
-            /// Use the path `GET /v1/projects/{project_id}/locations`. This may include public locations as well as
-            /// private or other locations specifically visible to the project.
+            /// Lists information about the supported locations for this service. This method lists locations based on
+            /// the resource scope provided in the [ListLocationsRequest.name] field: * **Global locations**: If `name`
+            /// is empty, the method lists the public locations available to all projects. * **Project-specific
+            /// locations**: If `name` follows the format `projects/{project}`, the method lists locations visible to
+            /// that specific project. This includes public, private, or other project-specific locations enabled for
+            /// the project. For gRPC and client library implementations, the resource name is passed as the `name`
+            /// field. For direct service calls, the resource name is incorporated into the request path based on the
+            /// specific service implementation and version.
             /// </summary>
             public class ListRequest : OracleDatabaseBaseServiceRequest<Google.Apis.OracleDatabase.v1.Data.ListLocationsResponse>
             {
@@ -7729,7 +7737,7 @@ namespace Google.Apis.OracleDatabase.v1.Data
     /// </summary>
     public class Database : Google.Apis.Requests.IDirectResponseSchema
     {
-        /// <summary>Required. The password for the default ADMIN user.</summary>
+        /// <summary>Optional. The password for the default ADMIN user.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("adminPassword")]
         public virtual string AdminPassword { get; set; }
 
