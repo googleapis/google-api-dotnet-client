08f5bb8863cd6bbeebcf702ec49d46fc7bd72cf6
Amanda Tarafa Mas <atarafamas@google.com>
2026-04-06 08:07:21 +0000

    feat: Generate Google.Apis.CloudLocationFinder.v1alpha version 1.73.0.4101

--- Src/Generated/Google.Apis.CloudLocationFinder.v1alpha/Google.Apis.CloudLocationFinder.v1alpha.cs
+++ Src/Generated/Google.Apis.CloudLocationFinder.v1alpha/Google.Apis.CloudLocationFinder.v1alpha.cs
@@ -618,10 +618,14 @@ namespace Google.Apis.CloudLocationFinder.v1alpha
             }
 
             /// <summary>
-            /// Lists information about the supported locations for this service. This method can be called in two ways:
-            /// * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:**
-            /// Use the path `GET /v1/projects/{project_id}/locations`. This may include public locations as well as
-            /// private or other locations specifically visible to the project.
+            /// Lists information about the supported locations for this service. This method lists locations based on
+            /// the resource scope provided in the [ListLocationsRequest.name] field: * **Global locations**: If `name`
+            /// is empty, the method lists the public locations available to all projects. * **Project-specific
+            /// locations**: If `name` follows the format `projects/{project}`, the method lists locations visible to
+            /// that specific project. This includes public, private, or other project-specific locations enabled for
+            /// the project. For gRPC and client library implementations, the resource name is passed as the `name`
+            /// field. For direct service calls, the resource name is incorporated into the request path based on the
+            /// specific service implementation and version.
             /// </summary>
             /// <param name="name">The resource that owns the locations collection, if applicable.</param>
             public virtual ListRequest List(string name)
@@ -630,10 +634,14 @@ namespace Google.Apis.CloudLocationFinder.v1alpha
             }
 
             /// <summary>
-            /// Lists information about the supported locations for this service. This method can be called in two ways:
-            /// * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:**
-            /// Use the path `GET /v1/projects/{project_id}/locations`. This may include public locations as well as
-            /// private or other locations specifically visible to the project.
+            /// Lists information about the supported locations for this service. This method lists locations based on
+            /// the resource scope provided in the [ListLocationsRequest.name] field: * **Global locations**: If `name`
+            /// is empty, the method lists the public locations available to all projects. * **Project-specific
+            /// locations**: If `name` follows the format `projects/{project}`, the method lists locations visible to
+            /// that specific project. This includes public, private, or other project-specific locations enabled for
+            /// the project. For gRPC and client library implementations, the resource name is passed as the `name`
+            /// field. For direct service calls, the resource name is incorporated into the request path based on the
+            /// specific service implementation and version.
             /// </summary>
             public class ListRequest : CloudLocationFinderBaseServiceRequest<Google.Apis.CloudLocationFinder.v1alpha.Data.ListLocationsResponse>
             {
