dcc6329b09d65cac932bfe1057ac9b7e96538ff7
Amanda Tarafa Mas <atarafamas@google.com>
2026-04-06 08:07:44 +0000

    feat: Generate Google.Apis.ContainerAnalysis.v1 version 1.73.0.4096

--- Src/Generated/Google.Apis.ContainerAnalysis.v1/Google.Apis.ContainerAnalysis.v1.cs
+++ Src/Generated/Google.Apis.ContainerAnalysis.v1/Google.Apis.ContainerAnalysis.v1.cs
@@ -8040,6 +8040,45 @@ namespace Google.Apis.ContainerAnalysis.v1.Data
     /// <summary>An instance of an analysis type that has been found on a resource.</summary>
     public class Occurrence : Google.Apis.Requests.IDirectResponseSchema
     {
+        private string _advisoryPublishTimeRaw;
+
+        private object _advisoryPublishTime;
+
+        /// <summary>The time this advisory was published by the source.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("advisoryPublishTime")]
+        public virtual string AdvisoryPublishTimeRaw
+        {
+            get => _advisoryPublishTimeRaw;
+            set
+            {
+                _advisoryPublishTime = Google.Apis.Util.Utilities.DeserializeForGoogleFormat(value);
+                _advisoryPublishTimeRaw = value;
+            }
+        }
+
+        /// <summary><seealso cref="object"/> representation of <see cref="AdvisoryPublishTimeRaw"/>.</summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        [System.ObsoleteAttribute("This property is obsolete and may behave unexpectedly; please use AdvisoryPublishTimeDateTimeOffset instead.")]
+        public virtual object AdvisoryPublishTime
+        {
+            get => _advisoryPublishTime;
+            set
+            {
+                _advisoryPublishTimeRaw = Google.Apis.Util.Utilities.SerializeForGoogleFormat(value);
+                _advisoryPublishTime = value;
+            }
+        }
+
+        /// <summary>
+        /// <seealso cref="System.DateTimeOffset"/> representation of <see cref="AdvisoryPublishTimeRaw"/>.
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnoreAttribute]
+        public virtual System.DateTimeOffset? AdvisoryPublishTimeDateTimeOffset
+        {
+            get => Google.Apis.Util.DiscoveryFormat.ParseGoogleDateTimeToDateTimeOffset(AdvisoryPublishTimeRaw);
+            set => AdvisoryPublishTimeRaw = Google.Apis.Util.DiscoveryFormat.FormatDateTimeOffsetToGoogleDateTime(value);
+        }
+
         /// <summary>Describes an attestation of an artifact.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("attestation")]
         public virtual AttestationOccurrence Attestation { get; set; }
