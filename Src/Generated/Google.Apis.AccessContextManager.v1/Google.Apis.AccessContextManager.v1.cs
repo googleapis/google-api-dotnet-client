9b221d9343f17c9a070c4951f548bb9074d79cbd
Amanda Tarafa Mas <atarafamas@google.com>
2026-04-08 08:04:34 +0000

    feat: Generate Google.Apis.AccessContextManager.v1 version 1.73.0.4105

--- Src/Generated/Google.Apis.AccessContextManager.v1/Google.Apis.AccessContextManager.v1.cs
+++ Src/Generated/Google.Apis.AccessContextManager.v1/Google.Apis.AccessContextManager.v1.cs
@@ -3682,8 +3682,8 @@ namespace Google.Apis.AccessContextManager.v1.Data
     {
         /// <summary>
         /// A list of identities that are allowed access through [EgressPolicy]. Identities can be an individual user,
-        /// service account, Google group, or third-party identity. For the list of supported identity types, see
-        /// https://docs.cloud.google.com/vpc-service-controls/docs/supported-identities.
+        /// service account, Google group, third-party identity, or agent identity. For the list of supported identity
+        /// types, see https://docs.cloud.google.com/vpc-service-controls/docs/supported-identities.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("identities")]
         public virtual System.Collections.Generic.IList<string> Identities { get; set; }
@@ -3985,8 +3985,8 @@ namespace Google.Apis.AccessContextManager.v1.Data
     {
         /// <summary>
         /// A list of identities that are allowed access through [IngressPolicy]. Identities can be an individual user,
-        /// service account, Google group, or third-party identity. For the list of supported identity types, see
-        /// https://docs.cloud.google.com/vpc-service-controls/docs/supported-identities.
+        /// service account, Google group, third-party identity, or agent identity. For the list of supported identity
+        /// types, see https://docs.cloud.google.com/vpc-service-controls/docs/supported-identities.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("identities")]
         public virtual System.Collections.Generic.IList<string> Identities { get; set; }
