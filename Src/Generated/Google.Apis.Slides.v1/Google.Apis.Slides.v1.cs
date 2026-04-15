e8fe3d8f3a0051816969a83b8d345872b1613150
Amanda Tarafa Mas <atarafamas@google.com>
2026-04-15 08:06:45 +0000

    feat: Generate Google.Apis.Slides.v1 version 1.73.0.4115

--- Src/Generated/Google.Apis.Slides.v1/Google.Apis.Slides.v1.cs
+++ Src/Generated/Google.Apis.Slides.v1/Google.Apis.Slides.v1.cs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -471,6 +471,10 @@ namespace Google.Apis.Slides.v1
                     /// <summary>The thumbnail image width of 200px.</summary>
                     [Google.Apis.Util.StringValueAttribute("SMALL")]
                     SMALL = 3,
+
+                    /// <summary>The thumbnail image width of 2000px.</summary>
+                    [Google.Apis.Util.StringValueAttribute("WIDTH2000_PX")]
+                    WIDTH2000PX = 4,
                 }
 
                 /// <summary>Gets the method name.</summary>
