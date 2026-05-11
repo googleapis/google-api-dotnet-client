6e912cf5f0ab2171d078c64186e79a7ff03ed8b7
Amanda Tarafa Mas <atarafamas@google.com>
2026-05-11 08:05:57 +0000

    feat: Generate Google.Apis.Batch.v1 version 1.74.0.4136

--- Src/Generated/Google.Apis.Batch.v1/Google.Apis.Batch.v1.cs
+++ Src/Generated/Google.Apis.Batch.v1/Google.Apis.Batch.v1.cs
@@ -1313,8 +1313,8 @@ namespace Google.Apis.Batch.v1
                 public virtual string Name { get; private set; }
 
                 /// <summary>
-                /// Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented
-                /// otherwise. This is primarily for internal usage.
+                /// Optional. Do not use this field unless explicitly documented otherwise. This is primarily for
+                /// internal usage.
                 /// </summary>
                 [Google.Apis.Util.RequestParameterAttribute("extraLocationTypes", Google.Apis.Util.RequestParameterType.Query)]
                 public virtual Google.Apis.Util.Repeatable<string> ExtraLocationTypes { get; set; }
