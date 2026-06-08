4f3206a778502c4391824f336cf0d34b80cd21cc
Amanda Tarafa Mas <atarafamas@google.com>
2026-06-08 08:07:27 +0000

    feat: Generate Google.Apis.CloudScheduler.v1 version 1.75.0.4159

--- Src/Generated/Google.Apis.CloudScheduler.v1/Google.Apis.CloudScheduler.v1.cs
+++ Src/Generated/Google.Apis.CloudScheduler.v1/Google.Apis.CloudScheduler.v1.cs
@@ -1229,10 +1229,14 @@ namespace Google.Apis.CloudScheduler.v1
             }
 
             /// <summary>
-            /// Lists information about the supported locations for this service. This method can be called in two ways:
-            /// * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:**
-            /// Use the path `GET /v1/projects/{project_id}/locations`. This may include public locations as well as
-            /// private or other locations specifically visible to the project.
+            /// Lists information about the supported locations for this service. This method lists locations based on
+            /// the resource scope provided in the ListLocationsRequest.name field: * **Global locations**: If `name` is
+            /// empty, the method lists the public locations available to all projects. * **Project-specific
+            /// locations**: If `name` follows the format `projects/{project}`, the method lists locations visible to
+            /// that specific project. This includes public, private, or other project-specific locations enabled for
+            /// the project. For gRPC and client library implementations, the resource name is passed as the `name`
+            /// field. For direct service calls, the resource name is incorporated into the request path based on the
+            /// specific service implementation and version.
             /// </summary>
             /// <param name="name">The resource that owns the locations collection, if applicable.</param>
             public virtual ListRequest List(string name)
@@ -1241,10 +1245,14 @@ namespace Google.Apis.CloudScheduler.v1
             }
 
             /// <summary>
-            /// Lists information about the supported locations for this service. This method can be called in two ways:
-            /// * **List all public locations:** Use the path `GET /v1/locations`. * **List project-visible locations:**
-            /// Use the path `GET /v1/projects/{project_id}/locations`. This may include public locations as well as
-            /// private or other locations specifically visible to the project.
+            /// Lists information about the supported locations for this service. This method lists locations based on
+            /// the resource scope provided in the ListLocationsRequest.name field: * **Global locations**: If `name` is
+            /// empty, the method lists the public locations available to all projects. * **Project-specific
+            /// locations**: If `name` follows the format `projects/{project}`, the method lists locations visible to
+            /// that specific project. This includes public, private, or other project-specific locations enabled for
+            /// the project. For gRPC and client library implementations, the resource name is passed as the `name`
+            /// field. For direct service calls, the resource name is incorporated into the request path based on the
+            /// specific service implementation and version.
             /// </summary>
             public class ListRequest : CloudSchedulerBaseServiceRequest<Google.Apis.CloudScheduler.v1.Data.ListLocationsResponse>
             {
@@ -1260,8 +1268,8 @@ namespace Google.Apis.CloudScheduler.v1
                 public virtual string Name { get; private set; }
 
                 /// <summary>
-                /// Optional. Do not use this field. It is unsupported and is ignored unless explicitly documented
-                /// otherwise. This is primarily for internal usage.
+                /// Optional. Do not use this field unless explicitly documented otherwise. This is primarily for
+                /// internal usage.
                 /// </summary>
                 [Google.Apis.Util.RequestParameterAttribute("extraLocationTypes", Google.Apis.Util.RequestParameterType.Query)]
                 public virtual Google.Apis.Util.Repeatable<string> ExtraLocationTypes { get; set; }
@@ -1444,7 +1452,9 @@ namespace Google.Apis.CloudScheduler.v1.Data
         /// will append `"AppEngine-Google; (+http://code.google.com/appengine)"` to the modified `User-Agent`. *
         /// `X-CloudScheduler`: This header will be set to true. * `X-CloudScheduler-JobName`: This header will contain
         /// the job name. * `X-CloudScheduler-ScheduleTime`: For Cloud Scheduler jobs specified in the unix-cron format,
-        /// this header will contain the job schedule as an offset of UTC parsed according to RFC3339. If the job has a
+        /// this header will contain the job schedule as an offset of UTC parsed according to RFC3339. Remains constant
+        /// across retries and can be used for [job request
+        /// deduplication](https://docs.cloud.google.com/scheduler/docs/overview#job-deduplication). If the job has a
         /// body and the following headers are not set by the user, Cloud Scheduler sets default values: *
         /// `Content-Type`: This will be set to `"application/octet-stream"`. You can override this default by
         /// explicitly setting `Content-Type` to a particular media type when creating the job. For example, you can set
@@ -1594,11 +1604,15 @@ namespace Google.Apis.CloudScheduler.v1.Data
         /// request. Some HTTP request headers are ignored or replaced. A partial list of headers that are ignored or
         /// replaced is below: * Host: This will be computed by Cloud Scheduler and derived from uri. *
         /// `Content-Length`: This will be computed by Cloud Scheduler. * `User-Agent`: This will be set to
-        /// `"Google-Cloud-Scheduler"`. * `X-Google-*`: Google internal use only. * `X-AppEngine-*`: Google internal use
-        /// only. * `X-CloudScheduler`: This header will be set to true. * `X-CloudScheduler-JobName`: This header will
-        /// contain the job name. * `X-CloudScheduler-ScheduleTime`: For Cloud Scheduler jobs specified in the unix-cron
-        /// format, this header will contain the job schedule as an offset of UTC parsed according to RFC3339. If the
-        /// job has a body and the following headers are not set by the user, Cloud Scheduler sets default values: *
+        /// `"Google-Cloud-Scheduler"`. * `X-Google-*`: Used internally by Google. If present in an external user
+        /// request, it is replaced by the internal header. * `X-AppEngine-*`: Used internally by Google. If present in
+        /// an external user request, it is replaced by the internal header. * `X-CloudScheduler`: This header will be
+        /// set to true. * `X-CloudScheduler-JobName`: This header will contain the job name. *
+        /// `X-CloudScheduler-ScheduleTime`: For Cloud Scheduler jobs specified in the unix-cron format, this header
+        /// will contain the job schedule as an offset of UTC parsed according to RFC3339. Remains constant across
+        /// retries and can be used for [job request
+        /// deduplication](https://docs.cloud.google.com/scheduler/docs/overview#job-deduplication). If the job has a
+        /// body and the following headers are not set by the user, Cloud Scheduler sets default values: *
         /// `Content-Type`: This will be set to `"application/octet-stream"`. You can override this default by
         /// explicitly setting `Content-Type` to a particular media type when creating the job. For example, you can set
         /// `Content-Type` to `"application/json"`. The total size of headers must be less than 80KB.
