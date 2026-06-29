1da4408459c23a85baf88bc72bcc241c5467e6f7
Amanda Tarafa Mas <atarafamas@google.com>
2026-06-29 14:54:30 -0700

    tests: Fix flake in `RequestParameterDescriptorProviderTest`.
    Because these tests rely on the static parameter descriptor cache, we cannot run them in parallel with other tests that may reset the cache. We put all of these tests on the same collection.

--- Src/Support/Google.Apis.Core/ApplicationContext.cs
+++ Src/Support/Google.Apis.Core/ApplicationContext.cs
@@ -17,7 +17,6 @@ limitations under the License.
 using Google.Apis.Logging;
 using Google.Apis.Util;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
