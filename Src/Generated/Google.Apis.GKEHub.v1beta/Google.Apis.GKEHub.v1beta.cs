17f36d4e2616a9dc03f725c25b9de92634c7c8b7
Amanda Tarafa Mas <atarafamas@google.com>
2026-07-27 08:07:13 +0000

    feat: Generate Google.Apis.GKEHub.v1beta version 1.75.0.4217

--- Src/Generated/Google.Apis.GKEHub.v1beta/Google.Apis.GKEHub.v1beta.cs
+++ Src/Generated/Google.Apis.GKEHub.v1beta/Google.Apis.GKEHub.v1beta.cs
@@ -7195,9 +7195,9 @@ namespace Google.Apis.GKEHub.v1beta.Data
         public virtual ConfigManagementHierarchyControllerConfig HierarchyController { get; set; }
 
         /// <summary>
-        /// Optional. Deprecated: In Preview, automatic Feature management is unavailable from version 1.21.0 onwards,
-        /// and Config Sync only supports manual upgrades. If set to manual upgrades, clear this field instead, which is
-        /// behaviorally equivalent.
+        /// Optional. Deprecated: Automatic Feature management is in Preview and is unavailable in version 1.21.0 and
+        /// later, after which Config Sync only supports manual upgrades. If set to manual upgrades, clear this field
+        /// instead, which is behaviorally equivalent but helps prevent compatibility issues with newer fields.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("management")]
         public virtual string Management { get; set; }
@@ -11877,6 +11877,10 @@ namespace Google.Apis.GKEHub.v1beta.Data
         /// <summary>Optional. Specifies modernization compatibility for the fleet.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("modernizationCompatibility")]
         public virtual string ModernizationCompatibility { get; set; }
+
+        /// <summary>Optional. Declares your intended modernization strategy for the fleet.</summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("modernizationStrategy")]
+        public virtual string ModernizationStrategy { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
