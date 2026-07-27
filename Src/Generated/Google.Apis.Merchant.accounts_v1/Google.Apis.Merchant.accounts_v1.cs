3f4f94ddd43119f50cdfb75636f61e6864e70984
Amanda Tarafa Mas <atarafamas@google.com>
2026-07-27 08:07:25 +0000

    feat: Generate Google.Apis.Merchant.accounts_v1 version 1.75.0.4220

--- Src/Generated/Google.Apis.Merchant.accounts_v1/Google.Apis.Merchant.accounts_v1.cs
+++ Src/Generated/Google.Apis.Merchant.accounts_v1/Google.Apis.Merchant.accounts_v1.cs
@@ -5933,6 +5933,13 @@ namespace Google.Apis.Merchant.accounts_v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("providerDisplayName")]
         public virtual string ProviderDisplayName { get; set; }
 
+        /// <summary>
+        /// Service type for UCP Checkout Management. The provider is managing the UCP Checkout capability integration
+        /// for the merchant.
+        /// </summary>
+        [Newtonsoft.Json.JsonPropertyAttribute("ucpCheckoutManagement")]
+        public virtual UcpCheckoutManagement UcpCheckoutManagement { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
@@ -8897,6 +8904,13 @@ namespace Google.Apis.Merchant.accounts_v1.Data
         [Newtonsoft.Json.JsonPropertyAttribute("minTransitDays")]
         public virtual System.Nullable<int> MinTransitDays { get; set; }
 
+        /// <summary>The ETag of the item.</summary>
+        public virtual string ETag { get; set; }
+    }
+
+    /// <summary>`UcpCheckoutManagement` payload.</summary>
+    public class UcpCheckoutManagement : Google.Apis.Requests.IDirectResponseSchema
+    {
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
