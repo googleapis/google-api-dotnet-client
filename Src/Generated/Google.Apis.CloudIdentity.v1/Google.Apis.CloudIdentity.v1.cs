be11e98dd08b5bab698cf5cfa5da6bb1b537004b
Amanda Tarafa Mas <atarafamas@google.com>
2026-08-10 08:07:13 +0000

    feat: Generate Google.Apis.CloudIdentity.v1 version 1.75.0.4232

--- Src/Generated/Google.Apis.CloudIdentity.v1/Google.Apis.CloudIdentity.v1.cs
+++ Src/Generated/Google.Apis.CloudIdentity.v1/Google.Apis.CloudIdentity.v1.cs
@@ -8324,7 +8324,7 @@ namespace Google.Apis.CloudIdentity.v1.Data
     /// <summary>Setting</summary>
     public class Setting : Google.Apis.Requests.IDirectResponseSchema
     {
-        /// <summary>Required. Immutable. The type of the Setting. .</summary>
+        /// <summary>Required. Immutable. The type of the Setting.</summary>
         [Newtonsoft.Json.JsonPropertyAttribute("type")]
         public virtual string Type { get; set; }
 
