cf09003d0152eb1e21cdafeabc1018b7fb3bb477
Amanda Tarafa Mas <atarafamas@google.com>
2026-08-12 08:05:15 +0000

    feat: Generate Google.Apis.Kmsinventory.v1 version 1.75.0.4238

--- Src/Generated/Google.Apis.Kmsinventory.v1/Google.Apis.Kmsinventory.v1.cs
+++ Src/Generated/Google.Apis.Kmsinventory.v1/Google.Apis.Kmsinventory.v1.cs
@@ -1527,7 +1527,7 @@ namespace Google.Apis.Kmsinventory.v1.Data
     {
         /// <summary>
         /// Optional. The resource name of the backend environment where the key material of CryptoKeyVersions is
-        /// associated with. Setting this field overrides the CryptoKeyBackend. This field may be set when
+        /// associated with. Setting this field overrides the crypto_key_backend. This field may be set when
         /// CryptoKeyVersions is set to EXTERNAL_VPC. Format: `projects/*/locations/*/ekmConnections/*`.
         /// </summary>
         [Newtonsoft.Json.JsonPropertyAttribute("ekmConnectionBackendOverride")]
