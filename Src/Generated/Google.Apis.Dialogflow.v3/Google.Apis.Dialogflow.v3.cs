b33114fbc6d82797530846df03d4e2256fd3f1c5
Amanda Tarafa Mas <atarafamas@google.com>
2026-08-24 08:07:25 +0000

    feat: Generate Google.Apis.Dialogflow.v3 version 1.76.0.4244

--- Src/Generated/Google.Apis.Dialogflow.v3/Google.Apis.Dialogflow.v3.cs
+++ Src/Generated/Google.Apis.Dialogflow.v3/Google.Apis.Dialogflow.v3.cs
@@ -19746,6 +19746,9 @@ namespace Google.Apis.Dialogflow.v3.Data
         [Newtonsoft.Json.JsonPropertyAttribute("message")]
         public virtual string Message { get; set; }
 
+        [Newtonsoft.Json.JsonPropertyAttribute("retryable")]
+        public virtual System.Nullable<bool> Retryable { get; set; }
+
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
     }
@@ -22301,6 +22304,9 @@ namespace Google.Apis.Dialogflow.v3.Data
     {
         [Newtonsoft.Json.JsonPropertyAttribute("message")]
         public virtual string Message { get; set; }
+
+        [Newtonsoft.Json.JsonPropertyAttribute("retryable")]
+        public virtual System.Nullable<bool> Retryable { get; set; }
 
         /// <summary>The ETag of the item.</summary>
         public virtual string ETag { get; set; }
