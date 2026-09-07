bdd37fb1daf3fd06137f96431eee12b441db0fa4
Amanda Tarafa Mas <atarafamas@google.com>
2026-09-07 08:07:29 +0000

    feat: Generate Google.Apis.CloudTasks.v2beta3 version 1.76.0.4260

--- Src/Generated/Google.Apis.CloudTasks.v2beta3/Google.Apis.CloudTasks.v2beta3.cs
+++ Src/Generated/Google.Apis.CloudTasks.v2beta3/Google.Apis.CloudTasks.v2beta3.cs
@@ -2961,7 +2961,7 @@ namespace Google.Apis.CloudTasks.v2beta3.Data
     }
 
     /// <summary>
-    /// HTTP target. When specified as a Queue, all the tasks with [HttpRequest] will be overridden according to the
+    /// HTTP target. When specified at the Queue level, all tasks with HttpRequest are overridden according to the
     /// target.
     /// </summary>
     public class HttpTarget : Google.Apis.Requests.IDirectResponseSchema
