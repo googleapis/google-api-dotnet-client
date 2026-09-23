cd06ec5c040062addd83f53c0a25a897aca86bfe
Amanda Tarafa Mas <atarafamas@google.com>
2026-09-23 08:08:44 +0000

    feat: Generate Google.Apis.Iam.v1 version 1.76.0.4278

--- Src/Generated/Google.Apis.Iam.v1/Google.Apis.Iam.v1.cs
+++ Src/Generated/Google.Apis.Iam.v1/Google.Apis.Iam.v1.cs
@@ -11291,9 +11291,9 @@ namespace Google.Apis.Iam.v1.Data
         public virtual System.Nullable<int> RotationWindowPercentage { get; set; }
 
         /// <summary>
-        /// Optional. If set to true, the trust domain will utilize the GCP-provisioned default CA. A default CA in the
-        /// same region as the workload will be selected to issue the certificate. Enabling this will clear any existing
-        /// `ca_pools` configuration to provision the certificates. NOTE: This field is mutually exclusive with
+        /// Optional. Determines whether the trust domain utilizes the Google Cloud-provisioned default CA. A default CA
+        /// in the same region as the workload will be selected to issue the certificate. Enabling this will clear any
+        /// existing `ca_pools` configuration to provision the certificates. NOTE: This field is mutually exclusive with
         /// `ca_pools`. If this flag is enabled, certificates will be automatically provisioned from the default shared
         /// CAs. This flag should not be set if you want to use your own CA pools to provision the certificates.
         /// </summary>
@@ -12936,7 +12936,7 @@ namespace Google.Apis.Iam.v1.Data
         public virtual System.Collections.Generic.IList<TrustAnchor> TrustAnchors { get; set; }
 
         /// <summary>
-        /// Optional. If set to True, the trust bundle will include the private ca managed identity regional root public
+        /// Optional. Determines whether the trust bundle includes the private CA managed identity regional root public
         /// certificates. Important: `trust_default_shared_ca` is only supported for managed identity trust domain
         /// resource.
         /// </summary>
@@ -13789,9 +13789,11 @@ namespace Google.Apis.Iam.v1.Data
         /// may be referenced in the expressions: * `assertion`: JSON representing the authentication credential issued
         /// by the provider. * `google`: The Google attributes mapped from the assertion in the `attribute_mappings`. *
         /// `attribute`: The custom attributes mapped from the assertion in the `attribute_mappings`. The maximum length
-        /// of the attribute condition expression is 4096 characters. If unspecified, all valid authentication
-        /// credential are accepted. The following example shows how to only allow credentials with a mapped
-        /// `google.groups` value of `admins`:
+        /// of the `attribute_condition` expression is 4,096 characters. Providing a condition longer than this will
+        /// result in an error. If unspecified, all valid authentication credentials are accepted. However, multi-tenant
+        /// identity providers (such as GitHub or Terraform Cloud) require an `attribute_condition` to prevent token
+        /// spoofing. The following example shows how to only allow credentials with a mapped `google.groups` value of
+        /// `admins`:
         /// ```
         /// "'admins' in google.groups"
         /// ```
